4f68099f8bd66384bb17b7a54f97e439dad32551
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-18 15:05:17 +0530

    Ignore dist form babel

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,5 @@
 echo "> Start transpiling ES2015"
 echo ""
-./node_modules/.bin/babel --plugins "transform-runtime" ./ --ignore node_modules --out-dir ./dist
+./node_modules/.bin/babel --plugins "transform-runtime" ./ --ignore node_modules,dist --out-dir ./dist
 echo ""
 echo "> Complete transpiling ES2015"
