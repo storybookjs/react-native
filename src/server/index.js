b423e48a293cc198787be9ca90b67023dbbd5e9a
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-23 16:38:00 +0530

    Load config.js from the .storybook directory

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,8 @@
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
-var getIndexHtml = require('./index.html.js');
-var config = require('./config');
+var getIndexHtml = require('./index.html');
+var config = require('./webpack.config');
 
 var app = new (require('express'))()
 var port = process.argv[2]? parseInt(process.argv[2]) : 4000;
