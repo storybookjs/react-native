832911c3d15e2254d0a5ac28ce39e4492757c663
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-23 16:24:37 +0530

    Add support for custom webpack loaders and plugins. This help us to support, nearly any CSS loading system

--- dist/server/index.js
+++ dist/server/index.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var getIndexHtml = require('./index.html.js');
-var config = require('./webpack.config');
+var config = require('./config');
 
 var app = new (require('express'))();
 var port = process.argv[2] ? parseInt(process.argv[2]) : 4000;
