d4b1c35e154659862e252106999257afda51d0e1
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-04-04 13:28:12 +0530

    Add development utility with 'npm run dev'

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,5 @@
 echo "> Start transpiling ES2015"
 echo ""
-./node_modules/.bin/babel --plugins "transform-runtime" ./src --ignore __test__ --out-dir ./dist
+./node_modules/.bin/babel --ignore __tests__ --plugins "transform-runtime" ./src --out-dir ./dist
 echo ""
 echo "> Complete transpiling ES2015"
