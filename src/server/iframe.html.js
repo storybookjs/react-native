e3765e0ff100c61c4cfc5a9c0a9bc7694deb1ec3
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2016-04-12 17:08:02 -0700

    Add React Developer Tools hook (#104)

--- src/server/iframe.html.js
+++ src/server/iframe.html.js
@@ -3,6 +3,11 @@ export default function (headHtml) {
     <!DOCTYPE html>
     <html>
       <head>
+        <script>
+          if (window.parent !== window) {
+            window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.parent.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+          }
+        </script>
         <title>React Storybook</title>
         ${headHtml}
       </head>
