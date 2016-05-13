320284878a027f1952e7acc5096e677d620f728e
Muhammed Thanish <mnmtanish@gmail.com>
2016-05-13 15:20:38 +0530

    Fix build error

--- src/modules/ui/routes.js
+++ src/modules/ui/routes.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './containers/layout';
-import Preview from '../preview/containers/preview';
 import LeftPanel from './containers/left_panel';
 import ActionLogger from './containers/action_logger';
 import ShortcutsHelp from './containers/shortcuts_help';
 
-export default function (injectDeps) {
+export default function (injectDeps, {Preview}) {
   const InjectedLayout = injectDeps(Layout);
   const InjectedShortcutsHelp = injectDeps(ShortcutsHelp);
   const rootEl = document.getElementById('root');
