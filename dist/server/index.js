2530b843732f416f10baa25cdf6fe2b9a40cd0ee
Muhammed Thanish <mnmtanish@gmail.com>
2016-05-25 13:12:34 +0530

    Build source files

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,26 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-var _webpack = require('webpack');
-
-var _webpack2 = _interopRequireDefault(_webpack);
-
-var _webpackDevMiddleware = require('webpack-dev-middleware');
-
-var _webpackDevMiddleware2 = _interopRequireDefault(_webpackDevMiddleware);
-
-var _webpackHotMiddleware = require('webpack-hot-middleware');
-
-var _webpackHotMiddleware2 = _interopRequireDefault(_webpackHotMiddleware);
-
-var _index = require('./index.html');
-
-var _index2 = _interopRequireDefault(_index);
-
-var _iframe = require('./iframe.html');
-
-var _iframe2 = _interopRequireDefault(_iframe);
-
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
@@ -28,18 +8,6 @@ var _express2 = _interopRequireDefault(_express);
 var _commander = require('commander');
 
 var _commander2 = _interopRequireDefault(_commander);
-
-var _package = require('../../package.json');
-
-var _package2 = _interopRequireDefault(_package);
-
-var _webpack3 = require('./webpack.config');
-
-var _webpack4 = _interopRequireDefault(_webpack3);
-
-var _config = require('./config');
-
-var _config2 = _interopRequireDefault(_config);
 
 var _path = require('path');
 
@@ -49,7 +17,13 @@ var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
 
-var _utils = require('./utils');
+var _middleware = require('./middleware');
+
+var _middleware2 = _interopRequireDefault(_middleware);
+
+var _package = require('../../package.json');
+
+var _package2 = _interopRequireDefault(_package);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -86,24 +60,7 @@ if (_commander2.default.staticDir) {
 // Build the webpack configuration using the `baseConfig`
 // custom `.babelrc` file and `webpack.config.js` files
 var configDir = _commander2.default.configDir || './.storybook';
-var config = (0, _config2.default)('DEVELOPMENT', _webpack4.default, configDir);
-
-var compiler = (0, _webpack2.default)(config);
-var devMiddlewareOptions = {
-  noInfo: true,
-  publicPath: config.output.publicPath
-};
-app.use((0, _webpackDevMiddleware2.default)(compiler, devMiddlewareOptions));
-app.use((0, _webpackHotMiddleware2.default)(compiler));
-
-app.get('/', function (req, res) {
-  res.send((0, _index2.default)());
-});
-
-var headHtml = (0, _utils.getHeadHtml)(configDir);
-app.get('/iframe.html', function (req, res) {
-  res.send((0, _iframe2.default)(headHtml));
-});
+app.use((0, _middleware2.default)(configDir));
 
 app.listen.apply(app, listenAddr.concat([function (error) {
   if (error) {
