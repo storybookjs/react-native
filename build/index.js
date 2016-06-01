108eb7b1b047962fbdbbb429f850a78ff4ce4437
Muhammed Thanish <mnmtanish@gmail.com>
2016-06-01 14:29:19 +0530

    Build source files

---
+++ build/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (storyFn) {
+  return _react2.default.createElement(
+    'div',
+    { style: style },
+    storyFn()
+  );
+};
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var style = {
+  height: '100vh',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+};
