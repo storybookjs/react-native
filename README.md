4a95280c2392d0d91771b667c3b233f1051cd371
Muhammed Thanish <mnmtanish@gmail.com>
2016-06-14 10:43:09 +0530

    Improve docs

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# React Storybook Story Component
+# React Storybook Story
 
-React Storybook Story Component
+A component to add additional information with your [react-storybook](#) stories.
+
+![React Storybook Screenshot](docs/screenshot.png)
+
+To get started, install the component as a `devDependency`
+
+```shell
+npm i -D react-storybook-story
+```
+
+When writing stories, wrap your component with the `<Story />` component and give it documentation via the `info` parameter.
+
+```
+import Story from 'react-storybook-story';
+
+const stories = storiesOf('<MyComponent />', module);
+
+stories.add('my-example-story', function () {
+  const info = \`
+    # My Example Story
+    This is an example story to...
+  \`;
+
+  return (
+    <Story info={info}>
+      <MyComponent foo={'bar'} />
+    </Story>
+  );
+});
+```
+
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,22 +2,45 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Story from '../index';
 
-const stories = storiesOf('Story', module);
+const stories = storiesOf('<Story />', module);
 
-stories.add('defaults', function () {
+stories.add('basic-usage', function () {
   const info = `
-    # Story
+    # <Story />
 
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    ## Basic Usage
+
+    This is a simple example story to demonstrate how this component can be used. The \`<Story>\` component can be used to attach additional information with your stories.
+
+    Information such as example source code
 
     ~~~jsx
-    <strong>Hello World</strong>
+    stories.add('basic-usage', function () {
+      const info = \`
+        # Story
+        This is a simple example story to...
+      \`;
+
+      return (
+        <Story info={info}>
+          <em>Click the "?" button on top-right corner</em>
+        </Story>
+      );
+    });
     ~~~
+
+    component properties
+
+    | property | type   | required | default |
+    |----------|--------|----------|---------|
+    | info     | string | no       | ""      |
+
+    or something else...
   `;
 
   return (
     <Story info={info}>
-      <strong>Hello World</strong>
+      <em>Click the "?" button on top-right corner</em>
     </Story>
   );
 });
