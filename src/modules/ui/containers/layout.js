7daf2b126b79498c33262d12d8d4718feca70133
Ritesh Kumar <rkritesh078@gmail.com>
2016-06-25 12:59:28 +0530

    removed unused code

--- src/modules/ui/containers/layout.js
+++ src/modules/ui/containers/layout.js
@@ -3,20 +3,13 @@ import { useDeps, composeAll } from 'mantra-core';
 import pick from 'lodash.pick';
 import reduxComposer from '../libs/redux_composer';
 
-export const composer = ({ shortcuts, api }, { actions }) => {
-  const actionMap = actions();
-  const propShortCuts = pick(
+export const composer = ({ shortcuts }) => {
+  return pick(
     shortcuts,
     'showLeftPanel',
     'showDownPanel',
-    'goFullScreen',
-    'showSearchBox'
+    'goFullScreen'
   );
-  return {
-    ...propShortCuts,
-    stories: api.stories,
-    actions: actionMap,
-  };
 };
 
 export default composeAll(
