646922bef71300ccbbdd468e27ccede10f4df295
Aruna Herath <arunabherath@gmail.com>
2016-07-26 16:47:19 +0530

    Call storyFn once and get the initial values.
    This avoids directly mutating the state while initialization of fields
    in the PropEditor.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import PropEditor from './components/PropEditor';
 
 export function addWithKnobs(storyName, storyFn) {
+  const initialValues = {};
+
+  const recordInitialValues = (name, initial, type) => {
+    let value = initial;
+    if (type === 'object') {
+      value = JSON.stringify(value, null, 4);
+    }
+
+    initialValues[name] = {
+      name,
+      value,
+      type,
+      valid: true,
+    };
+
+    return initial;
+  };
+
+  // Call storyFn once to get the initial values. Just ignore the result of
+  // this call.
+  const dummyContext = {}; // TODO: Find if this object needs some fields inside
+  storyFn(dummyContext, recordInitialValues);
+
   this.add(storyName, (context) => (
-    <PropEditor storyFn={storyFn} context={context} />
+    <PropEditor storyFn={storyFn} context={context} initialValues={initialValues} />
   ));
 }
