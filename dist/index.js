f793fb02203239229f059a20666bba51d495a368
Muhammed Thanish <mnmtanish@gmail.com>
2016-08-02 13:53:52 +0530

    Log all arguments

--- dist/index.js
+++ dist/index.js
@@ -20,30 +20,34 @@ var _ActionLogger2 = _interopRequireDefault(_ActionLogger);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _format(message) {
-  if (typeof message.preventDefault !== 'undefined') {
+function _format(arg) {
+  if (arg && typeof arg.preventDefault !== 'undefined') {
     return '[SyntheticEvent]';
   }
-  return message;
+  return arg;
 }
 
 function action(name) {
-  return function (_message) {
-    var message = _format(_message);
+  return function () {
+    for (var _len = arguments.length, _args = Array(_len), _key = 0; _key < _len; _key++) {
+      _args[_key] = arguments[_key];
+    }
+
+    var args = Array.from(_args).map(_format);
     var channel = _storybookAddons2.default.getChannel();
     channel.emit('addon:actions', {
       id: Math.random().toString(16).slice(2),
-      data: { name: name, message: message }
+      data: { name: name, args: args }
     });
   };
 }
 
 function register() {
   _storybookAddons2.default.register('kadirahq/storybook-addon-actions', function (api) {
+    var channel = _storybookAddons2.default.getChannel();
     _storybookAddons2.default.addPanel('kadirahq/storybook-addon-actions', {
       title: 'Action Logger',
       render: function render() {
-        var channel = _storybookAddons2.default.getChannel();
         return _react2.default.createElement(_ActionLogger2.default, { channel: channel });
       }
     });
--- src/index.js
+++ src/index.js
@@ -2,32 +2,30 @@ import React from 'react';
 import addons from '@kadira/storybook-addons';
 import ActionLogger from './containers/ActionLogger'
 
-function _format(message) {
-  if (typeof message.preventDefault !== 'undefined') {
+function _format(arg) {
+  if (arg && typeof arg.preventDefault !== 'undefined') {
     return '[SyntheticEvent]';
   }
-  return message;
+  return arg;
 }
 
 export function action(name) {
-  return function (_message) {
-    const message = _format(_message);
+  return function (..._args) {
+    const args = Array.from(_args).map(_format);
     const channel = addons.getChannel();
     channel.emit('addon:actions', {
       id: Math.random().toString(16).slice(2),
-      data: { name, message },
+      data: { name, args },
     });
   };
 }
 
 export function register() {
   addons.register('kadirahq/storybook-addon-actions', api => {
+    const channel = addons.getChannel();
     addons.addPanel('kadirahq/storybook-addon-actions', {
       title: 'Action Logger',
-      render() {
-        const channel = addons.getChannel();
-        return <ActionLogger channel={channel} />;
-      }
+      render: () => <ActionLogger channel={channel} />
     });
   });
 }
