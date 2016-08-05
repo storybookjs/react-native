a513e8454a5d2a0cf6e59e8c78ef33f3bdfd5d70
Aruna Herath <arunabherath@gmail.com>
2016-08-05 21:18:50 +0530

    Handle undefined info before full options are found

--- src/index.js
+++ src/index.js
@@ -9,26 +9,23 @@ const defaultOptions = {
 };
 
 export default {
-  addWithInfo(storyName, _info, _storyFn, _options) {
+  addWithInfo(storyName, info, storyFn, _options) {
+    if (typeof storyFn !== 'function') {
+      if (typeof info === 'function') {
+        _options = storyFn;
+        storyFn = info;
+        info = '';
+      } else {
+        throw new Error('No story defining function has been specified');
+      }
+    }
+
     const options = {
       ...defaultOptions,
       ..._options
     };
 
     this.add(storyName, (context) => {
-      let info = _info;
-      let storyFn = _storyFn;
-
-      if (typeof storyFn !== 'function') {
-        if (typeof info === 'function') {
-          options = storyFn;
-          storyFn = info;
-          info = '';
-        } else {
-          throw new Error('No story defining function has been specified');
-        }
-      }
-
       const props = {
         info,
         context,
