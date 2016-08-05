7ff9f74918c4aa241d31b19ef922800daa0af6ee
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-05 10:21:20 +0530

    Use the default babel-config from CRA. (#354)
    * Use the default babel-config from CRA.
    
    * Fix lint issues

--- dist/server/config/babel.prod.js
+++ dist/server/config/babel.prod.js
@@ -1,11 +1,41 @@
 'use strict';
 
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 module.exports = {
+  // Don't try to find .babelrc because we want to force this configuration.
   babelrc: false,
-  presets: ['babel-preset-es2015', 'babel-preset-es2016', 'babel-preset-stage-0', 'babel-preset-react'].map(require.resolve),
-  plugins: ['babel-plugin-transform-react-constant-elements'].map(require.resolve).concat([[require.resolve('babel-plugin-transform-runtime'), {
+  presets: [
+  // let, const, destructuring, classes, modules
+  'babel-preset-es2015',
+  // exponentiation
+  'babel-preset-es2016',
+  // JSX, Flow
+  'babel-preset-react'],
+  plugins: [
+  // function x(a, b, c,) { }
+  'babel-plugin-syntax-trailing-function-commas',
+  // await fetch()
+  'babel-plugin-syntax-async-functions',
+  // class { handleClick = () => { } }
+  'babel-plugin-transform-class-properties',
+  // { ...todo, completed: true }
+  'babel-plugin-transform-object-rest-spread',
+  // function* () { yield 42; yield 43; }
+  'babel-plugin-transform-regenerator',
+  // Polyfills the runtime needed for async/await and generators
+  ['babel-plugin-transform-runtime', {
     helpers: false,
     polyfill: false,
     regenerator: true
-  }]])
+  }],
+  // Optimization: hoist JSX that never changes out of render()
+  'babel-plugin-transform-react-constant-elements']
 };
