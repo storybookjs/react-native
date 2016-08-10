2425528aaaa6cb3a6ab9e75e79496ce0f9dd1947
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-10 23:16:14 +0530

    Add a generator for WEBPACK_REACT

---
+++ generators/WEBPACK_REACT/index.js
@@ -0,0 +1,17 @@
+var mergeDirs = require('merge-dirs').default;
+var helpers = require('../../lib/helpers');
+var path = require('path');
+
+mergeDirs(path.resolve(__dirname, 'template/'), '.', 'overwrite');
+
+var packageJson = helpers.getPackageJson();
+
+// TODO: Get the latest version of storybook here.
+packageJson.devDependencies = packageJson.devDependencies || {};
+packageJson.devDependencies['@kadira/storybook'] = '^2.0.0';
+
+packageJson.scripts = packageJson.scripts || {};
+packageJson.scripts['storybook'] = 'start-storybook -p 6006';
+packageJson.scripts['build-storybook'] = 'build-storybook';
+
+helpers.writePackageJson(packageJson);
