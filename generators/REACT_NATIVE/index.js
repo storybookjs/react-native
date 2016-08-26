d0398bfeb510cae9f0c9e6bd0d9506a9b35b8b1f
Muhammed Thanish <mnmtanish@gmail.com>
2016-08-26 15:26:03 +0530

    Set the app name if possible

--- generators/REACT_NATIVE/index.js
+++ generators/REACT_NATIVE/index.js
@@ -1,8 +1,20 @@
 var mergeDirs = require('merge-dirs').default;
 var helpers = require('../../lib/helpers');
 var path = require('path');
+var shell = require('shelljs');
 
+// copy all files from the template directory to project directory
 mergeDirs(path.resolve(__dirname, 'template/'), '.', 'overwrite');
+
+// set correct project name on entry files if possible
+var dirname = shell.ls('-d', 'ios/*.xcodeproj').stdout;
+var projectName = dirname && dirname.slice(
+  'ios/'.length,
+  dirname.length - '.xcodeproj'.length - 1
+);
+if (projectName) {
+  shell.sed('-i', '%APP_NAME%', projectName, 'storybook/index.ios.js');
+}
 
 var packageJson = helpers.getPackageJson();
 
