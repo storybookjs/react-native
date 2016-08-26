a9bab1b52d652131af2797a8046c63a8a311816e
Muhammed Thanish <mnmtanish@gmail.com>
2016-08-26 10:13:05 +0530

    Validate storyFn property in StoryView
    Fixes #57

--- src/preview/components/StoryView/index.js
+++ src/preview/components/StoryView/index.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 export default class StoryView extends Component {
   constructor(props, ...args) {
     super(props, ...args);
-    this.state = {storyFn: () => null};
+    this.state = {storyFn: null};
     this.props.events.on('story', storyFn => this.setState({storyFn}));
   }
 
   render() {
+    if (!this.state.storyFn) {
+      return null;
+    }
     return this.state.storyFn();
   }
 }
