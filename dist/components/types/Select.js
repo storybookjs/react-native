a7bd81888c1612549a6e508a6207c965732482d5
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-09-11 18:53:56 +0530

    Update dist/

--- dist/components/types/Select.js
+++ dist/components/types/Select.js
@@ -32,10 +32,6 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactTextareaAutosize = require('react-textarea-autosize');
-
-var _reactTextareaAutosize2 = _interopRequireDefault(_reactTextareaAutosize);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var styles = {
