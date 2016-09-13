8c9cd6f8fce00e87372ce5773dbd123440e9b2d0
Aruna Herath <arunabherath@gmail.com>
2016-09-13 17:57:42 +0530

    Added the ability to grep
    Can filter stories by a regex using -g parameter

--- src/cli.js
+++ src/cli.js
@@ -11,6 +11,7 @@ program
   .option('-u, --update [boolean]', 'Update saved story snapshots')
   .option('-i, --update-interactive [boolean]',
           'Update saved story snapshots interactively')
+  .option('-g, --grep [string]', 'only test stories matching regexp')
   .parse(process.argv);
 
 const configDir = program.configDir || './.storybook';
@@ -18,8 +19,37 @@ const configDir = program.configDir || './.storybook';
 const configPath = path.resolve(`${configDir}`, 'config');
 require(configPath);
 
-const storybook = getStorybook();
+let storybook = getStorybook();
 
-const { update, updateInteractive:updateI } = program;
+const { update, updateInteractive:updateI, grep } = program;
+
+if(grep) {
+  const filteredStorybook = [];
+  for (const group of storybook) {
+    const re = new RegExp(grep);
+    if(re.test(group.kind)){
+      filteredStorybook.push(group);
+      continue;
+    }
+
+    const filteredGroup = {
+      kind: group.kind,
+      stories: []
+    };
+
+    for (const story of group.stories) {
+      if(re.test(story.name)){
+        filteredGroup.stories.push(story);
+        continue;
+      }
+    }
+
+    if(filteredGroup.stories.length > 0){
+      filteredStorybook.push(filteredGroup);
+    }
+  }
+
+  storybook = filteredStorybook;
+}
 
 runTests(storybook, {configDir, update, updateI});
