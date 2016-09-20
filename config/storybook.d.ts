81e8b47efefb63033468166ffa8a7f834e2f44fd
Phil Cockfield <phil@cockfield.net>
2016-09-20 21:26:16 +1200

    Update TypeScript definition to include `addDecorator`. (#467)

--- config/storybook.d.ts
+++ config/storybook.d.ts
@@ -1,7 +1,12 @@
 declare var module: any; // dangerous
 
+interface StoryDecorator {
+    (story: Function): Object;
+}
+
 interface Story {
     add (storyName: string, callback: Function): Story;
+    addDecorator (decorator: StoryDecorator): Story;
 }
 
 export function storiesOf(name: string, module: any): Story;
