198677e3417846af0efb7df4d54cb0eec9d4e936
Muhammed Thanish <mnmtanish@gmail.com>
2016-09-23 16:32:13 +0530

    Update dist

--- dist/server/config/webpack.config.prod.js
+++ dist/server/config/webpack.config.prod.js
@@ -28,9 +28,7 @@ exports.default = function () {
         screw_ie8: true,
         warnings: false
       },
-      mangle: {
-        screw_ie8: true
-      },
+      mangle: false,
       output: {
         comments: false,
         screw_ie8: true
