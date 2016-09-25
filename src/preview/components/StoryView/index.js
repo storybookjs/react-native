585e77eb123f8632e459f016acf110576e4496e0
Muhammed Thanish <mnmtanish@gmail.com>
2016-09-25 17:22:42 +0530

    Add missing context argument

--- src/preview/components/StoryView/index.js
+++ src/preview/components/StoryView/index.js
@@ -3,14 +3,20 @@ import React, { Component } from 'react';
 export default class StoryView extends Component {
   constructor(props, ...args) {
     super(props, ...args);
-    this.state = {storyFn: null};
-    this.props.events.on('story', storyFn => this.setState({storyFn}));
+    this.state = {storyFn: null, selection: {}};
+    this.props.events.on('story', this.selectStory.bind(this));
+  }
+
+  selectStory(storyFn, selection) {
+    this.setState({storyFn, selection});
   }
 
   render() {
     if (!this.state.storyFn) {
       return null;
     }
-    return this.state.storyFn();
+    const { kind, story } = this.state.selection;
+    const context = { kind, story };
+    return this.state.storyFn(context);
   }
 }
