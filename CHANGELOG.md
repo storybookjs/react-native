2745ec855e6b5ff0dbd6df7702a2dadc7bbbcf19
Muhammed Thanish <mnmtanish@gmail.com>
2016-09-27 12:17:11 +0530

    Initial version

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## Changelog
+
+### v1.0.0
+26-Sep-2016
+
+* Implement send and receive functions
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import Channel from '@kadira/storybook-channel';
+
+export default function createChannel({ key }) {
+  const transport = new PostmsgTransport({ key });
+  return new Channel({ transport });
+}
+
+export class PostmsgTransport {
+  constructor({ key }) {
+    this._key = key;
+    this._buffer = [];
+    this._handler = null;
+    window.addEventListener('message', this._handleEvent.bind(this), false);
+    document.addEventListener('DOMContentLoaded', () => this._flush());
+  }
+
+  setHandler(handler) {
+    this._handler = handler;
+  }
+
+  send(event) {
+    const iframeWindow = this._getWindow();
+    if (!iframeWindow) {
+      return new Promise((resolve, reject) => {
+        this._buffer.push({ event, resolve, reject });
+      });
+    }
+    const data = JSON.stringify({ key: this._key, event });
+    iframeWindow.postMessage(data, '*');
+    return Promise.resolve(null);
+  }
+
+  _flush() {
+    const buffer = this._buffer;
+    this._buffer = [];
+    buffer.forEach(item => {
+      this.send(item.event)
+        .then(item.resolve)
+        .catch(item.reject);
+    });
+  }
+
+  _getWindow() {
+    if (window.top === window.self) {
+      // FIXME this is a really bad idea! use a better way to do this.
+      // This finds the storybook preview iframe to send messages to.
+      const iframes = document.getElementsByTagName('iframe');
+      if (!iframes.length) {
+        return null;
+      }
+      return iframes[0].contentWindow;
+    }
+    return window.top;
+  }
+
+  _handleEvent(e) {
+    const { key, event } = JSON.parse(e.data);
+    if (key !== this._key) {
+      return null;
+    }
+    this._handler(event);
+  }
+}
