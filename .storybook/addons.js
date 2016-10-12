9cde8affdb988f2723936904cb489201c28936fa
thinkholic <mailtokasun@gmail.com>
2016-10-12 13:54:14 +0530

    write stories for addon components

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,6 +3,7 @@
 
 // Use the line below to register this addon
 // import '@kadira/storybook-addon-comments/register';
+import '@kadira/storybook-addon-actions/register';
 import '@kadira/storybook-database-cloud/register';
 import { init } from '../src/manager';
 init();
