1c7d856556ee3cac2750241617495a517d66ee0a
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-14 07:19:00 +0530

    Do not try to load comments, if the user is not loggedIn.

--- src/manager/containers/CommentsPanel/dataStore.js
+++ src/manager/containers/CommentsPanel/dataStore.js
@@ -53,8 +53,12 @@ export default class DataStore {
   }
 
   setCurrentStory(sbKind, sbStory) {
-    this._reloadCurrentComments();
     this.currentStory = { sbKind, sbStory };
+
+    // We don't need to do anything if the there's no loggedIn user.
+    if(!this.user) return;
+
+    this._reloadCurrentComments();
     const item = this._getFromCache(this.currentStory);
 
     if (item) {
