0e02ced5bfd3dfd27837b74b7c3ccb5a872ea612
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-10-24 19:12:12 +0530

    Use process.env as a proper object. (#565)
    Earlier it's not possible to log process.env from the console.
    It's only individual keys like procss.env.NODE_ENV.

--- src/server/config/utils.js
+++ src/server/config/utils.js
@@ -26,19 +26,21 @@ export const nodePaths = (process.env.NODE_PATH || '')
 export function loadEnv(options = {}) {
   const defaultNodeEnv = options.production ? 'production' : 'development';
   const env = {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || defaultNodeEnv),
+    'NODE_ENV': process.env.NODE_ENV || defaultNodeEnv,
     // This is to support CRA's public folder feature.
     // In production we set this to dot(.) to allow the browser to access these assests
     // even when deployed inside a subpath. (like in GitHub pages)
     // In development this is just empty as we always serves from the root.
-    'process.env.PUBLIC_URL': JSON.stringify(options.production ? '.' : ''),
+    'PUBLIC_URL': options.production ? '.' : '',
   };
 
   Object.keys(process.env)
     .filter(name => /^STORYBOOK_/.test(name))
     .forEach((name) => {
-      env[`process.env.${name}`] = JSON.stringify(process.env[name]);
+      env[name] = process.env[name];
     });
 
-  return env;
+  return {
+    'process.env': JSON.stringify(env),
+  };
 }
