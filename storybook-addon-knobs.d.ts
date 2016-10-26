93fd3d4c7d3591b0201851808add8245e02b7e68
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-10-26 21:27:05 +1300

    Rename to storybook-addon-knobs.d.ts (+1 squashed commit) Squashed commits: [4d5a906] Move types to root

---
+++ storybook-addon-knobs.d.ts
@@ -0,0 +1,27 @@
+import WrapStory from "../src/components/WrapStory.js";
+
+interface KnobOption<T> {
+	value: T,
+	type: 'text' | 'boolean' | 'number' | 'object' | 'select' | 'date',
+}
+
+interface StoryContext {
+	kind: string,
+	story: string,
+}
+
+export function knob<T>(name: string, options: KnobOption<T>): T;
+
+export function text(name: string, value: string): string;
+
+export function boolean(name: string, value: boolean): boolean;
+
+export function number(name: string, value: number): number;
+
+export function object(name: string, value: Object): Object;
+
+export function select<T>(name: string, options: { [s: string]: T }, value: string): T;
+
+export function date(name: string, value: Date = new Date(0));
+
+export function withKnobs(storyFn: Function, context: StoryContext): WrapStory;
