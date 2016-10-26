81224bee2cfa8374a2a0ec7d231caab822f463d5
Frederick Fogerty <frederick.fogerty@gmail.com>
2016-10-27 10:27:53 +1300

    Fix lint

--- src/tests/typescript.js
+++ src/tests/typescript.js
@@ -1,4 +1,5 @@
 import * as tt from 'typescript-definition-tester';
+const { describe, it } = global;
 
 
 describe('TypeScript definitions', function () {
@@ -7,7 +8,7 @@ describe('TypeScript definitions', function () {
   it('should compile against index.d.ts', (done) => {
 		// This will test any typescript files in /examples/typescript against the type declarations.
     tt.compileDirectory(
-      __dirname + '/../../example/typescript',
+      `${__dirname}/../../example/typescript`,
       fileName => fileName.match(/\.ts$/),
       () => done()
     );
