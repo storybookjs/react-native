6cf1df29dbd468b5e68a4195d078d38e9f598dd8
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-11-25 08:33:48 +0530

    Fix a typo in the code.

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ export function number(name, value, options = {}) {
     value,
   };
 
-  return manager.knob(name, ...finalOptions);
+  return manager.knob(name, finalOptions);
 }
 
 export function color(name, value) {
