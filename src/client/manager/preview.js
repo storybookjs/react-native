ceebfc58b40491c0a3c8e91451a332d3c280cd78
Aruna Herath <arunabherath@gmail.com>
2016-11-28 14:27:20 +0530

    Completely avoid re-rendering the preview iframe (#631)
    When manager re-rendered there is a chance that this component is
    rerendered. That makes the iframe to load again. So the complete story
    is remounted. This is not preferred except when changing stories.

--- src/client/manager/preview.js
+++ src/client/manager/preview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const iframeStyle = {
   width: '100%',
@@ -8,13 +8,28 @@ const iframeStyle = {
   padding: 0,
 };
 
-const Preview = ({ url }) => (
-  <iframe
-    id="storybook-preview-iframe"
-    style={iframeStyle}
-    src={url}
-  />
-);
+class Preview extends Component {
+  /* eslint-disable class-methods-use-this */
+  shouldComponentUpdate() {
+    // When the manager is re-rendered, due to changes in the layout (going full screen / changing
+    // addon panel to right) Preview section will update. If its re-rendered the whole html page
+    // inside the html is re-rendered making the story to re-mount.
+    // We dont have to re-render this component for any reason since changes are communicated to
+    // story using the channel and necessary changes are done by it.
+    return false;
+  }
+  /* eslint-enable class-methods-use-this */
+
+  render() {
+    return (
+      <iframe
+        id="storybook-preview-iframe"
+        style={iframeStyle}
+        src={this.props.url}
+      />
+    );
+  }
+}
 
 Preview.propTypes = {
   url: React.PropTypes.string,
