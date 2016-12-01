ca338a1af73bb542e902080c7d692ecc046eddbc
William Monk <will@williammonk.co.uk>
2016-12-01 02:32:32 +0000

    Update Typescript Definition File closes #632 (#634)
    This adds the `addDecorator` exported function to the definitions.

--- config/storybook.d.ts
+++ config/storybook.d.ts
@@ -9,6 +9,7 @@ interface Story {
     addDecorator (decorator: StoryDecorator): Story;
 }
 
+export function addDecorator(decorator: StoryDecorator): void;
 export function linkTo(name: string, ...params: any[]): void;
 export function storiesOf(name: string, module: any): Story;
 export function action(name: string, ...params: any[]): any;
