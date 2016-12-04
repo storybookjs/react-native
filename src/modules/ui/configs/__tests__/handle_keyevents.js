f9781aa9d0f6aec3ea3bd47367af49e3f45016f4
Kent C. Dodds <kent@doddsfamily.us>
2016-12-03 21:08:59 -0700

    Add focusInInput check to key_events `handle`
    Closes #60

--- src/modules/ui/configs/__tests__/handle_keyevents.js
+++ src/modules/ui/configs/__tests__/handle_keyevents.js
@@ -20,11 +20,71 @@ describe('manager.ui.config.handle_keyevents', () => {
       shiftKey: true,
       keyCode: keycode('F'),
       preventDefault() {},
+      target: {
+        tagName: 'DIV',
+        getAttribute() { return null; },
+      },
     };
 
     window.onkeydown(e);
 
     window.onkeydown = originalOnkeydown;
     expect(actions.shortcuts.handleEvent.callCount).to.be.equal(1);
+  });
+
+  it('should not call any actions if the event target is an input', () => {
+    const actions = {
+      shortcuts: {
+        handleEvent: sinon.mock(),
+      },
+    };
+
+    const originalOnkeydown = window.onkeydown;
+    handleKeyEvents(actions);
+
+    const e = {
+      ctrlKey: true,
+      shiftKey: true,
+      keyCode: keycode('F'),
+      preventDefault() {},
+      target: {
+        tagName: 'INPUT',
+        getAttribute() { return null; },
+      },
+    };
+
+    window.onkeydown(e);
+
+    window.onkeydown = originalOnkeydown;
+    expect(actions.shortcuts.handleEvent.callCount).to.be.equal(0);
+  });
+
+  it('should not call any actions if the event target has contenteditable enabled', () => {
+    const actions = {
+      shortcuts: {
+        handleEvent: sinon.mock(),
+      },
+    };
+
+    const originalOnkeydown = window.onkeydown;
+    handleKeyEvents(actions);
+
+    const e = {
+      ctrlKey: true,
+      shiftKey: true,
+      keyCode: keycode('F'),
+      preventDefault() {},
+      target: {
+        tagName: 'DIV',
+        getAttribute(attr) {
+          return /contenteditable/i.test(attr) ? '' : null;
+        },
+      },
+    };
+
+    window.onkeydown(e);
+
+    window.onkeydown = originalOnkeydown;
+    expect(actions.shortcuts.handleEvent.callCount).to.be.equal(0);
   });
 });
