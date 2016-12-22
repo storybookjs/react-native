90b21d7003647f40d975ac0cfa9df9a74612e501
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-12-22 15:28:37 +0530

    Use standard as the linting tool.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
-import renderer from 'react-test-renderer';
-import path from 'path';
-import readPkgUp from 'read-pkg-up';
+import renderer from 'react-test-renderer'
+import path from 'path'
+import readPkgUp from 'read-pkg-up'
 
-const { describe, it, expect } = global;
+const { describe, it, expect } = global
 
-let storybook;
-let defaultStoriesPath;
+let storybook
+let defaultStoriesPath
 
-const pkg = readPkgUp.sync().pkg;
+const pkg = readPkgUp.sync().pkg
 const isStorybook =
   (pkg.devDependencies && pkg.devDependencies['@kadira/storybook']) ||
-  (pkg.dependencies && pkg.dependencies['@kadira/storybook']);
+  (pkg.dependencies && pkg.dependencies['@kadira/storybook'])
 const isRNStorybook =
   (pkg.devDependencies && pkg.devDependencies['@kadira/react-native-storybook']) ||
-  (pkg.dependencies && pkg.dependencies['@kadira/react-native-storybook']);
+  (pkg.dependencies && pkg.dependencies['@kadira/react-native-storybook'])
 
-export default function testStorySnapshots(options = {}) {
+export default function testStorySnapshots (options = {}) {
   if (isStorybook) {
-    storybook = require.requireActual('@kadira/storybook');
-    defaultStoriesPath = path.resolve('.storybook/config.js');
+    storybook = require.requireActual('@kadira/storybook')
+    defaultStoriesPath = path.resolve('.storybook/config.js')
   } else if (isRNStorybook) {
-    storybook = require.requireActual('@kadira/react-native-storybook');
-    defaultStoriesPath = path.resolve('storybook/stories');
+    storybook = require.requireActual('@kadira/react-native-storybook')
+    defaultStoriesPath = path.resolve('storybook/stories')
   } else {
-    throw new Error('\'storyshots\' is intended only to be used with react storybook or react native storybook');
+    throw new Error('\'storyshots\' is intended only to be used with react storybook or react native storybook')
   }
 
   try {
-    require.requireActual(options.storiesPath || defaultStoriesPath);
+    require.requireActual(options.storiesPath || defaultStoriesPath)
   } catch (e) {
-    throw new Error("Could not load stories. Check 'storiesPath' option");
+    throw new Error("Could not load stories. Check 'storiesPath' option")
   }
 
   if (typeof describe !== 'function') {
-    throw new Error('\'testStorySnapshots\' is intended only to be used inside jest');
+    throw new Error('\'testStorySnapshots\' is intended only to be used inside jest')
   }
 
-  const suit = options.suit || 'Storyshots';
-  const stories = storybook.getStorybook();
+  const suit = options.suit || 'Storyshots'
+  const stories = storybook.getStorybook()
 
   for (const group of stories) {
     describe(suit, () => {
       describe(group.kind, () => {
         for (const story of group.stories) {
           it(story.name, () => {
-            const renderedStory = story.render();
-            const tree = renderer.create(renderedStory).toJSON();
-            expect(tree).toMatchSnapshot();
-          });
+            const renderedStory = story.render()
+            const tree = renderer.create(renderedStory).toJSON()
+            expect(tree).toMatchSnapshot()
+          })
         }
-      });
-    });
+      })
+    })
   }
 }
