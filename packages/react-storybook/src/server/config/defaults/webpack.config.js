23dbb241f6c78b6d95637102ef4e593141e33c00
Norbert de Langen <ndelangen@me.com>
2017-04-05 19:48:06 +0200

    FIX linting

--- packages/react-storybook/src/server/config/defaults/webpack.config.js
+++ packages/react-storybook/src/server/config/defaults/webpack.config.js
@@ -2,7 +2,7 @@ import autoprefixer from 'autoprefixer';
 import { includePaths } from '../utils';
 
 // Add a default custom config which is similar to what React Create App does.
-module.exports = (storybookBaseConfig) => {
+module.exports = storybookBaseConfig => {
   const newConfig = { ...storybookBaseConfig };
   newConfig.module.loaders = [
     ...storybookBaseConfig.module.loaders,
@@ -12,21 +12,21 @@ module.exports = (storybookBaseConfig) => {
       loaders: [
         require.resolve('style-loader'),
         `${require.resolve('css-loader')}?importLoaders=1`,
-        require.resolve('postcss-loader'),
-      ],
+        require.resolve('postcss-loader')
+      ]
     },
     {
       test: /\.json$/,
       include: includePaths,
-      loader: require.resolve('json-loader'),
+      loader: require.resolve('json-loader')
     },
     {
       test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
       include: includePaths,
       loader: require.resolve('file-loader'),
       query: {
-        name: 'static/media/[name].[hash:8].[ext]',
-      },
+        name: 'static/media/[name].[hash:8].[ext]'
+      }
     },
     {
       test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
@@ -34,28 +34,21 @@ module.exports = (storybookBaseConfig) => {
       loader: require.resolve('url-loader'),
       query: {
         limit: 10000,
-        name: 'static/media/[name].[hash:8].[ext]',
-      },
-    },
+        name: 'static/media/[name].[hash:8].[ext]'
+      }
+    }
   ];
 
-  newConfig.postcss = () => {
-    return [
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9',
-        ],
-      }),
-    ];
-  };
+  newConfig.postcss = () => [
+    autoprefixer({
+      browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
+    })
+  ];
 
   newConfig.resolve.alias = {
     ...storybookBaseConfig.resolve.alias,
     // This is to support NPM2
-    'babel-runtime/regenerator': require.resolve('babel-runtime/regenerator'),
+    'babel-runtime/regenerator': require.resolve('babel-runtime/regenerator')
   };
 
   // Return the altered config
