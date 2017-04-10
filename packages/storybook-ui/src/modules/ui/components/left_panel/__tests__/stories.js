52a1052f1fbd6eedf80004e68105a683ed48b458
Aaron Mc Adam <aaron@aaronmcadam.com>
2017-04-10 19:28:18 +0100

    fix(storybook-ui): use Jest matchers in stories

--- packages/storybook-ui/src/modules/ui/components/left_panel/__tests__/stories.js
+++ packages/storybook-ui/src/modules/ui/components/left_panel/__tests__/stories.js
@@ -1,55 +1,54 @@
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
-import sinon from 'sinon';
 import Stories from '../stories';
 
 describe('manager.ui.components.left_panel.stories', () => {
   describe('render', () => {
-    it('should render stories - empty', () => {
+    test('should render stories - empty', () => {
       const data = [];
       const wrap = shallow(<Stories stories={data} />);
+
       const list = wrap.find('div').first().children('div').last();
-      expect(list.text()).to.equal('');
+
+      expect(list.text()).toBe('');
     });
 
-    it('should render stories', () => {
+    test('should render stories', () => {
       const data = [{ kind: 'a', stories: ['a1', 'a2'] }, { kind: '20', stories: ['b1', 'b2'] }];
       const wrap = shallow(<Stories stories={data} selectedKind="20" selectedStory="b2" />);
 
       const output = wrap.html();
-      expect(output).to.match(/20/);
-      expect(output).to.match(/b2/);
+
+      expect(output).toMatch(/20/);
+      expect(output).toMatch(/b2/);
     });
   });
 
   describe('events', () => {
-    it('should call the onSelectStory prop when a kind is clicked', () => {
+    test('should call the onSelectStory prop when a kind is clicked', () => {
       const data = [{ kind: 'a', stories: ['a1', 'a2'] }, { kind: 'b', stories: ['b1', 'b2'] }];
-      const onSelectStory = sinon.spy();
-
+      const onSelectStory = jest.fn();
       const wrap = shallow(
         <Stories stories={data} selectedKind="b" selectedStory="b2" onSelectStory={onSelectStory} />
       );
 
       const kind = wrap.find('a').filterWhere(el => el.text() === 'a').last();
       kind.simulate('click');
-      expect(onSelectStory.calledOnce).to.equal(true);
-      expect(onSelectStory.firstCall.args).to.deep.equal(['a', null]);
+
+      expect(onSelectStory).toHaveBeenCalledWith('a', null);
     });
 
-    it('should call the onSelectStory prop when a story is clicked', () => {
+    test('should call the onSelectStory prop when a story is clicked', () => {
       const data = [{ kind: 'a', stories: ['a1', 'a2'] }, { kind: 'b', stories: ['b1', 'b2'] }];
-      const onSelectStory = sinon.spy();
-
+      const onSelectStory = jest.fn();
       const wrap = shallow(
         <Stories stories={data} selectedKind="b" selectedStory="b2" onSelectStory={onSelectStory} />
       );
 
       const kind = wrap.find('a').filterWhere(el => el.text() === 'b1').last();
       kind.simulate('click');
-      expect(onSelectStory.calledOnce).to.equal(true);
-      expect(onSelectStory.firstCall.args).to.deep.equal(['b', 'b1']);
+
+      expect(onSelectStory).toHaveBeenCalledWith('b', 'b1');
     });
   });
 });
