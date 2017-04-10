56aeb495d32c1b2a73165e4194ea2c6411f1d274
Aaron Mc Adam <aaron@aaronmcadam.com>
2017-04-10 18:46:27 +0100

    fix(storybook-ui): use Jest matchers in shortcuts_help

--- packages/storybook-ui/src/modules/ui/containers/__tests__/shortcuts_help.js
+++ packages/storybook-ui/src/modules/ui/containers/__tests__/shortcuts_help.js
@@ -1,13 +1,10 @@
-const { describe, it } = global;
-import { expect } from 'chai';
 import { mapper } from '../shortcuts_help';
 
 describe('manager.ui.containers.shortcuts_help', () => {
   describe('mapper', () => {
-    it('should give correct data', () => {
+    test('should give correct data', () => {
       const toggleShortcutsHelp = () => null;
       const showShortcutsHelp = true;
-
       const props = {};
       const env = {
         actions: () => ({
@@ -20,10 +17,10 @@ describe('manager.ui.containers.shortcuts_help', () => {
       const state = {
         showShortcutsHelp
       };
+      const result = mapper(state, props, env);
 
-      const data = mapper(state, props, env);
-      expect(data.isOpen).to.be.equal(showShortcutsHelp);
-      expect(data.onClose).to.be.equal(toggleShortcutsHelp);
+      expect(result.isOpen).toBe(showShortcutsHelp);
+      expect(result.onClose).toBe(toggleShortcutsHelp);
     });
   });
 });
