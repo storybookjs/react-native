41badd31ce9e05b1852a86a89661940806a8ff2a
Aaron Mc Adam <aaron@aaronmcadam.com>
2017-04-10 18:44:11 +0100

    fix(storybook-ui): use Jest mocks in init_panels

--- packages/storybook-ui/src/modules/ui/configs/__tests__/init_panels.js
+++ packages/storybook-ui/src/modules/ui/configs/__tests__/init_panels.js
@@ -1,13 +1,10 @@
 import initPanels from '../init_panels';
-import { expect } from 'chai';
-import sinon from 'sinon';
-const { describe, it } = global;
 
 describe('manager.ui.config.init_panels', () => {
-  it('should call the selectDownPanel with first panel name', () => {
+  test('should call the selectDownPanel with first panel name', () => {
     const actions = {
       ui: {
-        selectDownPanel: sinon.mock()
+        selectDownPanel: jest.fn()
       }
     };
 
@@ -23,8 +20,6 @@ describe('manager.ui.config.init_panels', () => {
 
     initPanels({ provider }, actions);
 
-    /* eslint-disable no-unused-expressions */
-    expect(actions.ui.selectDownPanel.calledWith('test1')).to.be.true;
-    /* eslint-enable no-unused-expressions */
+    expect(actions.ui.selectDownPanel).toHaveBeenCalledWith('test1');
   });
 });
