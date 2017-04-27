372c634de24adb4dbd0f836204595ccf980f9917
Norbert de Langen <ndelangen@me.com>
2017-04-27 22:35:22 +0200

    FIX unneeded require

--- packages/react-storybook/src/server/config/babel.js
+++ packages/react-storybook/src/server/config/babel.js
@@ -7,7 +7,6 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-const path = require('path');
 const findCacheDir = require('find-cache-dir');
 
 module.exports = {
