da08abd1cbdd7c02e641c49d3dd8bb8090745f3e
Michael Shilman <shilman@lab80.co>
2017-05-01 16:49:44 +1000

    Add edit-this-page link

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -4,11 +4,16 @@ import { config } from 'config'
 
 const Markdown = ({ route }) => {
   const post = route.page.data
-
+  const editUrl = `https://github.com/storybooks/storybooks.github.io/tree/source/pages${route.path.replace(/\/$/g, '.md')}`
   return (
     <DocumentTitle title={`${post.title} | ${config.siteTitle}`}>
       <div className="markdown">
         <h1>{post.title}</h1>
+        <p>
+          <a class="edit-link" href={editUrl} target="_blank">
+            Edit this page
+          </a>
+        </p>
         <div dangerouslySetInnerHTML={{ __html: post.body }} />
       </div>
     </DocumentTitle>
