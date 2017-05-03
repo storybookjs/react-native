5840f278f916b7858d71407e05feba004de59122
Michael Shilman <shilman@lab80.co>
2017-05-03 14:54:00 +1000

    Fix favicon

--- html.js
+++ html.js
@@ -28,7 +28,7 @@ class HTML extends Component {
             content="width=device-width, initial-scale=1.0"
           />
           <title>{title}</title>
-          <link rel="icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="icon" href={favicon} type="image/x-icon" />
           {css}
         </head>
         <body>
