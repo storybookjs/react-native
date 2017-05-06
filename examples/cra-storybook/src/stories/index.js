88fdf7e9fab6c40b2685a999403cbaf8850c74ec
Aaron Mc Adam <aaron@aaronmcadam.com>
2017-05-06 11:29:19 +0100

    refactor: update imports in examples

--- examples/cra-storybook/src/stories/index.js
+++ examples/cra-storybook/src/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf, action, linkTo } from '@storybook/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
 
