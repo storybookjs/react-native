663b8cac12f095a7c86a739c55747fbcbb111ee2
Norbert de Langen <ndelangen@me.com>
2017-05-11 17:54:56 +0200

    FIX linting

--- packages/addon-links/src/preview.js
+++ packages/addon-links/src/preview.js
@@ -2,11 +2,11 @@ import addons from '@kadira/storybook-addons';
 import { EVENT_ID } from './';
 
 export function linkTo(kind, story) {
-  return function (...args) {
+  return function(...args) {
     const resolvedKind = typeof kind === 'function' ? kind(...args) : kind;
     const resolvedStory = typeof story === 'function' ? story(...args) : story;
-    
+
     const channel = addons.getChannel();
-    channel.emit(EVENT_ID, {kind: resolvedKind, story: resolvedStory});
+    channel.emit(EVENT_ID, { kind: resolvedKind, story: resolvedStory });
   };
 }
