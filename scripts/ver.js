51ad5d5e8d6d98bec05a32cf850879735209b014
Norbert de Langen <ndelangen@me.com>
2017-05-16 08:16:22 +0200

    Single source of truth for LICENCE for all packages

--- scripts/ver.js
+++
@@ -1,5 +0,0 @@
-const shell = require('shelljs');
-const chalk = require('chalk');
-const packageJson = require('../package.json');
-
-shell.echo(chalk.bold(`${packageJson.name}@${packageJson.version}`));
