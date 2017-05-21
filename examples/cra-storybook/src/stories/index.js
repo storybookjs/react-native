6a41f71b0abe6a652a5c9ecccf4e4b5965cccfcc
Norbert de Langen <ndelangen@me.com>
2017-05-21 19:38:34 +0200

    FIX postcss options missing && UPDATE dependencies

--- examples/cra-storybook/src/stories/index.js
+++ examples/cra-storybook/src/stories/index.js
@@ -6,9 +6,14 @@ import { linkTo } from '@storybook/addon-links';
 
 import Button from './Button';
 import Welcome from './Welcome';
+import App from '../App';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('App', module)
+  .add('with text', () => <App />)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
