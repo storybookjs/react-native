d6dd1947309333845c3e0820c71d224f1f205df3
Michael Shilman <shilman@lab80.co>
2017-05-23 21:05:26 +1000

    Updated thumbnails, curated list, and tweaked visuals

--- components/Grid/GridItem/index.js
+++ components/Grid/GridItem/index.js
@@ -5,11 +5,14 @@ export default ({ title, description, source, demo, site, thumbnail }) => (
   <div className="grid-item">
     <div className="photobox" style={{ backgroundImage: `url(${thumbnail})` }}>
       <div className="overlay" />
-      <a target="_blank" className="demo" href={demo}>Demo</a>
     </div>
     <div className="text">
       <h2>{title}</h2>
       <p className="desc">{description}</p>
+      <div className="button-row">
+        {demo ? <a target="_blank" className="link" href={demo}>Demo</a> : null}
+        {source ? <a target="_blank" className="link" href={source}>Source</a> : null}
+      </div>
     </div>
   </div>
 );
