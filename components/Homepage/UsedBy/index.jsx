894eba33dd975c957f296070f20c6d17288a04a5
Michael Shilman <shilman@lab80.co>
2017-05-26 03:15:43 +1000

    Added squarespace

--- components/Homepage/UsedBy/index.jsx
+++ components/Homepage/UsedBy/index.jsx
@@ -4,19 +4,31 @@ import './style.css';
 
 const UsedByBg = ({ color }) => (
   <div className="used-by-bg">
-    <svg width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 1440 380" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <svg
+      width="100%"
+      height="100%"
+      preserveAspectRatio="none"
+      viewBox="0 0 1440 380"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
       <g id="Canvas" transform="translate(618 -3261)">
-        <use xlinkHref="#path0_fill" transform="translate(-618 3261.5)" fill={color} id="used-by-bg" />
+        <use
+          xlinkHref="#path0_fill"
+          transform="translate(-618 3261.5)"
+          fill={color}
+          id="used-by-bg"
+        />
       </g>
       <defs>
-        <path id="path0_fill" d="M 43.5 300.5L 0 35L 1440 0L 1371.5 379.5L 43.5 300.5Z"/>
+        <path id="path0_fill" d="M 43.5 300.5L 0 35L 1440 0L 1371.5 379.5L 43.5 300.5Z" />
       </defs>
     </svg>
   </div>
 );
 
 const User = ({ logo, demo, site, title }) => (
-  <a className="used-by-user" href={demo || site}>
+  <a className="used-by-user" href={demo || site} target="_blank" rel="nofollow">
     <img className="used-by-user-image" src={logo} alt={title} />
   </a>
 );
