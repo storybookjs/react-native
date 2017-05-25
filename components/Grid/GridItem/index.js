0e54b3ef81d770fa181ec22ddde6017067822455
Michael Shilman <shilman@lab80.co>
2017-05-26 03:22:49 +1000

    Add nofollow links to reduce SEO spam

--- components/Grid/GridItem/index.js
+++ components/Grid/GridItem/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './style.css';
 
+const linkProps = {
+  rel: 'nofollow',
+  target: '_blank',
+  className: 'link',
+};
+
 export default ({ title, description, source, demo, site, thumbnail }) => (
   <div className="grid-item">
     <div className="photobox" style={{ backgroundImage: `url(${thumbnail})` }}>
@@ -10,8 +16,8 @@ export default ({ title, description, source, demo, site, thumbnail }) => (
       <h2>{title}</h2>
       <p className="desc">{description}</p>
       <div className="button-row">
-        {demo ? <a target="_blank" className="link" href={demo}>Demo</a> : null}
-        {source ? <a target="_blank" className="link" href={source}>Source</a> : null}
+        {demo ? <a href={demo} {...linkProps}>Demo</a> : null}
+        {source ? <a href={source} {...linkProps}>Source</a> : null}
       </div>
     </div>
   </div>
