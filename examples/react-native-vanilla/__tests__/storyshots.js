8cef3d5c03f7a63fcd73b7ad91fb88b5d073c875
Tom Coleman <tom@thesnail.org>
2017-06-07 10:53:10 +1000

    Separate out testing the RN app and the rest.
    Although RN uses Jest for testing, it uses a Jest preset which messes with the environment. AFIACT it's not possible to selectively apply the environment to different tests so we just separate them.

--- examples/react-native-vanilla/__tests__/storyshots.js
+++ examples/react-native-vanilla/__tests__/storyshots.js
@@ -1,4 +1,7 @@
 import path from 'path';
 import initStoryshots, { snapshotWithOptions } from '@storybook/addon-storyshots';
 
-initStoryshots();
+initStoryshots({
+  framework: 'react-native',
+  configPath: path.join(__dirname, '..', 'storybook'),
+});
