bc84ee633dca256cd8402ad7361801663e7aedb6
Alexandre BODIN <bodin.alex@gmail.com>
2017-06-18 17:39:46 +0200

    Rename vue example to vue-kitchen-sink

---
+++ examples/vue-kitchen-sink/src/stories/index.js
@@ -0,0 +1,161 @@
+import Vuex from 'vuex';
+import { storiesOf } from '@storybook/vue';
+
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { addonNotes } from '@storybook/addon-notes';
+
+import { addonKnobs, text, number } from '@storybook/addon-knobs';
+
+import MyButton from './Button.vue';
+import Welcome from './Welcome.vue';
+import App from '../App.vue';
+
+storiesOf('Welcome', module).add('Welcome', () => ({
+  render: h => h(Welcome),
+}));
+
+storiesOf('App', module).add('App', () => ({
+  render: h => h(App),
+}));
+
+storiesOf('Button', module)
+  // Works if Vue.component is called in the config.js in .storybook
+  .add('story as a function template', () => ({
+    template: '<my-button :rounded="true">story as a function template</my-button>',
+  }))
+  .add('story as a function renderer', () => ({
+    render: h => h('div', ['story as a function renderer']),
+  }))
+  .add('story as a function component with template', () => ({
+    template: '<my-button :rounded="true">story as a function component with template</my-button>',
+  }))
+  .add('story as a function component with renderer', () => ({
+    render: h =>
+      h('my-button', { props: { rounded: true } }, ['story as a function component with renderer']),
+  }))
+  .add('with vuex', () => ({
+    components: { MyButton },
+    template: '<my-button :handle-click="log">with vuex: {{ $store.state.count }}</my-button>',
+    store: new Vuex.Store({
+      state: { count: 0 },
+      mutations: {
+        increment(state) {
+          state.count += 1; // eslint-disable-line
+          action();
+        },
+      },
+    }),
+    methods: {
+      log() {
+        this.$store.commit('increment');
+      },
+    },
+  }))
+  .add('with text', () => ({
+    // need to register local component until we can make sur Vue.componennt si called before mounting the root Vue
+    components: { MyButton },
+    template: '<my-button :handle-click="log">with text: {{ count }}</my-button>',
+    data: () => ({
+      count: 10,
+    }),
+    methods: {
+      action: action('I love vue'),
+      log() {
+        this.count += 1;
+        this.action(this.count);
+      },
+    },
+  }));
+
+storiesOf('Other', module)
+  .add('button with emoji', () => ({
+    template: '<button>😑😐😶🙄</button>',
+  }))
+  .add('p with emoji', () => ({
+    template: '<p>🤔😳😯😮</p>',
+  }))
+  .add('colorful', () => ({
+    render(h) {
+      return h(MyButton, { props: { color: 'pink' } }, ['colorful']);
+    },
+  }))
+  .add('rounded', () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="true">rounded</my-button>',
+  }))
+  .add('not rounded', () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="false" :handle-click="action">not rounded</my-button>',
+    methods: {
+      action: linkTo('Button'),
+    },
+  }));
+
+storiesOf('Addon Notes', module)
+  .add(
+    'with some emoji',
+    addonNotes({ notes: 'My notes on emojies' })(() => ({
+      template: '<p>🤔😳😯😮</p>',
+    }))
+  )
+  .add(
+    'with some button',
+    addonNotes({ notes: 'My notes on some button' })(() => ({
+      components: { MyButton },
+      template: '<my-button :rounded="true">rounded</my-button>',
+    }))
+  )
+  .add(
+    'with some color',
+    addonNotes({ notes: 'Some notes on some colored component' })(() => ({
+      render(h) {
+        return h(MyButton, { props: { color: 'pink' } }, ['colorful']);
+      },
+    }))
+  )
+  .add(
+    'with some text',
+    addonNotes({ notes: 'My notes on some text' })(() => ({
+      template: '<div>Text</div>',
+    }))
+  )
+  .add(
+    'with some long text',
+    addonNotes({ notes: 'My notes on some long text' })(() => ({
+      template: '<div>A looooooooonnnnnnnggggggggggggg text</div>',
+    }))
+  )
+  .add(
+    'with some bold text',
+    addonNotes({ notes: 'My notes on some bold text' })(() => ({
+      render: h => h('div', [h('strong', ['A very long text to display'])]),
+    }))
+  );
+
+storiesOf('Addon Knobs', module)
+  .add(
+    'With some name',
+    addonKnobs()(() => {
+      const name = text('Name', 'Arunoda Susiripala');
+      const age = number('Age', 89);
+
+      const content = `I am ${name} and I'm ${age} years old.`;
+      return {
+        template: `<div>${content}</div>`,
+      };
+    })
+  )
+  .add(
+    'With some different name',
+    addonKnobs()(() => {
+      const name = text('Name', 'Story Teller');
+      const age = number('Age', 120);
+
+      const content = `I am a ${name} and I'm ${age} years old.`;
+      return {
+        template: `<div>${content}</div>`,
+      };
+    })
+  );
--- examples/vue-kitchen-sink/webpack.config.js
+++ examples/vue-kitchen-sink/webpack.config.js
@@ -1,12 +1,12 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   module: {
     rules: [
@@ -14,57 +14,56 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-          }
+          loaders: {},
           // other vue-loader options go here
-        }
+        },
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
+      vue$: 'vue/dist/vue.esm.js',
+    },
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: true,
   },
   performance: {
-    hints: false
+    hints: false,
   },
-  devtool: '#eval-source-map'
-}
+  devtool: '#eval-source-map',
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
-      }
+        NODE_ENV: '"production"',
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+      minimize: true,
+    }),
+  ]);
 }
