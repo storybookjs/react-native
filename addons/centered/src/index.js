cf111e64651d5f65c31189a26bf1c393857cc577
kazuya kawaguchi <kawakazu80@gmail.com>
2017-08-12 03:20:53 +0900

    :up: update(addons/centered): fix mistake

--- addons/centered/src/index.js
+++ addons/centered/src/index.js
@@ -2,6 +2,6 @@ import { window } from 'global';
 import ReactCentered from './react';
 import VueCentered from './vue';
 
-const Centered = window.STORYBOOK_ENV === 'vue' ? ReactCentered : VueCentered;
+const Centered = window.STORYBOOK_ENV === 'vue' ? VueCentered : ReactCentered;
 
 export default Centered;
