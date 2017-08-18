a82f305bf4c25cf2600e9cdec0d5d66f4ec5888e
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-18 12:43:35 +0300

    Restrict import/extensions to warning in docs package

--- docs/.eslintrc.js
+++ docs/.eslintrc.js
@@ -1,4 +1,16 @@
+const warn = 1;
+
 module.exports = {
+  rules: {
+    // because of highlight.js
+    'import/extensions': [
+      warn,
+      {
+        js: 'never',
+        json: 'always',
+      },
+    ],
+  },
   settings: {
     'import/core-modules': ['config'],
   },
