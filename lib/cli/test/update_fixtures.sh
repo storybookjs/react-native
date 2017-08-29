6798614df904a395b79d4d3dcf1c34369d0979ce
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-30 02:47:30 +0300

    Skip CRNA for node 4

--- lib/cli/test/update_fixtures.sh
+++ lib/cli/test/update_fixtures.sh
@@ -4,9 +4,16 @@ set -e -x
 
 function update_fixture {
   rm -rf $2
-  ../../node_modules/.bin/$1 $2
+
+  if [ $2 == "react_native_scripts" ]
+  then
+    ../../node_modules/.bin/check-node-version --node '>=6' --quiet ||
+        (echo "create-react-native-app requires node >=6" && return 0)
+  fi
+
+  yarn create $1 $2
 }
 
 cd fixtures
-update_fixture create-react-app react_scripts
-update_fixture create-react-native-app react_native_scripts
+update_fixture react-app react_scripts
+update_fixture react-native-app react_native_scripts
