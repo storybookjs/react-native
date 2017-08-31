df6a5662e7ef8907fd6739a2647b55077a2ecb8f
Josh Callender <jcallender@lyft.com>
2017-08-31 14:54:58 -0700

    remove the local storybook and use cra-kitchen-sink for dev

--- addons/viewport/.storybook/stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-storiesOf('Viewport', module)
-  .add('Example', () => (
-    <h1>
-      Change viewport sizes below
-    </h1>
-  ));
