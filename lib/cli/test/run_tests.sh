a3e82305ccb3d208fdbe661c2b3ef7021d33667d
hypnos <Filipp.Riabchun@jetbrains.com>
2017-09-04 01:59:55 +0300

    Integrate CLI tests into tests CLI

--- lib/cli/test/run_tests.sh
+++ lib/cli/test/run_tests.sh
@@ -15,7 +15,7 @@ done
 cd ..
 
 # snapshot tests run only on node >= 6
-../../node_modules/.bin/check-node-version --node '>=6' || exit 0
+../node_modules/.bin/check-node-version --node '>=6' || exit 0
 
 update=0
 
@@ -29,13 +29,13 @@ done
 
 if [ $update -eq 1 ]
   then
-    rsync -r --exclude=**node_modules** run/ snapshots
+    rsync -r --exclude={node_modules**,yarn.lock,.DS_Store,*.md} run/ snapshots
   else
-    declare diff=`diff -rq -x **node_modules** -x .DS_Store run snapshots`
+    declare diff=`diff -rq -x node_modules** -x yarn.lock -x .DS_Store -x *.md run snapshots`
     if [[ $diff ]]
     then
       echo "$diff" 1>&2
-      echo "Snapshots don't match. Inspect your code changes or run 'yarn test:cli:update' to update them" 1>&2
+      echo "Snapshots don't match. Inspect your code changes or run 'yarn test -- --cli --update' to update them" 1>&2
       exit 1
     fi
   fi
