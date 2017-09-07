2d01afcd8da4b4778e7aff3929a8486893b60f6e
hypnos <Filipp.Riabchun@jetbrains.com>
2017-09-08 02:23:17 +0300

    Run smoke tests (disabled for now). Add docs and clarifying comments

--- lib/cli/test/run_tests.sh
+++ lib/cli/test/run_tests.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# exit on error
 set -e
 
+# copy all files from `fixtures` to `run`
 rsync -rl --delete fixtures/ run
 cd run
 
 for dir in *
 do
   cd $dir
+
+  # run @storybook/cli
   ../../../bin/index.js
+
+  # check that storybook starts without errors
+  # smoke-test option may be unknown in earlier storybook versions,
+  # so skip `already_has_storybook` here
+  if [ $dir != "already_has_storybook" ]
+  then
+    # TODO uncomment when 3.3.0 gets released
+    # yarn storybook -- --smoke-test
+  fi
+
   cd ..
 done
 
@@ -19,6 +33,7 @@ cd ..
 
 update=0
 
+# parse command-line options. `-u` turns on snapshot update mode
 while getopts ":u" opt; do
   case $opt in
     u)
@@ -29,11 +44,15 @@ done
 
 if [ $update -eq 1 ]
   then
+    # copy `run` directory contents to `snapshots`, skipping irrelevant files
     rsync -r --exclude={node_modules**,yarn.lock,.DS_Store,*.md} run/ snapshots
   else
+    # check if there is any difference between `run` and `snapshots` directories,
+    # skipping irrelevant files
     declare diff=`diff -rq -x node_modules** -x yarn.lock -x .DS_Store -x *.md run snapshots`
     if [[ $diff ]]
     then
+      # if there is some diff, output it to stderr along with a clarifying message
       echo "$diff" 1>&2
       echo "Snapshots don't match. Inspect your code changes or run 'yarn test -- --cli --update' to update them" 1>&2
       exit 1
--- lib/cli/test/update_fixtures.sh
+++ lib/cli/test/update_fixtures.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# exit on error
 set -e
 
+# update CR(N)A fixtures using latest versions of creators
 function update_fixture {
+  # remove old fixture
   rm -rfd $2
 
   if [ $2 == "react_native_scripts" ]
@@ -11,6 +14,7 @@ function update_fixture {
     ../../node_modules/.bin/check-node-version --node '>=6' || return 0
   fi
 
+  # use `npx` rather then `yarn create` to avoid installing global deps
   ../../node_modules/.bin/npx $1 $2
 }
 
