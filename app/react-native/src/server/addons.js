8003d59142f7362a772a72b73f218bd90cab001a
hypnos <Filipp.Riabchun@jetbrains.com>
2017-09-29 02:50:06 +0300

    Run `eslint --fix`

--- app/react-native/src/server/addons.js
+++ app/react-native/src/server/addons.js
@@ -2,5 +2,7 @@ import deprecate from 'util-deprecate';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 
-deprecate(() => {},
-'@storybook/react-native/addons is deprecated. See https://storybook.js.org/addons/using-addons/')();
+deprecate(
+  () => {},
+  '@storybook/react-native/addons is deprecated. See https://storybook.js.org/addons/using-addons/'
+)();
