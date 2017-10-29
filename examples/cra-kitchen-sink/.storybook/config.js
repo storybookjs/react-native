6c6d822661c346b3ab98d5e9271b07c462cc4d9e
Daniel Duan <danielduan88@yahoo.com>
2017-10-29 15:04:30 -0400

    #1750 reorganize knobs and notes

--- examples/cra-kitchen-sink/.storybook/config.js
+++ examples/cra-kitchen-sink/.storybook/config.js
@@ -22,6 +22,7 @@ setAddon(infoAddon);
 // put welcome screen at the top of the list so it's the first one displayed
 require('../src/stories/welcome');
 
+// automatically import all story js files that end with *.stories.js
 const req = require.context('../src/stories', true, /\.stories\.js$/)
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
