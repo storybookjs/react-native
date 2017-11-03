fcaf34b0e93b5e56cc687a3a85c3d707ec3539a3
tvs <tvansloten@xebia.com>
2017-11-03 14:32:47 +0100

    Renamed PlaygroundButton to playground-button for consistency

--- examples/polymer-cli/src/stories/index.js
+++ examples/polymer-cli/src/stories/index.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/polymer';
 // import { action } from '@storybook/addon-actions';
-import '../PlaygroundButton.html';
+import '../playground-button.html';
 import './storybook-welcome-to-polymer.html';
 
 storiesOf('Welcome', module).add(
@@ -8,7 +8,7 @@ storiesOf('Welcome', module).add(
   () => '<storybook-welcome-to-polymer></storybook-welcome-to-polymer>'
 );
 
-storiesOf('PlaygroundButton', module).add(
+storiesOf('playground-button', module).add(
   'default mode',
   () => '<playground-button></playground-button>'
 );
