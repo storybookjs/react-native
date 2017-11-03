bcdd36e4b43147f6b0ef2ae5631b207cb43ec777
Léon Rodenburg <l.rodenburg@instanceof.nl>
2017-11-03 10:53:02 +0100

    Use Babel for transpilation Run through webpack-dev-server

--- examples/polymer-cli/webpack.config.js
+++ examples/polymer-cli/webpack.config.js
@@ -1,20 +1,31 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/polymer-playground-app.html',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js',
+    filename: '[name].[chunkhash:8].js',
   },
   module: {
     loaders: [
       {
         test: /\.html$/,
-        use: [{ loader: 'polymer-webpack-loader' }],
+        exclude: require.resolve('./index.html'),
+        use: [{ loader: 'babel-loader' }, { loader: 'polymer-webpack-loader' }],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'node_modules/@webcomponents/webcomponentsjs/webcomponents*.js', flatten: true },
+      { from: 'node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js' },
+    ]),
+  ],
   devServer: { historyApiFallback: true },
   devtool: 'eval-source-map',
 };
