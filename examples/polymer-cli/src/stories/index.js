1d4f472f7e37de46f90b0de174e1fe8be5890b00
tvs <tvansloten@xebia.com>
2017-11-03 14:22:43 +0100

    Added Welcome story

--- examples/polymer-cli/src/stories/index.js
+++ examples/polymer-cli/src/stories/index.js
@@ -1,5 +1,11 @@
 import { storiesOf } from '@storybook/polymer';
 import '../PlaygroundButton.html';
+import './storybook-welcom-to-polymer.html';
+
+storiesOf('Welcome', module).add(
+  'Welcome',
+  () => '<storybook-welcome-to-polymer></storybook-welcome-to-polymer>'
+);
 
 storiesOf('PlaygroundButton', module).add(
   'default mode',
