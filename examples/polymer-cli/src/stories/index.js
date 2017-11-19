a16f3668603999f52e476e927e1a301d52165320
tvs <tvansloten@xebia.com>
2017-11-19 11:33:37 +0100

    Expanded polymer example stories a bit

--- examples/polymer-cli/src/stories/index.js
+++ examples/polymer-cli/src/stories/index.js
@@ -34,33 +34,64 @@ storiesOf('App', module)
   );
 
 storiesOf('Button', module)
-  .addDecorator(withKnobs)
-  .add('default', () => '<playground-button></playground-button>')
-  .add('with actions', () => {
+  .add('rounded', () => '<playground-button></playground-button>')
+  .add('square', () => '<playground-button is-square></playground-button>');
+
+storiesOf('Methods for rendering Polymer', module)
+  .add(
+    'string',
+    () => '<div><p>Rendered with string:</p><playground-button></playground-button></div>'
+  )
+  .add('document.createElement', () => {
     const el = document.createElement('playground-button');
-    el.addEventListener('click', action('Button clicked'));
-    return el;
-  })
-  .add('with knobs', () => {
-    const title = text('Button title', 'Hello');
-    const el = document.createElement('playground-button');
-    el.setAttribute('title', title);
-    button('testing', () => el.setAttribute('title', 'testing'));
+    el.setAttribute('title', 'Rendered with document.createElement');
     return el;
   });
 
-storiesOf('Addon Notes', module).add(
-  'with notes',
-  withNotes({
-    text: `
+storiesOf('Decorator', module)
+  .addDecorator(story => {
+    const el = story();
+    el.setAttribute('title', `${el.getAttribute('title')} - decorated`);
+    return el;
+  })
+  .add('example decoration', () => {
+    const el = document.createElement('playground-button');
+    el.setAttribute('title', 'An example title');
+    return el;
+  });
+
+storiesOf('Addon Actions', module)
+  .add('Action only', () => {
+    const el = document.createElement('playground-button');
+    el.addEventListener('click', action('log1'));
+    return el;
+  })
+  .add('Action and method', () => {
+    const el = document.createElement('playground-button');
+    el.addEventListener('click', e => action('log2')(e.target));
+    return el;
+  });
+
+storiesOf('Addon Notes', module)
+  .add(
+    'Simple note',
+    withNotes({ text: 'My notes on some bold text' })(
+      () =>
+        '<p><strong>Etiam vulputate elit eu venenatis eleifend. Duis nec lectus augue. Morbi egestas diam sed vulputate mollis. Fusce egestas pretium vehicula. Integer sed neque diam. Donec consectetur velit vitae enim varius, ut placerat arcu imperdiet. Praesent sed faucibus arcu. Nullam sit amet nibh a enim eleifend rhoncus. Donec pretium elementum leo at fermentum. Nulla sollicitudin, mauris quis semper tempus, sem metus tristique diam, efficitur pulvinar mi urna id urna.</strong></p>'
+    )
+  )
+  .add(
+    'Note with HTML',
+    withNotes({
+      text: `
       <h2>My notes on emojies</h2>
 
       <em>It's not all that important to be honest, but..</em>
 
       Emojis are great, I love emojis, in fact I like using them in my Component notes too! 😇
     `,
-  })(() => '<p>🤔😳😯😮<br/>😄😩😓😱<br/>🤓😑😶😊</p>')
-);
+    })(() => '<p>🤔😳😯😮<br/>😄😩😓😱<br/>🤓😑😶😊</p>')
+  );
 
 storiesOf('Addon Knobs', module)
   .addDecorator(withKnobs)
