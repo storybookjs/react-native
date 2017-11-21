58b08359447c62a4c6653fc8b41ebed1ad32f498
hypnos <Filipp.Riabchun@jetbrains.com>
2017-11-21 04:18:36 +0300

    Run prettier

--- scripts/jest.init.js
+++ scripts/jest.init.js
@@ -5,7 +5,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 // mock console.info calls for cleaner test execution
-global.console.info = jest.fn().mockImplementation(() => {})
+global.console.info = jest.fn().mockImplementation(() => {});
 
 configure({ adapter: new Adapter() });
 
