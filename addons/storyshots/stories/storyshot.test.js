b4c7098b27ed7df5e033b4131b3d702a2797ceec
Tobias Lengsholz <kontakt@lhwebsolutions.de>
2017-12-01 18:47:50 +0100

    Add enzyme-to-json for serializing, initStoryshots with custom renderer

--- addons/storyshots/stories/storyshot.test.js
+++ addons/storyshots/stories/storyshot.test.js
@@ -1,8 +1,19 @@
 import path from 'path';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import initStoryshots, { multiSnapshotWithOptions } from '../src';
 
+// with react-test-renderer
 initStoryshots({
   framework: 'react',
   configPath: path.join(__dirname, '..', '.storybook'),
   test: multiSnapshotWithOptions({}),
+});
+
+// with enzyme
+initStoryshots({
+  framework: 'react',
+  configPath: path.join(__dirname, '..', '.storybook'),
+  renderer: mount,
+  serializer: toJSON,
 });
