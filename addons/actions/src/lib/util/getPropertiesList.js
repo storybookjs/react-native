8f9858340e9b34507fe463ffa1cc3b3f821e8da1
Rob Halff <rob.halff@gmail.com>
2017-12-07 17:29:51 +0000

    remove obsolete indexOf check

--- addons/actions/src/lib/util/getPropertiesList.js
+++ addons/actions/src/lib/util/getPropertiesList.js
@@ -6,10 +6,7 @@ export default function getPropertiesList(value) {
   // eslint-disable-next-line no-restricted-syntax
   for (const name in value) {
     // noinspection JSUnfilteredForInLoop
-    if (
-      keys.indexOf(name) === -1 &&
-      !(typeof value[name] === 'function' && !hasOwnProperty.call(value, name))
-    ) {
+    if (!(typeof value[name] === 'function' && !hasOwnProperty.call(value, name))) {
       keys.push(name);
     }
   }
