08b0bdafa6a004fd4d1f75c83053e7310297769b
igor <davydkin.igor@gmail.com>
2017-12-20 08:38:30 +0200

    Remove setMeta

--- app/angular/src/client/preview/angular/utils.ts
+++ app/angular/src/client/preview/angular/utils.ts
@@ -15,17 +15,6 @@ function getMeta(component, [name1, name2]: any, defaultValue) {
   return window['Reflect'].getMetadata(name2, component) || defaultValue;
 }
 
-function setMeta(component, [name1, name2]: any, value) {
-  if (!name2) {
-    name2 = name1;
-    name1 = `__${name1}__`;
-  }
-
-  component[name1] = value;
-  component[name2] = value;
-  window['Reflect'].defineMetadata(name2, value, component);
-}
-
 export function getAnnotations(component) {
   return getMeta(component, ['annotations'], []);
 }
@@ -36,16 +25,4 @@ export function getPropMetadata(component) {
 
 export function getParameters(component) {
   return getMeta(component, ['parameters'], []);
-}
-
-export function setAnnotations(component, value) {
-  setMeta(component, ['annotations'], value);
-}
-
-export function setParameters(component, value) {
-  setMeta(component, ['parameters'], value);
-}
-
-export function setPropMetadata(component, value) {
-    setMeta(component, ['__prop__metadata__', 'propMetadata'], value);
 }
