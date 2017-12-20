fda897567e96711f9a759f76ddceb3a2f4a4198d
Tom Coleman <tom@thesnail.org>
2017-12-20 15:22:08 +1100

    Created `examples/official-storybook` with ui/components/addon stories

---
+++ examples/official-storybook/README.md
@@ -0,0 +1,7 @@
+# Official storybook
+
+This storybook includes stories for:
+
+ - `@storybook/components` - reusable UI components for addon authors
+ - `@storybook/ui` - the UI of storybook itself
+ - `@storybook/addon-*` - various addons.
--- examples/official-storybook/config.js
+++ examples/official-storybook/config.js
@@ -1,13 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-import React from 'react';
 import { configure } from '@storybook/react';
 
 function loadStories() {
   let req;
-  req = require.context('../lib/ui/src', true, /\.stories\.js$/);
+  req = require.context('../../lib/ui/src', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 
-  req = require.context('../lib/components/src', true, /\.stories\.js$/);
+  req = require.context('../../lib/components/src', true, /\.stories\.js$/);
+  req.keys().forEach(filename => req(filename));
+
+  req = require.context('./stories', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
