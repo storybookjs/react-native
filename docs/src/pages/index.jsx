89fac01903016925653a14951962e93a78e73062
hypnos <Filipp.Riabchun@jetbrains.com>
2017-12-28 02:00:42 +0300

    Docs: fix logos display

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -3,4 +3,9 @@ import { values } from 'lodash';
 import Homepage from '../components/Homepage';
 import users from './_users.yml';
 
+const usersArray = values(users);
+usersArray.forEach(user => {
+  user.logoSrc = require(`./logos/${user.logo}`);
+});
+
 export default () => <Homepage users={values(users)} />;
