fcd387a281e125a6d28beb1868d49e3b14e483ef
hypnos <Filipp.Riabchun@jetbrains.com>
2017-12-28 02:26:55 +0300

    Fix lint errors

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -3,9 +3,10 @@ import { values } from 'lodash';
 import Homepage from '../components/Homepage';
 import users from './_users.yml';
 
-const usersArray = values(users);
-usersArray.forEach(user => {
-  user.logoSrc = require(`./logos/${user.logo}`);
-});
+const usersArray = values(users).map(user => ({
+  ...user,
+  // eslint-disable-next-line import/no-dynamic-require, global-require
+  logoSrc: require(`./logos/${user.logo}`),
+}));
 
-export default () => <Homepage users={values(users)} />;
+export default () => <Homepage users={usersArray} />;
