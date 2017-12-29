2c460e4c36cdd09724671a23740cd75538a7e21b
igor <davydkin.igor@gmail.com>
2017-12-29 16:22:47 +0200

    Extract more stories to separate files

---
+++ examples/angular-cli/src/stories/app.component.stories.ts
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/angular';
+import { AppComponent } from '../app/app.component';
+
+storiesOf('App Component', module)
+  .add('Component with separate template', () => ({
+    component: AppComponent,
+    props: {}
+  }));
+
