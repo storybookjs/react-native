36daf9c514434b1ddebd3eee3f4766d253ae5365
igor <davydkin.igor@gmail.com>
2018-01-02 01:33:27 +0200

    Use config-loader in vue/loader

--- addons/storyshots/src/vue/loader.js
+++ addons/storyshots/src/vue/loader.js
@@ -1,40 +1,29 @@
-/* eslint-disable global-require,import/no-extraneous-dependencies */
-import path from 'path';
 import runWithRequireContext from '../require_context';
 import hasDependency from '../hasDependency';
+import loadConfig from '../config-loader';
 
-const babel = require('babel-core');
+function mockVueToIncludeCompiler() {
+  jest.mock('vue', () => require.requireActual('vue/dist/vue.common.js'));
+}
 
 function test(options) {
   return options.framework === 'vue' || (!options.framework && hasDependency('@storybook/vue'));
 }
 
 function load(options) {
-  // Mock vue to have compiler-included build
-  jest.mock('vue', () => {
-    const vueCommonJs = require('vue/dist/vue.common.js');
-    return vueCommonJs;
+  mockVueToIncludeCompiler();
+
+  const { content, contextOpts } = loadConfig({
+    configDirPath: options.configPath,
+    babelConfigPath: '@storybook/vue/dist/server/babel_config',
   });
-
-  const storybook = require.requireActual('@storybook/vue');
-  const loadBabelConfig = require('@storybook/vue/dist/server/babel_config').default;
-
-  const configDirPath = path.resolve(options.configPath || '.storybook');
-  const configPath = path.join(configDirPath, 'config.js');
-
-  const babelConfig = loadBabelConfig(configDirPath);
-  const content = babel.transformFileSync(configPath, babelConfig).code;
-  const contextOpts = {
-    filename: configPath,
-    dirname: configDirPath,
-  };
 
   runWithRequireContext(content, contextOpts);
 
   return {
-    renderTree: require('./renderTree').default,
     framework: 'vue',
-    storybook,
+    renderTree: require.requireActual('./renderTree').default,
+    storybook: require.requireActual('@storybook/vue'),
   };
 }
 
