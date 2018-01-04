5a24d9d705797a059a9bf0ac07687ff463728106
Thomas Bertet <thomas.bertet@mirakl.com>
2018-01-04 11:32:19 +0100

    remove unused code & fix image-snapshots script

--- examples/official-storybook/image-snapshots/jest.config.js
+++ examples/official-storybook/image-snapshots/jest.config.js
@@ -9,21 +9,3 @@ finalJestConfig.testMatch = [
 ];
 
 module.exports = finalJestConfig;
-
-//
-// module.exports = {
-//   rootDir: '../../../',
-//   testMatch: ['<rootDir>/examples/official-storybook/image-snapshots/storyshots-image.runner.js'],
-//   verbose: true,
-//   notify: true,
-//   transform: {
-//     '^.+\\.jsx?$': 'babel-jest',
-//   },
-//   clearMocks: true,
-//   moduleNameMapper: {
-//     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-//       '<rootDir>/__mocks__/fileMock.js',
-//     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-//     '\\.(md)$': '<rootDir>/__mocks__/htmlMock.js',
-//   },
-// };
