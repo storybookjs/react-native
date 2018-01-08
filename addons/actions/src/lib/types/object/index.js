cd12993f3315b3f396eb4b3688929d43136c9ea6
Rob Halff <rob.halff@gmail.com>
2018-01-08 05:30:00 +0000

    configure max depth for objects

--- addons/actions/src/lib/types/object/index.js
+++ addons/actions/src/lib/types/object/index.js
@@ -1,12 +1,13 @@
 import createNamedObject from './createNamedObject';
 import getObjectName from './getObjectName';
+import configureDepth from './configureDepth';
 
 const KEY = '$___storybook.objectName';
 
 const objectType = {
   KEY,
   // is: (value) => , // not used
-  serialize: value => ({ [KEY]: getObjectName(value) }),
+  serialize: value => configureDepth({ [KEY]: getObjectName(value) }, value),
   deserialize: value => createNamedObject(value, KEY),
 };
 
