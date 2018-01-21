3116a1da0a149fa1777148f0c3a1c2d14407ece5
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-01-21 17:09:52 +0300

    Pass default webpack config as third argument in Full Control Mode

--- app/polymer/src/server/config/defaults/webpack.config.js
+++ app/polymer/src/server/config/defaults/webpack.config.js
@@ -1,5 +1,7 @@
+import deprecate from 'util-deprecate';
 import { createDefaultWebpackConfig } from '@storybook/core/server';
-import { includePaths } from '../utils';
 
-module.exports = storybookBaseConfig =>
-  createDefaultWebpackConfig(storybookBaseConfig, includePaths);
+module.exports = deprecate(
+  createDefaultWebpackConfig,
+  "importing default webpack config generator from '@storybook/polymer/dist/server/config/defaults/webpack.config.js' is deprecated. Use third argument of your exported function instead. See https://storybook.js.org/configurations/custom-webpack-config/#full-control-mode--default"
+);
