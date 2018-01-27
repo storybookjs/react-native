a33c659ef4dc36439de808298f45bd40b091bfec
igor <davydkin.igor@gmail.com>
2018-01-27 15:42:33 +0200

    Add ts compilation to prepare.js

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 const chalk = require('chalk');
@@ -7,26 +8,54 @@ const modulePath = path.resolve('./');
 // eslint-disable-next-line import/no-dynamic-require
 const packageJson = require(path.join(modulePath, 'package.json'));
 
-shell.rm('-rf', 'dist');
-
-const babel = path.join(__dirname, '..', 'node_modules', '.bin', 'babel');
-const args = [
-  '--ignore __mocks__/,tests/*,__tests__/,**.test.js,stories/,**.story.js,**.stories.js,__snapshots__',
-  '--plugins "transform-runtime"',
-  './src --out-dir ./dist',
-  '--copy-files',
-].join(' ');
-
-const command = `${babel} ${args}`;
-const { code } = shell.exec(command, { silent: true });
-
-if (code !== 0) {
-  log.error(`FAILED: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`);
-  shell.exit(code);
+function removeDist() {
+  shell.rm('-rf', 'dist');
 }
 
-const licence = path.join(__dirname, '..', 'LICENSE');
-shell.cp(licence, './');
+function babelify() {
+  const babel = path.join(__dirname, '..', 'node_modules', '.bin', 'babel');
+  const args = [
+    '--ignore __mocks__/,tests/*,__tests__/,**.test.js,stories/,**.story.js,**.stories.js,__snapshots__',
+    '--plugins "transform-runtime"',
+    './src --out-dir ./dist',
+    '--copy-files',
+  ].join(' ');
+
+  const command = `${babel} ${args}`;
+  const { code } = shell.exec(command, { silent: true });
+
+  if (code !== 0) {
+    log.error(`FAILED: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`);
+    shell.exit(code);
+  }
+}
+
+function tscify() {
+  if (!fs.existsSync('tsconfig.json')) {
+    return;
+  }
+
+  const tsc = path.join(__dirname, '..', 'node_modules', '.bin', 'tsc');
+  const args = ['--outDir dist', '-d true'].join(' ');
+
+  const command = `${tsc} ${args}`;
+  const { code } = shell.exec(command, { silent: true });
+
+  if (code !== 0) {
+    log.error(`FAILED: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`);
+    shell.exit(code);
+  }
+}
+
+function copyLicence() {
+  const licence = path.join(__dirname, '..', 'LICENSE');
+  shell.cp(licence, './');
+}
+
+removeDist();
+babelify();
+tscify();
+copyLicence();
 
 // eslint-disable-next-line no-console
 console.log(chalk.gray(`Built: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`));
