755830d4d06993cd829d81f42986f71e47e441e6
igor <davydkin.igor@gmail.com>
2018-01-28 19:17:13 +0200

    Check for 'src' dir to prevent errors

--- scripts/compile-js.js
+++ scripts/compile-js.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-console */
+const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 
 function babelify(options = {}) {
   const { watch = false, silent = true, errorCallback } = options;
+
+  if (!fs.existsSync('src')) {
+    if (!silent) console.log('No src dir');
+    return;
+  }
 
   const babel = path.join(__dirname, '..', 'node_modules', '.bin', 'babel');
   const args = [
