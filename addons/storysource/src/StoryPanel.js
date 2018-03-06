f6965c0f1e85548a99833aefecfa2e8360339744
igor-dv <davydkin.igor@gmail.com>
2018-03-06 10:42:56 +0200

    Add ability to select stories from inside of the StoryPanel

--- addons/storysource/src/StoryPanel.js
+++ addons/storysource/src/StoryPanel.js
@@ -8,8 +8,13 @@ import { EVENT_ID } from './';
 
 registerLanguage('jsx', jsx);
 
+const keyCodeEnter = 13;
+
 const styles = {
-  selections: {
+  story: {
+    cursor: 'pointer',
+  },
+  selectedStory: {
     backgroundColor: 'rgba(255, 242, 60, 0.2)',
   },
   panel: {
@@ -18,6 +23,23 @@ const styles = {
 };
 
 export default class StoryPanel extends Component {
+  static areLocationsEqual(a, b) {
+    return (
+      a.startLoc.line === b.startLoc.line &&
+      a.startLoc.col === b.startLoc.col &&
+      a.endLoc.line === b.endLoc.line &&
+      a.endLoc.col === b.endLoc.col
+    );
+  }
+
+  static getLocationKeys(locationsMap) {
+    return locationsMap
+      ? Array.from(Object.keys(locationsMap)).sort(
+          (key1, key2) => locationsMap[key1].startLoc.line - locationsMap[key2].startLoc.line
+        )
+      : [];
+  }
+
   constructor(props) {
     super(props);
 
@@ -25,15 +47,21 @@ export default class StoryPanel extends Component {
 
     const { channel } = props;
 
-    channel.on(EVENT_ID, ({ source, location }) => {
+    channel.on(EVENT_ID, ({ source, currentLocation, locationsMap }) => {
+      const locationsKeys = StoryPanel.getLocationKeys(locationsMap);
+
       this.setState({
         source,
-        location,
+        currentLocation,
+        locationsMap,
+        locationsKeys,
       });
     });
 
     this.setSelectedStoryRef = this.setSelectedStoryRef.bind(this);
     this.lineRenderer = this.lineRenderer.bind(this);
+    this.clickOnStory = this.clickOnStory.bind(this);
+    this.keyDownOnStory = this.keyDownOnStory.bind(this);
   }
 
   componentDidUpdate() {
@@ -44,6 +72,23 @@ export default class StoryPanel extends Component {
 
   setSelectedStoryRef(ref) {
     this.selectedStoryRef = ref;
+  }
+
+  clickOnStory(key) {
+    const { api } = this.props;
+    const [kind, story] = key.split('@');
+
+    if (kind && story) {
+      api.selectStory(kind, story);
+    }
+  }
+
+  keyDownOnStory(e, key) {
+    if (e.keyCode !== keyCodeEnter) {
+      return;
+    }
+
+    this.clickOnStory(key);
   }
 
   createPart(rows, stylesheet, useInlineStyles) {
@@ -57,29 +102,74 @@ export default class StoryPanel extends Component {
     );
   }
 
-  lineRenderer({ rows, stylesheet, useInlineStyles }) {
-    const { location } = this.state;
+  createStoryPart(rows, stylesheet, useInlineStyles, location, kindStory) {
+    const { currentLocation } = this.state;
+    const first = location.startLoc.line - 1;
+    const last = location.endLoc.line;
 
-    if (location) {
-      const first = location.startLoc.line - 1;
-      const last = location.endLoc.line;
+    const storyRows = rows.slice(first, last);
+    const story = this.createPart(storyRows, stylesheet, useInlineStyles);
+    const storyKey = `${first}-${last}`;
 
-      const start = this.createPart(rows.slice(0, first), stylesheet, useInlineStyles);
-      const selected = this.createPart(rows.slice(first, last), stylesheet, useInlineStyles);
-      const end = this.createPart(rows.slice(last), stylesheet, useInlineStyles);
-
+    if (StoryPanel.areLocationsEqual(location, currentLocation)) {
       return (
-        <span>
-          {start}
-          <div ref={this.setSelectedStoryRef} style={styles.selections}>
-            {selected}
-          </div>
-          {end}
-        </span>
+        <div key={storyKey} ref={this.setSelectedStoryRef} style={styles.selectedStory}>
+          {story}
+        </div>
       );
     }
 
-    return this.createPart(rows, stylesheet, useInlineStyles);
+    return (
+      <div
+        key={storyKey}
+        onClick={() => this.clickOnStory(kindStory)}
+        onKeyDown={e => this.keyDownOnStory(e, kindStory)}
+        tabIndex="0"
+        style={styles.story}
+        role="link"
+      >
+        {story}
+      </div>
+    );
+  }
+
+  createParts(rows, stylesheet, useInlineStyles) {
+    const { locationsMap, locationsKeys } = this.state;
+
+    const parts = [];
+    let lastRow = 0;
+
+    locationsKeys.forEach(key => {
+      const location = locationsMap[key];
+      const first = location.startLoc.line - 1;
+      const last = location.endLoc.line;
+
+      const start = this.createPart(rows.slice(lastRow, first), stylesheet, useInlineStyles);
+      const storyPart = this.createStoryPart(rows, stylesheet, useInlineStyles, location, key);
+
+      parts.push(start);
+      parts.push(storyPart);
+
+      lastRow = last;
+    });
+
+    const lastPart = this.createPart(rows.slice(lastRow), stylesheet, useInlineStyles);
+
+    parts.push(lastPart);
+
+    return parts;
+  }
+
+  lineRenderer({ rows, stylesheet, useInlineStyles }) {
+    const { locationsMap, locationsKeys } = this.state;
+
+    if (!locationsMap || !locationsKeys.length) {
+      return this.createPart(rows, stylesheet, useInlineStyles);
+    }
+
+    const parts = this.createParts(rows, stylesheet, useInlineStyles);
+
+    return <span>{parts.map(part => part)}</span>;
   }
 
   render() {
@@ -98,6 +188,9 @@ export default class StoryPanel extends Component {
 }
 
 StoryPanel.propTypes = {
+  api: PropTypes.shape({
+    selectStory: PropTypes.func.isRequired,
+  }).isRequired,
   channel: PropTypes.shape({
     emit: PropTypes.func,
     on: PropTypes.func,
