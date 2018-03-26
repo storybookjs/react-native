d0a4d7daac34e9a75f80363fa78ecf3219f3ce3c
Daniel Duan <1474548+danielduan@users.noreply.github.com>
2018-03-26 15:44:58 -0400

    remove confusing drop shadow from info body sections

--- addons/info/src/components/Story.js
+++ addons/info/src/components/Story.js
@@ -58,7 +58,6 @@ const stylesheet = {
     border: '1px solid #eee',
     padding: '20px 40px 40px',
     borderRadius: '2px',
-    boxShadow: '0px 2px 3px rgba(0, 0, 0, 0.05)',
     backgroundColor: '#fff',
     marginTop: '20px',
     marginBottom: '20px',
