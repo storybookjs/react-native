112d892b6c3ea7a8ed7c389242c999ce57ffe3b7
Alexander Schrab <alexander.schrab@gmail.com>
2018-03-28 13:28:59 +0200

    Update react-native symlink resolving

--- app/react-native/src/bin/storybook-start.js
+++ app/react-native/src/bin/storybook-start.js
@@ -58,14 +58,27 @@ if (!program.skipPackager) {
     roots = roots.concat(program.root.split(',').map(root => path.resolve(root)));
   }
 
+  
   try {
-    const findSymlinksPaths = require('react-native/local-cli/util/findSymlinksPaths'); // eslint-disable-line global-require
-    symlinks = findSymlinksPaths(path.join(projectDir, 'node_modules'), [projectDir]);
+    require("babel-register")({
+      "presets": ["flow"],
+      ignore: false,
+      babelrc: false});
+
+     // eslint-disable-next-line global-require
+    const findSymlinkedModules = require('react-native/local-cli/util/findSymlinkedModules');
+    const resolveSymlinksForRoots = roots =>
+      roots.reduce(
+        (arr, rootPath) => arr.concat(
+          findSymlinkedModules(rootPath, roots)
+        ),
+        [...roots]);
+    symlinks = resolveSymlinksForRoots(roots);
   } catch (e) {
-    console.warn(`Unable to load findSymlinksPaths: ${e.message}`);
+    console.warn(`Unable to load findSymlinksPaths: ${e.message}`, e);
   }
 
-  let projectRoots = (configDir === projectDir ? [configDir] : [configDir, projectDir]).concat(
+  let projectRoots = (configDir === projectDir ? [] : [configDir]).concat(
     symlinks
   );
 
@@ -85,7 +98,6 @@ if (!program.skipPackager) {
     [
       cliCommand,
       `--projectRoots ${projectRoots.join(',')}`,
-      `--root ${roots.join(',')}`,
       program.resetCache && '--reset-cache',
       program.packagerPort && `--port=${program.packagerPort}`,
     ]
