61a1feafb25bf5e64bdd09f98132f05d2f929946
Tom Coleman <tom@thesnail.org>
2018-04-04 23:14:49 +1000

    Added stories for vue

--- examples/vue-kitchen-sink/src/stories/addon-actions.stories.js
+++ examples/vue-kitchen-sink/src/stories/addon-actions.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
+import { action, actions } from '@storybook/addon-actions';
 
 storiesOf('Addon|Actions', module)
   .add('Action only', () => ({
@@ -7,6 +7,16 @@ storiesOf('Addon|Actions', module)
     methods: {
       log: action('log1'),
     },
+  }))
+  .add('Multiple actions', () => ({
+    template:
+      '<my-button :handle-click="click" :handle-dblclick="doubleclick">Click me to log the action</my-button>',
+    methods: actions('click', 'doubleclick'),
+  }))
+  .add('Multiple actions, object', () => ({
+    template:
+      '<my-button :handle-click="click" :handle-dblclick="doubleclick">Click me to log the action</my-button>',
+    methods: actions({ click: 'clicked', doubleclick: 'double clicked' }),
   }))
   .add('Action and method', () => ({
     template: '<my-button :handle-click="log">Click me to log the action</my-button>',
