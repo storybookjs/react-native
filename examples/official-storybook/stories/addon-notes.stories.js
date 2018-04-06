fdafcbfd7efe3ba966b3bd1db3f258006cdcf7d6
Tom Coleman <tom@thesnail.org>
2018-04-06 12:27:50 +1000

    Updated addon-notes to add params based app.
    Use official-storybook stories

--- examples/official-storybook/stories/addon-notes.stories.js
+++ examples/official-storybook/stories/addon-notes.stories.js
@@ -7,22 +7,11 @@ import { action } from '@storybook/addon-actions';
 import BaseButton from '../components/BaseButton';
 import markdownNotes from './notes/notes.md';
 
-storiesOf('Addons|Notes', module)
-  .add(
-    'withNotes',
-    withNotes(
-      'This is the notes for a button. This is helpful for adding details about a story in a separate panel.'
-    )(() => <BaseButton label="Button with notes - check the notes panel for details" />)
-  )
-  .add(
-    'withNotes rendering imported markdown',
-    withNotes(markdownNotes)(() => (
-      <BaseButton label="Button with notes - check the notes panel for details" />
-    ))
-  )
-  .add(
-    'withNotes rendering inline, github-flavored markdown',
-    withMarkdownNotes(`
+const baseStory = () => (
+  <BaseButton label="Button with notes - check the notes panel for details" />
+);
+
+const markdownString = `
 # Documentation
 
 This is inline github-flavored markdown!
@@ -37,10 +26,20 @@ storiesOf('Addons|Notes', module)
     ))
   )
 ~~~
-    `)(() => (
-      <BaseButton label="Button with notes from inline github-flavored markdown - check the notes panel for details" />
-    ))
-  )
+    `;
+
+storiesOf('Addons|Notes', module)
+  .addDecorator(withNotes)
+  .add('withNotes', baseStory, {
+    notes:
+      'This is the notes for a button. This is helpful for adding details about a story in a separate panel.',
+  })
+  .add('withNotes rendering imported markdown', baseStory, { notes: { markdown: markdownNotes } })
+  .add('withNotes rendering inline, github-flavored markdown', baseStory, {
+    notes: { markdown: markdownString },
+  })
+  .add('using decorator arguments, withNotes', withNotes('Notes into withNotes')(baseStory))
+  .add('using decorator arguments, withMarkdownNotes', withMarkdownNotes(markdownString)(baseStory))
   .add('using deprecated API', () => (
     <WithNotes notes="Hello">
       <BaseButton onClick={action('clicked')} label="😀 😎 👍 💯" />
