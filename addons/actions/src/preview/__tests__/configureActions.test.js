3547919037944c2240ea1da482b0c6e1e0ad1923
Keraito <dragoon_v2_chak_shun@hotmail.com>
2018-04-20 17:19:40 +0200

    Fix action configuration test

--- addons/actions/src/preview/__tests__/configureActions.test.js
+++ addons/actions/src/preview/__tests__/configureActions.test.js
@@ -4,13 +4,16 @@ import { configureActions } from '../../';
 describe('Configure Actions', () => {
   it('can configure actions', () => {
     const depth = 100;
+    const clearActionLogger = true;
 
     configureActions({
       depth,
+      clearActionLogger,
     });
 
     expect(config).toEqual({
       depth,
+      clearActionLogger,
     });
   });
 });
