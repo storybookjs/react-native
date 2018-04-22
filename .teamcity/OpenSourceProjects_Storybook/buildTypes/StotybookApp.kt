9243827e0eb42a590b5bfe44cd72f9966e49d36b
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-04-23 00:43:15 +0300

    TeamCity: depend on HTML build

--- .teamcity/OpenSourceProjects_Storybook/buildTypes/StotybookApp.kt
+++ .teamcity/OpenSourceProjects_Storybook/buildTypes/StotybookApp.kt
@@ -14,7 +14,7 @@ enum class StorybookApp(val appName: String, val exampleDir: String, val branch:
     ANGULAR("Angular", "angular-cli"),
     POLYMER("Polymer", "polymer-cli"),
     MITHRIL("Mithril", "mithril-kitchen-sink"),
-    HTML("HTML", "html-kitchen-sink", "pull/3475");
+    HTML("HTML", "html-kitchen-sink");
 
     val lowerName = appName.toLowerCase()
 
