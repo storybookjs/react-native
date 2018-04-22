3f3300c5da8502da0a562b2636f024eb79b9c846
filipp.riabchun <filipp.riabchun@TeamCity>
2018-04-22 19:45:42 +0000

    TeamCity change in 'Open-source projects :: Storybook' project: runners of 'Examples' build configuration were updated

--- .teamcity/OpenSourceProjects_Storybook/patches/buildTypes/8cc5f747-4ca7-4f0d-940d-b0c422f501a6.kts
+++ .teamcity/OpenSourceProjects_Storybook/patches/buildTypes/8cc5f747-4ca7-4f0d-940d-b0c422f501a6.kts
@@ -1,6 +1,8 @@
 package OpenSourceProjects_Storybook.patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2017_2.ui.*
 
 /*
@@ -9,6 +11,61 @@ To apply the patch, change the buildType with uuid = '8cc5f747-4ca7-4f0d-940d-b0
 accordingly and delete the patch script.
 */
 changeBuildType("8cc5f747-4ca7-4f0d-940d-b0c422f501a6") {
+    expectSteps {
+        script {
+            name = "Bootstrap"
+            scriptContent = """
+                yarn
+                yarn bootstrap --core
+            """.trimIndent()
+            dockerImage = "node:latest"
+        }
+        script {
+            name = "official-storybook"
+            scriptContent = """
+                #!/bin/sh
+                
+                set -e -x
+                
+                cd examples/official-storybook
+                yarn build-storybook
+            """.trimIndent()
+            dockerImage = "node:latest"
+        }
+        script {
+            name = "Image storyshots"
+            scriptContent = """
+                #!/bin/sh
+                
+                set -e -x
+                
+                # Workaround for https://github.com/GoogleChrome/puppeteer/issues/290
+                
+                apt-get update
+                apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 \
+                  libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 \
+                  libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 \
+                  libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 \
+                  ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+                yarn test --image --teamcity
+            """.trimIndent()
+            dockerImage = "node:8"
+        }
+    }
+    steps {
+        update<ScriptBuildStep>(1) {
+            scriptContent = """
+                #!/bin/sh
+                
+                set -e -x
+                
+                cd examples/official-storybook
+                rm -rf storybook-static
+                yarn build-storybook
+            """.trimIndent()
+        }
+    }
+
     dependencies {
         expect("OpenSourceProjects_Storybook_CRA") {
             snapshot {
