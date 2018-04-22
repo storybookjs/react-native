1e02ad124abcefb47738e56606c80c65b0ff5384
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-04-23 01:55:03 +0300

    TeamCity: fix artifacts rules

--- .teamcity/OpenSourceProjects_Storybook/buildTypes/OpenSourceProjects_Storybook_Examples.kt
+++ .teamcity/OpenSourceProjects_Storybook/buildTypes/OpenSourceProjects_Storybook_Examples.kt
@@ -11,10 +11,11 @@ object OpenSourceProjects_Storybook_Examples : BuildType({
     id = "OpenSourceProjects_Storybook_Examples"
     name = "Examples"
 
-    artifactRules = StorybookApp.values().map { it.artifactPath }.joinToString("\n") + """
-        examples/official-storybook/storybook-static => official.zip
-        examples/official-storybook/image-snapshots/__image_snapshots__ => image-snapshots
-    """.trimIndent()
+    artifactRules = """
+${StorybookApp.values().map { it.artifactPath }.joinToString("\n")}
+examples/official-storybook/storybook-static => official.zip
+examples/official-storybook/image-snapshots/__image_snapshots__ => image-snapshots
+""".trimIndent()
 
     vcs {
         root(OpenSourceProjects_Storybook.vcsRoots.OpenSourceProjects_Storybook_HttpsGithubComStorybooksStorybookRefsHeadsMaster)
