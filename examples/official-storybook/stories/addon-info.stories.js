d96a2565df7951afe6da68d76ee0a5b5e6afdd15
Andy Orahoske <orahoske.2@gmail.com>
2018-04-21 21:43:06 -0400

    Add example story for excludedPropTypes

--- examples/official-storybook/stories/addon-info.stories.js
+++ examples/official-storybook/stories/addon-info.stories.js
@@ -59,6 +59,14 @@ storiesOf('Addons|Info.Options.inline', module).add(
   })(() => <BaseButton label="Button" />)
 );
 
+storiesOf('Addons|Info.Options.excludedPropTypes').add(
+  'Excludes propTypes that are in the excludedPropTypes array',
+  withInfo({
+    text: 'Label propType should be excluded',
+    excludedPropTypes: ['label']
+  })(() => <BaseButton />)
+);
+
 storiesOf('Addons|Info.Options.header', module).add(
   'Shows or hides Info Addon header',
   withInfo({
