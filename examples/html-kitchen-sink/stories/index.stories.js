17bf3be26d5b29ac4c7db4949853b55eba66108e
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-04-24 01:36:53 +0300

    Introduce subscriptionsStore

--- examples/html-kitchen-sink/stories/index.stories.js
+++ examples/html-kitchen-sink/stories/index.stories.js
@@ -2,19 +2,31 @@ import { document } from 'global';
 import { storiesOf } from '@storybook/html';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { subscriptionsStore } from '@storybook/core/client';
 
 import './welcome.css';
 import welcome from './welcome.html';
 
-document.addEventListener('click', e => {
-  const { kind, story } = e.target.dataset;
-  if (kind || story) {
-    e.preventDefault();
-    linkTo(kind, story)();
-  }
-});
+// TODO move to addon-links
+const linkSubscribtion = () => {
+  const listener = e => {
+    const { sbKind, sbStory } = e.target.dataset;
+    if (sbKind || sbStory) {
+      e.preventDefault();
+      linkTo(sbKind, sbStory)();
+    }
+  };
 
-storiesOf('Welcome', module).add('Welcome', () => welcome);
+  document.addEventListener('click', listener);
+  return () => document.removeEventListener('click', listener);
+};
+
+storiesOf('Welcome', module)
+  .addDecorator(story => {
+    subscriptionsStore.register(linkSubscribtion);
+    return story();
+  })
+  .add('Welcome', () => welcome);
 
 storiesOf('Demo', module)
   .add('heading', () => '<h1>Hello World</h1>')
