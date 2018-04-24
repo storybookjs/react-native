c24e5ee198a2830008364e0806c5ffc7f66bef67
igor-dv <davydkin.igor@gmail.com>
2018-04-24 15:18:27 +0300

    Add Notes addon

---
+++ examples/html-kitchen-sink/stories/addon-notes.stories.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/html';
+import { withNotes } from '@storybook/addon-notes';
+
+storiesOf('Addons|Notes', module)
+  .addDecorator(withNotes)
+  .add(
+    'Simple note',
+    () =>
+      `
+      <p>
+        <strong>
+          This is a fragment of HTML
+        </strong>
+      </p>
+      `,
+    {
+      notes: 'My notes on some bold text',
+    }
+  );
