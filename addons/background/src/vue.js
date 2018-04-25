b3a703c527823e321260e68d81aa816f0f6ed8a4
Wei-Wei Wu <wuxx1045@umn.edu>
2018-04-25 00:19:16 -0700

    vue background

--- addons/background/src/vue.js
+++ addons/background/src/vue.js
@@ -1,2 +1,28 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Vue from 'vue';
+import addons from '@storybook/addons';
+
+const vueHandler = (channel, backgrounds) => (getStory, context) => ({
+  data() {
+    return {
+      context,
+      getStory,
+      story: getStory(context),
+    };
+  },
+
+  render(h) {
+    return h(this.story);
+  },
+
+  created() {
+    channel.emit('background-set', backgrounds);
+  },
+
+  beforeDestroy() {
+    channel.emit('background-unset');
+  },
+});
+
+export default function makeDecorator(backgrounds) {
+  const channel = addons.getChannel();
+  return vueHandler(channel, backgrounds);
+}
