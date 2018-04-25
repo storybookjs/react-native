ea48baeda1f4f3fb524a26da899c25c5548ec21e
igor-dv <davydkin.igor@gmail.com>
2018-04-25 17:08:09 +0300

    Update knobs example

--- examples/html-kitchen-sink/stories/addon-knobs.stories.js
+++ examples/html-kitchen-sink/stories/addon-knobs.stories.js
@@ -1,15 +1,11 @@
 import { storiesOf } from '@storybook/html';
-import { document } from 'global';
-import { withKnobs, text } from '@storybook/addon-knobs/html';
+import { withKnobs, text, number } from '@storybook/addon-knobs/html';
 
 storiesOf('Addons|Knobs', module)
   .addDecorator(withKnobs)
   .add('simple', () => {
-    const title = text('Button title', 'Hello');
+    const title = text('Title', 'Hello');
+    const num = number('Number', 1);
 
-    const element = document.createElement('div');
-
-    element.innerHTML = title;
-
-    return element;
+    return `<div>${title} ${num}</div>`;
   });
