4ca019ac020f18bf8f41a4959e04caf32a4f9a10
klimentru1986 <kliment1986@inbox.ru>
2018-05-06 02:17:16 +0300

    lint

--- examples/angular-cli/jest-config/globalMocks.ts
+++ examples/angular-cli/jest-config/globalMocks.ts
@@ -1,20 +1,18 @@
 const mock = () => {
-    let storage = {};
-    return {
-      getItem: key => (key in storage ? storage[key] : null),
-      setItem: (key, value) => (storage[key] = value || ''),
-      removeItem: key => delete storage[key],
-      clear: () => (storage = {})
-    };
+  let storage = {};
+  return {
+    getItem: key => (key in storage ? storage[key] : null),
+    setItem: (key, value) => (storage[key] = value || ''),
+    removeItem: key => delete storage[key],
+    clear: () => (storage = {}),
   };
-  
-  Object.defineProperty(window, 'localStorage', {
-    value: mock()
-  });
-  Object.defineProperty(window, 'sessionStorage', {
-    value: mock()
-  });
-  Object.defineProperty(window, 'getComputedStyle', {
-    value: () => ['-webkit-appearance']
-  });
-  
+};
+Object.defineProperty(window, 'localStorage', {
+  value: mock(),
+});
+Object.defineProperty(window, 'sessionStorage', {
+  value: mock(),
+});
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ['-webkit-appearance'],
+});
