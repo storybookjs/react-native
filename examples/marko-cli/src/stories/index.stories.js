24bdcfcd3c7300510d0f09f7f369e9983398922a
Neville Mehta <nevmehta@ebay.com>
2018-05-05 15:00:05 -0700

    remove No Preview story

--- examples/marko-cli/src/stories/index.stories.js
+++ examples/marko-cli/src/stories/index.stories.js
@@ -9,7 +9,6 @@ storiesOf('Welcome', module).add('welcome', () => Welcome.renderSync({}));
 
 storiesOf('Hello', module)
   .add('Simple', () => Hello.renderSync({ name: 'abc', age: 20 }))
-  .add('with No Preview!')
   .add('with ERROR!', () => 'NOT A MARKO RENDER_RESULT');
 
 storiesOf('ClickCount', module).add('Simple', () => ClickCount.renderSync({}));
