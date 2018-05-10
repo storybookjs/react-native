974b0ad4f3cc14c79ebcdcf674ea0a861919bb03
klimentru1986 <kliment1986@inbox.ru>
2018-05-11 00:11:07 +0300

    added centered for angular

--- addons/centered/src/index.js
+++ addons/centered/src/index.js
@@ -1,7 +1,9 @@
 import { window } from 'global';
 import ReactCentered from './react';
 import VueCentered from './vue';
+import AngularCentered from './angular';
 
 const Centered = window.STORYBOOK_ENV === 'vue' ? VueCentered : ReactCentered;
-
 export default Centered;
+
+export const centeredAng = AngularCentered;
