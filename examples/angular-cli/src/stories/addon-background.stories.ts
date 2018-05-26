7d576ae844a85c2b70e9ffbebc964423963ae009
klimentru1986 <kliment1986@inbox.ru>
2018-05-26 12:13:04 +0300

    background example updated

--- examples/angular-cli/src/stories/addon-background.stories.ts
+++ examples/angular-cli/src/stories/addon-background.stories.ts
@@ -1,11 +1,31 @@
 import { moduleMetadata, storiesOf } from '@storybook/angular';
 import { Button } from '@storybook/angular/demo';
+import backgrounds from '@storybook/addon-backgrounds';
+import { AppComponent } from '../app/app.component';
+
+storiesOf('Addon|Background', module)
+  .addDecorator(
+    backgrounds([
+      { name: 'twitter', value: '#00aced', default: true },
+      { name: 'facebook', value: '#3b5998' },
+    ])
+  )
+  .add('background component', () => ({
+    component: AppComponent,
+    props: {},
+  }));
 
 storiesOf('Addon|Background', module)
   .addDecorator(
     moduleMetadata({
       declarations: [Button],
     })
+  )
+  .addDecorator(
+    backgrounds([
+      { name: 'twitter', value: '#00aced', default: true },
+      { name: 'facebook', value: '#3b5998' },
+    ])
   )
   .add('background template', () => ({
     template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
