d2faeb720f2ca9dc10dff8dba8e229b7fbe3004c
Norbert de Langen <ndelangen@me.com>
2018-06-02 00:08:04 +0200

    REFACTOR knobs field to use field component

--- addons/knobs/src/components/PropField.js
+++ addons/knobs/src/components/PropField.js
@@ -1,32 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'react-emotion';
 
+import { Field } from '@storybook/components';
 import TypeMap from './types';
 
 const InvalidType = () => <span>Invalid Type</span>;
 
-const Field = styled('div')({
-  display: 'table-row',
-  padding: '5px',
-});
-const Label = styled('label')({
-  display: 'table-cell',
-  boxSizing: 'border-box',
-  verticalAlign: 'top',
-  paddingRight: 5,
-  paddingTop: 5,
-  textAlign: 'right',
-  width: 80,
-  fontSize: 12,
-  fontWeight: 600,
-});
-
 export default function PropField({ onChange, onClick, knob }) {
   const InputType = TypeMap[knob.type] || InvalidType;
   return (
-    <Field>
-      <Label htmlFor={knob.name}>{!knob.hideLabel && `${knob.name}`}</Label>
+    <Field label={!knob.hideLabel && `${knob.name}`}>
       <InputType knob={knob} onChange={onChange} onClick={onClick} />
     </Field>
   );
