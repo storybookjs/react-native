f877ac9a08d082e59df028487d8e5b4cf22b2005
Norbert de Langen <ndelangen@me.com>
2018-06-02 00:12:57 +0200

    theme knobs date

--- addons/knobs/src/components/types/Date/index.js
+++ addons/knobs/src/components/types/Date/index.js
@@ -1,25 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'react-emotion';
+
 import Datetime from 'react-datetime';
-import insertCss from 'insert-css';
+// import { Input } from '@storybook/components';
+
 import style from './styles';
 
-const customStyle = `
-  .rdt input {
-    outline: 0;
-    width: 100%;
-    border: 1px solid #f7f4f4;
-    border-radius: 2px;
-    font-size: 11px;
-    padding: 5px;
-    color: #555;
-    display: table-cell;
-    box-sizing: border-box;
-  }
-`;
-
-insertCss(style);
-insertCss(customStyle);
+const DateInput = styled(Datetime)(style);
 
 class DateType extends React.Component {
   static getDerivedStateFromProps(props, state) {
@@ -37,33 +25,26 @@ class DateType extends React.Component {
   };
 
   render() {
-    const { knob } = this.props;
     const { value } = this.state;
 
     return (
-      <div>
-        <Datetime
-          id={knob.name}
-          value={value ? new Date(value) : null}
-          type="date"
-          onChange={this.handleChange}
-        />
-      </div>
+      <DateInput
+        value={value ? new Date(value) : null}
+        type="date"
+        onChange={this.handleChange}
+        size="flex"
+      />
     );
   }
 }
 
-DateType.defaultProps = {
-  knob: {},
-  onChange: value => value,
-};
-
 DateType.propTypes = {
+  // eslint-disable-next-line react/no-unused-prop-types
   knob: PropTypes.shape({
     name: PropTypes.string,
     value: PropTypes.number,
-  }),
-  onChange: PropTypes.func,
+  }).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 DateType.serialize = value => String(value);
