cb988d0b39d0117595ea22eef5ca6b96a0ee01ef
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-06-11 01:22:21 +0300

    Move lib/core to babel@7

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-stage-0', '@babel/preset-react'],
+  presets: [
+    ['@babel/preset-env'],
+    ['@babel/preset-stage-0', { decoratorsLegacy: true }],
+    '@babel/preset-react',
+  ],
   plugins: [
-    'emotion',
+    'babel-plugin-emotion',
     'babel-plugin-macros',
     [
       '@babel/plugin-transform-runtime',
