52ef4c4fd302ab6db62995e41d8a043f4f76def2
igor-dv <davydkin.igor@gmail.com>
2018-06-10 20:05:52 +0300

    Import framework loaders dynamically

--- addons/storyshots/storyshots-core/src/frameworks/frameworkLoader.js
+++ addons/storyshots/storyshots-core/src/frameworks/frameworkLoader.js
@@ -1,12 +1,24 @@
-import loaderReact from './react/loader';
-import loaderRn from './rn/loader';
-import loaderAngular from './angular/loader';
-import loaderVue from './vue/loader';
-import loaderHTML from './html/loader';
+/* eslint-disable global-require,import/no-dynamic-require */
+import fs from 'fs';
+import path from 'path';
 
-const loaders = [loaderReact, loaderAngular, loaderRn, loaderVue, loaderHTML];
+const loaderScriptName = 'loader.js';
+
+const isDirectory = source => fs.lstatSync(source).isDirectory();
+
+function getLoaders() {
+  return fs
+    .readdirSync(__dirname)
+    .map(name => path.join(__dirname, name))
+    .filter(isDirectory)
+    .map(framework => path.join(framework, loaderScriptName))
+    .filter(fs.existsSync)
+    .map(loader => require(loader).default);
+}
 
 function loadFramework(options) {
+  const loaders = getLoaders();
+
   const loader = loaders.find(frameworkLoader => frameworkLoader.test(options));
 
   if (!loader) {
