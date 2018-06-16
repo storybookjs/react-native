261108bf961e083ce7696f006ff3a158816c6ada
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-06-16 15:36:36 +0300

    TeamCity: add Hyperapp build

--- .teamcity/OpenSourceProjects_Storybook/buildTypes/StotybookApp.kt
+++ .teamcity/OpenSourceProjects_Storybook/buildTypes/StotybookApp.kt
@@ -14,7 +14,7 @@ enum class StorybookApp(val appName: String, val exampleDir: String, val merged:
     MITHRIL("Mithril", "mithril-kitchen-sink"),
     HTML("HTML", "html-kitchen-sink"),
     MARKO("Marko", "marko-cli"),
-    MARKO("Hyperapp", "hyperapp-kitchen-sink", false);
+    HYPERAPP("Hyperapp", "hyperapp-kitchen-sink", false);
 
     val lowerName = appName.toLowerCase()
 
