ce09ea7b0c789c11de650022520bb4442ad1ff35
Gavin King <gavin.king@itslearning.com>
2018-06-17 16:46:56 +0200

    No longer using this file for svelte, refactor not required for review

--- addons/centered/src/index.js
+++ addons/centered/src/index.js
@@ -2,15 +2,5 @@ import { window } from 'global';
 import ReactCentered from './react';
 import VueCentered from './vue';
 
-function getCentered(env) {
-  switch (env) {
-    case 'vue':
-      return VueCentered;
-    default:
-      return ReactCentered;
-  }
-}
-
-const Centered = getCentered(window.STORYBOOK_ENV);
-
+const Centered = window.STORYBOOK_ENV === 'vue' ? VueCentered : ReactCentered;
 export default Centered;
