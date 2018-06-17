40660ea7ea2c820000e1accb8842b71144efc0f6
Gavin King <gavin.king@itslearning.com>
2018-06-17 04:26:25 +0200

    Added knob addon example

---
+++ examples/svelte-kitchen-sink/src/stories/views/KnobView.svelte
@@ -0,0 +1,14 @@
+<div style="width: {width}px; background-color: {backgroundColor}">
+    <p>I am interactive</p>
+</div>
+
+<script>
+    export default {
+        data() {
+            return {
+                width: 200,
+                backgroundColor: 'yellow'
+            };
+        }
+    };
+</script>
