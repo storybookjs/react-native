1035614ced7384d446be36197fef1d5b611280d6
Gavin King <gavin.king@itslearning.com>
2018-06-17 19:45:34 +0200

    Unnecessary code

--- app/svelte/src/client/preview/index.js
+++ app/svelte/src/client/preview/index.js
@@ -3,15 +3,7 @@ import { start } from '@storybook/core/client';
 import './globals';
 import render from './render';
 
-// const createWrapperComponent = Target => ({
-//   functional: true,
-//   render(h, c) {
-//     return h(Target, c.data, c.children);
-//   },
-// });
-
 const { clientApi, configApi, forceReRender } = start(render);
-// const { clientApi, configApi, forceReRender } = start(render, { decorateStory });
 
 export const {
   storiesOf,
