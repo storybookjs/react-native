b4547fd298d0f93d1f81cd5d53bf2fc22dcc307e
Sebastian Werner <s.werner@sebastian-software.de>
2018-07-02 15:47:30 +0200

    Reworked placement of env setup according to feedback in #3744.

--- app/angular/src/server/build.js
+++ app/angular/src/server/build.js
@@ -1,9 +1,4 @@
-// Building for distribution defaults to production target.
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+import { buildStatic } from '@storybook/core/server';
+import options from './options';
 
-// Note: We are importing via CommonJS to be able to adjust NODE_ENV
-// to production first before proceeding. This is required to correctly
-// e.g. selecting the `.env` files to import. It also helps with all ESM
-// imports which as some side-effect decide on things at "loadtime".
-const server = require('@storybook/core/server');
-server.buildStatic(require('./options').default);
+buildStatic(options);
--- app/polymer/bin/build.js
+++ app/polymer/bin/build.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 require('../dist/server/build');
