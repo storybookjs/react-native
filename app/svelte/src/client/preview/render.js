dd1e609f788b2b5a22ac96a79cde895c79708749
Gavin King <gavin.king@itslearning.com>
2018-07-24 16:50:11 +0200

    Componnents must be destroyed when mounting a new story to clean up any lingering internal event handlers

--- app/svelte/src/client/preview/render.js
+++ app/svelte/src/client/preview/render.js
@@ -1,18 +1,15 @@
 import { document } from 'global';
 import { stripIndents } from 'common-tags';
 
-let listeners = [];
+let previousComponent = null;
 
-/**
- * Since we may have added events using `.on()` we need to ensure that these
- * events are cleaned up when changing between stories.
- *
- * @see mountView()
- */
-function cancelPreviousStoryListeners() {
-  listeners.forEach(listener => listener.cancel());
+function cleanUpPreviousStory() {
+  if (!previousComponent) {
+    return;
+  }
 
-  listeners = [];
+  previousComponent.destroy();
+  previousComponent = null;
 }
 
 function mountView({ Component, target, data, on }) {
@@ -20,13 +17,12 @@ function mountView({ Component, target, data, on }) {
 
   if (on) {
     // Attach svelte event listeners.
-    listeners = Object.keys(on).map(eventName => {
-      const listener = component.on(eventName, on[eventName]);
-
-      // We need this so we can call `cancel()` on it when the next story is rendered.
-      return listener;
+    Object.keys(on).forEach(eventName => {
+      component.on(eventName, on[eventName]);
     });
   }
+
+  previousComponent = component;
 }
 
 export default function render({
@@ -46,11 +42,7 @@ export default function render({
     on,
   } = story();
 
-  const target = document.getElementById('root');
-
-  target.innerHTML = '';
-
-  cancelPreviousStoryListeners();
+  cleanUpPreviousStory();
 
   if (!Component) {
     showError({
@@ -64,6 +56,10 @@ export default function render({
 
     return;
   }
+
+  const target = document.getElementById('root');
+
+  target.innerHTML = '';
 
   mountView({ Component, target, data, on });
 
