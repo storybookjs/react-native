205582a09f19b7e7f96976a3275698e536678c65
Ka-wai Wong <wong.kwd@gmail.com>
2018-07-24 19:09:31 -0500

    Add additional device options to addon-viewport

--- addons/viewport/src/shared/index.js
+++ addons/viewport/src/shared/index.js
@@ -40,11 +40,41 @@ export const INITIAL_VIEWPORTS = {
     },
     type: 'mobile',
   },
+  iphone8p: {
+    name: 'iPhone 7 Plus',
+    styles: {
+      height: '960px',
+      width: '540px',
+    },
+  },
+  iphonex: {
+    name: 'iPhone X',
+    styles: {
+      height: '1218px',
+      width: '563px',
+    },
+  },
   ipad: {
     name: 'iPad',
     styles: {
       height: '1024px',
       width: '768px',
+    },
+    type: 'tablet',
+  },
+  ipad10p: {
+    name: 'iPad Pro 10.5-in',
+    styles: {
+      height: '1112px',
+      width: '834px',
+    },
+    type: 'tablet',
+  },
+  ipad12p: {
+    name: 'iPad Pro 12.9-in',
+    styles: {
+      height: '1366px',
+      width: '1024px',
     },
     type: 'tablet',
   },
@@ -55,6 +85,13 @@ export const INITIAL_VIEWPORTS = {
       width: '360px',
     },
     type: 'mobile',
+  },
+  galaxys9: {
+    name: 'Galaxy S9',
+    styles: {
+      height: '1480px',
+      width: '720px',
+    },
   },
   nexus5x: {
     name: 'Nexus 5X',
@@ -71,6 +108,20 @@ export const INITIAL_VIEWPORTS = {
       width: '412px',
     },
     type: 'mobile',
+  },
+  pixel: {
+    name: 'Pixel',
+    styles: {
+      height: '960px',
+      width: '540px',
+    },
+  },
+  pixelxl: {
+    name: 'Pixel XL',
+    styles: {
+      height: '1280px',
+      width: '720px',
+    },
   },
 };
 export const DEFAULT_VIEWPORT = 'responsive';
