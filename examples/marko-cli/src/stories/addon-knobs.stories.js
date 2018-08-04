45d5b965d560f9805cead68af4d8d6fc8ddafc4b
Neville Mehta <nevmehta@ebay.com>
2018-08-04 14:53:52 -0700

    make options add-on accept params

--- examples/marko-cli/src/stories/addon-knobs.stories.js
+++ examples/marko-cli/src/stories/addon-knobs.stories.js
@@ -1,11 +1,14 @@
 import { storiesOf } from '@storybook/marko';
 import { withKnobs, boolean, text, number } from '@storybook/addon-knobs';
+import { withOptions } from '@storybook/addon-options';
 import Hello from '../components/hello/index.marko';
 import MarkoWidgetHello from '../components/marko-widgets/hello';
 import MarkoWidgetButton from '../components/marko-widgets/button';
 
 storiesOf('Addons|Knobs/Hello', module)
+  .addDecorator(withOptions)
   .addDecorator(withKnobs)
+  .addParameters({ options: { addonPanelInRight: true } })
   .add('Simple', () => {
     const name = text('Name', 'John Doe');
     const age = number('Age', 44);
@@ -16,7 +19,9 @@ storiesOf('Addons|Knobs/Hello', module)
   });
 
 storiesOf('Addons|Knobs/Hello (Marko Widget)', module)
+  .addDecorator(withOptions)
   .addDecorator(withKnobs)
+  .addParameters({ options: { addonPanelInRight: false } })
   .add('Simple', () => {
     const name = text('Name', 'John Doe');
     return MarkoWidgetHello.renderSync({
