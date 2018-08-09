deef786c2b50b2aa1240b5c28bcc8e201a7d3c3b
Tom Coleman <tom@thesnail.org>
2018-08-09 12:27:54 +1000

    Skip error stories in storyshots

--- examples/official-storybook/stories/core.stories.js
+++ examples/official-storybook/stories/core.stories.js
@@ -3,6 +3,7 @@ import { storiesOf, addParameters } from '@storybook/react';
 import addons from '@storybook/addons';
 import Events from '@storybook/core-events';
 import { Button } from '@storybook/components';
+import { navigator } from 'global';
 
 const globalParameter = 'globalParameter';
 const chapterParameter = 'chapterParameter';
@@ -32,9 +33,13 @@ storiesOf('Core|Events', module).add('Force re-render', () => (
   <Button onClick={increment}>Clicked: {timesClicked}</Button>
 ));
 
-storiesOf('Core|Errors', module)
-  .add('story throws exception', () => {
-    throw new Error('error');
-  })
-  // Story does not return something react can render
-  .add('story errors', () => null);
+// Skip these stories in storyshots, they will throw -- NOTE: would rather do this
+// via a params API, see https://github.com/storybooks/storybook/pull/3967#issuecomment-411616023
+if (navigator && navigator.userAgent && !(navigator.userAgent.indexOf('jsdom') > -1)) {
+  storiesOf('Core|Errors', module)
+    .add('story throws exception', () => {
+      throw new Error('error');
+    })
+    // Story does not return something react can render
+    .add('story errors', () => null);
+}
