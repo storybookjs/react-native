d9c57d2fd7b047ea1190d7f843c7e463d6f46d7b
Norbert de Langen <ndelangen@me.com>
2018-08-10 15:59:17 +0200

    REFACTOR WIP

--- examples/official-storybook/stories/addon-graphql.stories.js
+++ examples/official-storybook/stories/addon-graphql.stories.js
@@ -1,15 +1,15 @@
-import { storiesOf } from '@storybook/react';
-import { setupGraphiQL } from '@storybook/addon-graphql';
+// import { storiesOf } from '@storybook/react';
+// import { setupGraphiQL } from '@storybook/addon-graphql';
 
 // setup the graphiql helper which can be used with the add method later
-const graphiql = setupGraphiQL({ url: 'http://localhost:3000/graphql' });
+// const graphiql = setupGraphiQL({ url: 'http://localhost:3000/graphql' });
 
-// run yarn graphql in examples/official-storybook to start graphql server
-storiesOf('Addons|GraphQL', module).add(
-  'get user info',
-  graphiql(`{
-    user(id: "1") {
-      name
-    }
-  }`)
-);
+// // run yarn graphql in examples/official-storybook to start graphql server
+// storiesOf('Addons|GraphQL', module).add(
+//   'get user info',
+//   graphiql(`{
+//     user(id: "1") {
+//       name
+//     }
+//   }`)
+// );
