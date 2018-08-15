850b08391ccaba5bb4ed3c8e89dcbd5342828540
Norbert de Langen <ndelangen@me.com>
2018-08-15 11:51:16 +0200

    FIX angular example with a hack?

--- examples/angular-cli/.storybook/config.ts
+++ examples/angular-cli/.storybook/config.ts
@@ -2,18 +2,23 @@ import { configure } from '@storybook/angular';
 import { setOptions } from '@storybook/addon-options';
 import addCssWarning from '../src/cssWarning';
 
+const path =require('path');
+const rc = require('require-context');
+
 addCssWarning();
 
 setOptions({
   hierarchyRootSeparator: /\|/,
 });
 
+const pathToStories = path.join(__dirname, '../src/stories');
+
 function loadStories() {
   // put welcome screen at the top of the list so it's the first one displayed
   require('../src/stories');
 
   // automatically import all story ts files that end with *.stories.ts
-  const req = require.context('../src/stories', true, /\.stories\.ts$/);
+  const req = rc(pathToStories, true, /\.stories\.ts$/);
   req.keys().forEach(filename => req(filename));
 }
 
--- examples/angular-cli/jest-config/setup.ts
+++ examples/angular-cli/jest-config/setup.ts
@@ -1,6 +1,2 @@
 import 'jest-preset-angular';
 import './globalMocks';
-
-import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
-
-registerRequireContextHook();
