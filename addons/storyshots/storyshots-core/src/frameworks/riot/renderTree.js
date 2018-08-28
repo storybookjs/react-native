19f565080c88670e28680a1343389e5b46f653f1
lionel benychou <lbenychou@egencia.com>
2018-08-28 11:21:45 +0200

    storyshots

--- addons/storyshots/storyshots-core/src/frameworks/riot/renderTree.js
+++ addons/storyshots/storyshots-core/src/frameworks/riot/renderTree.js
@@ -1,5 +1,9 @@
 import { document } from 'global';
 
+document.body = document.createElement('body');
+// eslint-disable-next-line no-unused-vars
+const rootElement = document.body.appendChild(document.createElement('root'));
+
 function getRenderedTree(story, context) {
   story.render(context);
 
