be5d4c01817a473ca172ab071f2fc28a41bede8e
Gabriel J. Csapo <gcsapo@linkedin.biz>
2018-09-30 15:06:09 -0700

    [framework] add ember support

---
+++ examples/ember-cli/stories/addon-knobs.stories.js
@@ -0,0 +1,29 @@
+import hbs from 'htmlbars-inline-precompile';
+import { storiesOf } from '@storybook/ember';
+import { withKnobs, text, color } from '@storybook/addon-knobs';
+
+storiesOf('Addon|Knobs', module)
+  .addDecorator(withKnobs)
+  .addParameters({ options: { selectedAddonPanel: 'storybooks/storybook-addon-knobs' } })
+  .add('with text', () => ({
+    template: hbs`
+      {{welcome-banner
+        backgroundColor=backgroundColor
+        titleColor=titleColor
+        subTitleColor=subTitleColor
+        title=title
+        subtitle=subtitle
+        click=(action onClick)
+      }}
+    `,
+    context: {
+      backgroundColor: color('backgroundColor', '#FDF4E7'),
+      titleColor: color('titleColor', '#DF4D37'),
+      subTitleColor: color('subTitleColor', '#B8854F'),
+      title: text('title', 'Welcome to storybook'),
+      subtitle: text('subtitle', 'This environment is completely editable'),
+      onClick: e => {
+        console.log(e);
+      },
+    },
+  }));
