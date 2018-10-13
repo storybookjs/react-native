fcabc23a6eee4a7e46989d1a2e253fadd47e86bd
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2018-10-13 21:25:46 +0200

    Some teamcity maintenance

--- .teamcity/OpenSourceProjects_Storybook/buildTypes/OpenSourceProjects_Storybook_NativeSmokeTests.kt
+++ .teamcity/OpenSourceProjects_Storybook/buildTypes/OpenSourceProjects_Storybook_NativeSmokeTests.kt
@@ -4,10 +4,10 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 
-object OpenSourceProjects_Storybook_ReactNative : BuildType({
+object OpenSourceProjects_Storybook_NativeSmokeTests : BuildType({
     uuid = "ac276912-df1a-44f1-8de2-056276193ce8"
-    id = "OpenSourceProjects_Storybook_ReactNative"
-    name = "React Native"
+    id = "OpenSourceProjects_Storybook_NativeSmokeTests"
+    name = "Native Smoke Tests"
 
     params {
         param("env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD", "true")
