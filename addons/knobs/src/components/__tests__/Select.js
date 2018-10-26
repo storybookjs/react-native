86743801faf5d816a8732625e12efd067ee81d33
Steven Sinatra <steven@stevensinatra.id>
2018-10-26 17:27:48 +1100

    Streamline tests

--- addons/knobs/src/components/__tests__/Select.js
+++ addons/knobs/src/components/__tests__/Select.js
@@ -17,14 +17,12 @@ describe('Select', () => {
       };
     });
 
-    describe('displays value', () => {
-      it('correctly maps option keys and values', () => {
-        const wrapper = shallow(<SelectType knob={knob} />);
+    it('correctly maps option keys and values', () => {
+      const wrapper = shallow(<SelectType knob={knob} />);
 
-        const green = wrapper.find('option').first();
-        expect(green.text()).toEqual('Green');
-        expect(green.prop('value')).toEqual('Green');
-      });
+      const green = wrapper.find('option').first();
+      expect(green.text()).toEqual('Green');
+      expect(green.prop('value')).toEqual('Green');
     });
   });
 
@@ -37,14 +35,12 @@ describe('Select', () => {
       };
     });
 
-    describe('displays value', () => {
-      it('correctly maps option keys and values', () => {
-        const wrapper = shallow(<SelectType knob={knob} />);
+    it('correctly maps option keys and values', () => {
+      const wrapper = shallow(<SelectType knob={knob} />);
 
-        const green = wrapper.find('option').first();
-        expect(green.text()).toEqual('green');
-        expect(green.prop('value')).toEqual('green');
-      });
+      const green = wrapper.find('option').first();
+      expect(green.text()).toEqual('green');
+      expect(green.prop('value')).toEqual('green');
     });
   });
 });
