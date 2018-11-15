e25388e37cc9cbe5e6ae65372cdd92f67a812e24
Norbert de Langen <ndelangen@me.com>
2018-11-15 21:02:47 +0100

    FIX backgrounds addon

--- addons/backgrounds/src/register.js
+++ addons/backgrounds/src/register.js
@@ -2,25 +2,13 @@ import React from 'react';
 import addons, { types } from '@storybook/addons';
 
 import { ADDON_ID, PANEL_ID } from './constants';
-import Panel from './Panel';
 import Tool from './Tool';
 
-function init() {
-  addons.register(ADDON_ID, api => {
-    const channel = addons.getChannel();
+addons.register(ADDON_ID, api => {
+  const channel = addons.getChannel();
 
-    addons.add(PANEL_ID, {
-      type: types.TOOL,
-      render: () => <Tool channel={channel} api={api} />,
-    });
-
-    addons.add(PANEL_ID, {
-      type: types.PANEL,
-      title: 'Backgrounds',
-      // eslint-disable-next-line react/prop-types
-      render: ({ active }) => <Panel channel={channel} api={api} active={active} />,
-    });
+  addons.add(PANEL_ID, {
+    type: types.TOOL,
+    render: () => <Tool channel={channel} api={api} />,
   });
-}
-
-export { init };
+});
