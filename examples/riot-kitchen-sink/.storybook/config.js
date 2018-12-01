9aeb439f893d9ced05e9d03e6cea2b0d0dd69522
Mansoor Basha Bellary <mbellary@chewy.com>
2018-11-30 23:12:51 -0500

    fix(): deprecations with replacing setOptions with withOptions

--- examples/riot-kitchen-sink/.storybook/config.js
+++ examples/riot-kitchen-sink/.storybook/config.js
@@ -1,9 +1,11 @@
-import { configure } from '@storybook/riot';
-import { setOptions } from '@storybook/addon-options';
+import { configure, addDecorator } from '@storybook/riot';
+import { withOptions } from '@storybook/addon-options';
 
-setOptions({
-  hierarchyRootSeparator: /\|/,
-});
+addDecorator(
+  withOptions({
+    hierarchyRootSeparator: /\|/,
+  })
+);
 
 function loadStories() {
   require('../src/stories');
