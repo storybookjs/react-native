9b4d4997f1d1b85ee146e03d55abef3b7e1c2177
Bart Waardenburg <bwaardenburg@anwb.nl>
2018-12-04 10:55:16 +0100

    chore: Added proper welcome story, added cli support and updated ADDONS_SUPPORT table

--- examples/preact-kitchen-sink/src/stories/addon-links.stories.js
+++ examples/preact-kitchen-sink/src/stories/addon-links.stories.js
@@ -2,6 +2,7 @@ import { h } from 'preact';
 
 import { storiesOf } from '@storybook/preact';
 import { linkTo } from '@storybook/addon-links';
+
 import Button from '../Button';
 
 storiesOf('Addons|Links', module).add('Go to welcome', () => (
