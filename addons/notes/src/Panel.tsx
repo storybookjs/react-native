4c5345d15cadb33e0277d399963c27bcee3b7623
Kai Röder <roeder.kai@gmail.com>
2018-12-10 20:58:09 +0100

    migrated to ts

--- addons/notes/src/Panel.tsx
+++ addons/notes/src/Panel.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { SyntaxHighlighter as SyntaxHighlighterBase, Placeholder } from '@storybook/components';
 import Markdown from 'markdown-to-jsx';
@@ -9,17 +9,52 @@ import { PARAM_KEY } from './shared';
 const Panel = styled.div({
   padding: 10,
   boxSizing: 'border-box',
-  width: '100%',
+  width: '100%'
 });
 
-const read = params => (typeof params === 'string' ? params : params.text || params.markdown);
-
-const SyntaxHighlighter = props => <SyntaxHighlighterBase bordered copyable {...props} />;
-
-export default class NotesPanel extends React.Component {
-  state = {
-    value: '',
+interface NotesPanelProps {
+  active: boolean;
+  channel: {
+    emit: any;
+    on(listener: string, callback: (text: string) => void): any;
+    removeListener(listener: string, callback: (text: string) => void): void;
   };
+  api: {
+    setQueryParams: any; // todo check correct definition
+    getQueryParam(queryParamName: string): any;
+    onStory(callback: () => void): () => void;
+  };
+}
+
+interface NotesPanelState {
+  value: string;
+}
+
+type ReadParams = string | { text: string; markdown: string };
+const read = (params: ReadParams) => (typeof params === 'string' ? params : params.text || params.markdown);
+const SyntaxHighlighter = (props: NotesPanelProps) => <SyntaxHighlighterBase bordered copyable {...props} />;
+
+export default class NotesPanel extends React.Component<NotesPanelProps, NotesPanelState> {
+
+  static propTypes = {
+    active: PropTypes.bool.isRequired,
+    channel: PropTypes.shape({
+      on: PropTypes.func,
+      emit: PropTypes.func,
+      removeListener: PropTypes.func
+    }).isRequired,
+    api: PropTypes.shape({
+      onStory: PropTypes.func,
+      getQueryParam: PropTypes.func,
+      setQueryParams: PropTypes.func
+    }).isRequired
+  };
+
+  readonly state: NotesPanelState = {
+    value: ''
+  };
+
+  mounted: boolean;
 
   // use our SyntaxHighlighter component in place of a <code> element when
   // converting markdown to react elements
@@ -68,17 +103,3 @@ export default class NotesPanel extends React.Component {
     );
   }
 }
-
-NotesPanel.propTypes = {
-  active: PropTypes.bool.isRequired,
-  channel: PropTypes.shape({
-    on: PropTypes.func,
-    emit: PropTypes.func,
-    removeListener: PropTypes.func,
-  }).isRequired,
-  api: PropTypes.shape({
-    onStory: PropTypes.func,
-    getQueryParam: PropTypes.func,
-    setQueryParams: PropTypes.func,
-  }).isRequired,
-};
