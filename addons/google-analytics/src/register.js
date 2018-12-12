63da58840ddcc3814c073df2cdafb9bbd39ad5ec
Norbert de Langen <ndelangen@me.com>
2018-12-12 15:01:48 +0100

    REFACTOR addon-googleAnalytics && ADD error tracking and missing stories tracking

--- addons/google-analytics/src/register.js
+++ addons/google-analytics/src/register.js
@@ -1,17 +1,26 @@
-import addons from '@storybook/addons';
 import { window } from 'global';
+import addons from '@storybook/addons';
+import { STORY_CHANGED, STORY_ERRORED, STORY_MISSING } from '@storybook/core-events';
+
 import ReactGA from 'react-ga';
 
 addons.register('storybook/google-analytics', api => {
   ReactGA.initialize(window.STORYBOOK_GA_ID);
 
-  api.onStory((kind, story) => {
-    let path = window.location.pathname;
-
-    if (path === '/') {
-      path = '';
-    }
-
-    ReactGA.pageview(`${path}/${kind}/${story}`);
+  api.on(STORY_CHANGED, () => {
+    const { url } = api.getUrlState();
+    ReactGA.pageview(url);
+  });
+  api.on(STORY_ERRORED, ({ description }) => {
+    ReactGA.exception({
+      description,
+      fatal: true,
+    });
+  });
+  api.on(STORY_MISSING, ({ id }) => {
+    ReactGA.exception({
+      description: `attempted to render ${id}, but it is missing`,
+      fatal: false,
+    });
   });
 });
