59d3a15565b9197e7a0458fc6b89ba6ba8957c9b
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2018-12-19 17:05:25 +0200

    renamed storybook to .storybook in react-native example

--- examples-native/crna-kitchen-sink/App.js
+++ examples-native/crna-kitchen-sink/App.js
@@ -1,4 +1,4 @@
-export default from './storybook';
+export default from './.storybook';
 
 // NOTE: The code below is what CRNA generates out of the box. We currently
 // have no clever way of replacing this with Storybook's UI (Vanilla RN does!)
