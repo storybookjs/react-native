6630dfc35c461adf1d2e654a65103f1026f08d0e
Tom Coleman <tom@percolatestudio.com>
2019-01-10 20:57:37 +1100

    Sort out image snapshot tests

--- addons/storyshots/storyshots-puppeteer/src/url.js
+++ addons/storyshots/storyshots-puppeteer/src/url.js
@@ -1,11 +1,12 @@
+import { toId } from '@storybook/core/client';
+
 import { URL } from 'url';
 
 export const constructUrl = (storybookUrl, kind, story) => {
-  const encodedKind = encodeURIComponent(kind);
-  const encodedStoryName = encodeURIComponent(story);
-  const storyUrl = `/iframe.html?selectedKind=${encodedKind}&selectedStory=${encodedStoryName}`;
-  const { protocol, host, pathname, search } = new URL(storybookUrl);
+  const id = toId(kind, story);
+
+  const storyUrl = `/iframe.html?id=${id}`;
+  const { protocol, host, pathname } = new URL(storybookUrl);
   const pname = pathname.replace(/\/$/, ''); // removes trailing /
-  const query = search.replace('?', '&'); // convert leading ? to &
-  return `${protocol}//${host}${pname}${storyUrl}${query}`;
+  return `${protocol}//${host}${pname}${storyUrl}`;
 };
