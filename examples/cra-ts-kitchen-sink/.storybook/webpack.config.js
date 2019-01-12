fd996bb1d858138fd0faa263d77016aafb21f34b
Michael Shilman <michael@lab80.co>
2019-01-12 12:51:35 +0800

    Update webpack for 5.0

--- examples/cra-ts-kitchen-sink/.storybook/webpack.config.js
+++ examples/cra-ts-kitchen-sink/.storybook/webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = (_baseCnig, _env, config) => ({
-  ...config,
+module.exports = {
   module: {
     rules: [
-      ...config.module.rules,
       {
         test: /\.tsx?$/,
         include: path.resolve(__dirname, '../src'),
@@ -13,5 +11,5 @@ module.exports = (_baseCnig, _env, config) => ({
       },
     ],
   },
-  plugins: [...config.plugins, new ForkTsCheckerWebpackPlugin()],
-});
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+};
