784ea3db4bcfa8c26524765a5f809627f7ce66e1
Tom Coleman <tom@thesnail.org>
2019-01-24 16:54:07 +1100

    Refactor shortcuts screen utils into libs/shortcuts
    And change shortcuts screen to accept a single keypress

--- lib/ui/src/libs/shortcut.test.ts
+++ lib/ui/src/libs/shortcut.test.ts
@@ -1,92 +1,97 @@
-import * as utils from './utils';
+import { eventToShortcut, keyToSymbol } from './shortcut';
 const ev = (attr: object) => new KeyboardEvent('keydown', { ...attr });
-describe('parseKey', () => {
+
+describe('eventToShortcut', () => {
   test('it handles alt key inputs', () => {
-    const output = utils.parseKey(ev({ altKey: true }));
-    expect(output).toEqual(['alt']);
+    const output = eventToShortcut(ev({ altKey: true }));
+    expect(output).toEqual(null);
   });
   test('it handles ctrl key inputs', () => {
-    const output = utils.parseKey(ev({ ctrlKey: true }));
-    expect(output).toEqual(['control']);
+    const output = eventToShortcut(ev({ ctrlKey: true }));
+    expect(output).toEqual(null);
   });
   test('it handles meta key inputs', () => {
-    const output = utils.parseKey(ev({ metaKey: true }));
-    expect(output).toEqual(['meta']);
+    const output = eventToShortcut(ev({ metaKey: true }));
+    expect(output).toEqual(null);
+  });
+  test('it handles shift key inputs', () => {
+    const output = eventToShortcut(ev({ shiftKey: true }));
+    expect(output).toEqual(null);
   });
   test('it handles enter key inputs', () => {
-    const output = utils.parseKey(ev({ key: 'Enter' }));
+    const output = eventToShortcut(ev({ key: 'Enter' }));
     expect(output).toEqual([]);
   });
   test('it handles space bar inputs', () => {
-    const output = utils.parseKey(ev({ key: ' ' }));
+    const output = eventToShortcut(ev({ key: ' ' }));
     expect(output).toEqual(['space']);
   });
   test('it handles escape inputs', () => {
-    const output = utils.parseKey(ev({ key: 'Escape' }));
+    const output = eventToShortcut(ev({ key: 'Escape' }));
     expect(output).toEqual(['escape']);
   });
-  test('it handles shift key inputs', () => {
-    const output = utils.parseKey(ev({ shiftKey: true }));
-    expect(output).toEqual(['shift']);
-  });
   test('it capitalizes a letter key through', () => {
-    const output = utils.parseKey(ev({ key: 'a' }));
+    const output = eventToShortcut(ev({ key: 'a' }));
     expect(output).toEqual(['A']);
   });
   test('it passes regular key through', () => {
-    const output = utils.parseKey(ev({ key: '1' }));
+    const output = eventToShortcut(ev({ key: '1' }));
     expect(output).toEqual(['1']);
+  });
+  test('it passes modified regular key through', () => {
+    const output = eventToShortcut(ev({ altKey: true, key: '1' }));
+    expect(output).toEqual(['alt', '1']);
   });
 });
 
 describe('keyToSymbol', () => {
   test('control returns a caret', () => {
-    const result = utils.keyToSymbol('control');
+    const result = keyToSymbol('control');
     expect(result).toBe('⌃');
   });
 
   test('meta returns ⌘', () => {
-    const result = utils.keyToSymbol('meta');
+    const result = keyToSymbol('meta');
     expect(result).toEqual('⌘');
   });
   test('shift returns ⇧', () => {
-    const result = utils.keyToSymbol('shift');
+    const result = keyToSymbol('shift');
     expect(result).toBe('⇧​');
   });
   test('enter returns an empty string', () => {
-    const result = utils.keyToSymbol('Enter');
+    const result = keyToSymbol('Enter');
     expect(result).toBe('');
   });
 
   // tslint:disable-next-line:quotemark
   test("' ' returns SPACE", () => {
-    const result = utils.keyToSymbol(' ');
+    const result = keyToSymbol(' ');
     expect(result).toEqual('SPACE');
   });
   test('escape returns esc', () => {
-    const result = utils.keyToSymbol('escape');
+    const result = keyToSymbol('escape');
     expect(result).toEqual('');
   });
   test('ArrowUp returns ↑​​​', () => {
-    const result = utils.keyToSymbol('ArrowUp');
+    const result = keyToSymbol('ArrowUp');
     expect(result).toBe('↑');
   });
   test('ArrowDown returns ↓​​​', () => {
-    const result = utils.keyToSymbol('ArrowDown');
+    const result = keyToSymbol('ArrowDown');
     expect(result).toBe('↓');
   });
   test('ArrowLeft returns ←', () => {
-    const result = utils.keyToSymbol('ArrowLeft');
+    const result = keyToSymbol('ArrowLeft');
     expect(result).toBe('←');
   });
 
   test('ArrowRight returns →', () => {
-    const result = utils.keyToSymbol('ArrowRight');
+    const result = keyToSymbol('ArrowRight');
     expect(result).toBe('→');
   });
 
   test('it capitalizes a lowercase key', () => {
-    const output = utils.keyToSymbol('a');
+    const output = keyToSymbol('a');
     expect(output).toBe('A');
   });
 });
