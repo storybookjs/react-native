9b057f5856dd57229673694219e6fd4d3b63c43e
Norbert de Langen <ndelangen@me.com>
2019-01-30 00:16:15 +0100

    MIGRATE animation to typescript

--- lib/theming/src/animation.ts
+++ lib/theming/src/animation.ts
@@ -5,7 +5,7 @@ export const easing = {
   rubber: 'cubic-bezier(0.175, 0.885, 0.335, 1.05)',
 };
 
-export const rotate360 = keyframes`
+const rotate360 = keyframes`
 	from {
 		transform: rotate(0deg);
 	}
@@ -14,27 +14,35 @@ export const rotate360 = keyframes`
 	}
 `;
 
-export const glow = keyframes`
+const glow = keyframes`
   0%, 100% { opacity: 1; }
   50% { opacity: .4; }
 `;
 
-export const float = keyframes`
+const float = keyframes`
   0% { transform: translateY(1px); }
   25% { transform: translateY(0px); }
   50% { transform: translateY(-3px); }
   100% { transform: translateY(1px); }
 `;
 
-export const jiggle = keyframes`
+const jiggle = keyframes`
   0%, 100% { transform:translate3d(0,0,0); }
   12.5%, 62.5% { transform:translate3d(-4px,0,0); }
   37.5%, 87.5% {  transform: translate3d(4px,0,0);  }
 `;
 
-export const inlineGlow = css`
+const inlineGlow = css`
   animation: ${glow} 1.5s ease-in-out infinite;
   background: ${color.border};
   color: transparent;
   cursor: progress;
 `;
+
+export const animation = {
+  rotate360,
+  glow,
+  float,
+  jiggle,
+  inlineGlow,
+};
