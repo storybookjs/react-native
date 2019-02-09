07e933ff95801d1dcf34220a58724e1a5d900433
domyen <dom@hichroma.com>
2019-02-08 19:06:43 -0500

    Start theming dark mode

--- lib/theming/src/themes/light.ts
+++ lib/theming/src/themes/light.ts
@@ -2,15 +2,49 @@ import { create } from '../create';
 
 import { color, typography, background } from '../base';
 
+// export default create({
+//   // Storybook-specific color palette
+//   colorPrimary: '#FF4785', // coral
+//   colorSecondary: '#1EA7FD', // ocean
+//
+//   // UI
+//   appBg: background.app,
+//   appContentBg: color.lightest,
+//   appBorderColor: color.border,
+//   appBorderRadius: 4,
+//
+//   // Fonts
+//   fontBase: typography.fonts.base,
+//   fontCode: typography.fonts.mono,
+//
+//   // Text colors
+//   textColor: color.darkest,
+//   textInverseColor: color.lightest,
+//
+//   // Toolbar default and active colors
+//   barTextColor: color.mediumdark,
+//   barSelectedColor: color.secondary,
+//   barBg: color.lightest,
+//
+//   // Form colors
+//   inputBg: color.lightest,
+//   inputBorder: color.border,
+//   inputTextColor: color.darkest,
+//   inputBorderRadius: 4,
+//
+//   // Brand logo/text
+//   brand: null,
+// });
+
 export default create({
   // Storybook-specific color palette
   colorPrimary: '#FF4785', // coral
   colorSecondary: '#1EA7FD', // ocean
 
   // UI
-  appBg: background.app,
-  appContentBg: color.lightest,
-  appBorderColor: color.border,
+  appBg: '#222222',
+  appContentBg: color.darkest,
+  appBorderColor: 'rgba(255,255,255,.1)',
   appBorderRadius: 4,
 
   // Fonts
@@ -18,18 +52,18 @@ export default create({
   fontCode: typography.fonts.mono,
 
   // Text colors
-  textColor: color.darkest,
-  textInverseColor: color.lightest,
+  textColor: color.lightest,
+  textInverseColor: color.darkest,
 
   // Toolbar default and active colors
-  barTextColor: color.mediumdark,
+  barTextColor: '#999999',
   barSelectedColor: color.secondary,
-  barBg: color.lightest,
+  barBg: color.darkest,
 
   // Form colors
-  inputBg: color.lightest,
-  inputBorder: color.border,
-  inputTextColor: color.darkest,
+  inputBg: '#303030',
+  inputBorder: 'rgba(0,0,0,.1)',
+  inputTextColor: color.lightest,
   inputBorderRadius: 4,
 
   // Brand logo/text
