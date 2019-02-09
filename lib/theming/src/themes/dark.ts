cd573a89578f409e67c660d3d8e090ba6f92e6fc
Michael Shilman <michael@lab80.co>
2019-02-09 10:53:57 +0800

    Dark theme WIP (get build working)

--- lib/theming/src/themes/dark.ts
+++ lib/theming/src/themes/dark.ts
@@ -2,40 +2,36 @@ import { create } from '../create';
 
 import { color, typography, background } from '../base';
 
-// export default create({
-//   // Storybook-specific color palette
-//   color: {
-//     primary: '#FF4785', // coral
-//     secondary: '#1EA7FD', // ocean
-//     tertiary: '#FAFBFC', // neutral buttons
-//     ancillary: '#22a699', // for code
-//   },
-//
-//   // UI colors
-//   bgApp: background.app,
-//   bgHoverItem: background.hoverable,
-//   borderColor: color.border,
-//   borderRadius: 4,
-//
-//   // Typography
-//   baseFont: typography.fonts.base,
-//   codeFont: typography.fonts.mono,
-//   mainTextSize: typography.size.s2 - 1, // 13px
-//
-//
-//
-//   // Text colors (dark on light)
-//
-//   mainTextColor: color.darkest,
-//   mainTextBgColor: color.lightest,
-//
-//   // Text colors (light on dark)
-//   inverseTextColor: color.lightest,
-//   inverseTextBgColor: color.mediumdark,
-//
-//   barTextColor: color.mediumdark,
-//   barSelectedColor: color.secondary,
-//
-//   inputFill: color.lightest,
-//   inputBorder: color.border,
-// });
+export default create({
+  // Storybook-specific color palette
+  colorPrimary: '#FF4785', // coral
+  colorSecondary: '#1EA7FD', // ocean
+  // colorTertiary: '#FAFBFC', // neutral buttons
+  // colorAncillary: '#22a699', // for code
+
+  // UI colors
+  appBg: background.app,
+  // bgHoverItem: background.hoverable,
+  appBorderColor: color.border,
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: typography.fonts.base,
+  fontCode: typography.fonts.mono,
+  // mainTextSize: typography.size.s2 - 1, // 13px
+
+  // Text colors (dark on light)
+
+  textColor: color.darkest,
+  textInverseColor: color.lightest,
+
+  // Text colors (light on dark)
+  // inverseTextColor: color.lightest,
+  // inverseTextBgColor: color.mediumdark,
+
+  barTextColor: color.mediumdark,
+  barSelectedColor: color.secondary,
+
+  inputBg: color.lightest,
+  inputBorder: color.border,
+});
