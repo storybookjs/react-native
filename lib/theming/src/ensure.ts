ca15ae03eaaea0e9f4983d3385d7b2cc39cfab10
Norbert de Langen <ndelangen@me.com>
2019-02-20 17:39:26 +0100

    FIX ensure (color typo)

--- lib/theming/src/ensure.ts
+++ lib/theming/src/ensure.ts
@@ -1,32 +1,35 @@
 import light from './themes/light';
 import { Theme } from './base';
 
-export const ensure = (input: any): Theme => ({
-  base: input.base,
-  color: input.colors || light.colors,
-  background: input.background || light.background,
-  typography: input.typography || light.typography,
-  animation: input.animation || light.animation,
-  easing: input.easing || light.easing,
+export const ensure = (input: any): Theme =>
+  !input
+    ? light
+    : {
+        base: input.base,
+        color: input.color || light.color,
+        background: input.background || light.background,
+        typography: input.typography || light.typography,
+        animation: input.animation || light.animation,
+        easing: input.easing || light.easing,
 
-  input: input.input || light.input,
+        input: input.input || light.input,
 
-  // UI
-  layoutMargin: input.layoutMargin || light.layoutMargin,
-  appBorderColor: input.appBorderColor || light.appBorderColor,
-  appBorderRadius: input.appBorderRadius || light.appBorderRadius,
+        // UI
+        layoutMargin: input.layoutMargin || light.layoutMargin,
+        appBorderColor: input.appBorderColor || light.appBorderColor,
+        appBorderRadius: input.appBorderRadius || light.appBorderRadius,
 
-  // Toolbar default/active colors
-  barTextColor: input.barTextColor || light.barTextColor,
-  barSelectedColor: input.barSelectedColor || light.barSelectedColor,
-  barBg: input.barBg || light.barBg,
+        // Toolbar default/active colors
+        barTextColor: input.barTextColor || light.barTextColor,
+        barSelectedColor: input.barSelectedColor || light.barSelectedColor,
+        barBg: input.barBg || light.barBg,
 
-  // Brand logo/text
-  brand: input.brand || light.brand,
+        // Brand logo/text
+        brand: input.brand || light.brand,
 
-  code: input.code || light.code,
+        code: input.code || light.code,
 
-  // Addon actions theme
-  // API example https://github.com/xyc/react-inspector/blob/master/src/styles/themes/chromeLight.js
-  addonActionsTheme: input.addonActionsTheme || light.addonActionsTheme,
-});
+        // Addon actions theme
+        // API example https://github.com/xyc/react-inspector/blob/master/src/styles/themes/chromeLight.js
+        addonActionsTheme: input.addonActionsTheme || light.addonActionsTheme,
+      };
