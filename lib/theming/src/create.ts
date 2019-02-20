f16213b32ae07887f05e0d1702a32d5f2b332272
Norbert de Langen <ndelangen@me.com>
2019-02-20 13:41:08 +0100

    ADD an ensure around the theme
    if no proper theme is provided, we use light / shallow-merge in light base

--- lib/theming/src/create.ts
+++ lib/theming/src/create.ts
@@ -5,6 +5,7 @@ import { easing, animation } from './animation';
 import { create as createSyntax } from './modules/syntax';
 import { chromeLight, chromeDark } from 'react-inspector';
 import { opacify } from 'polished';
+import light from './themes/light';
 
 interface Rest {
   [key: string]: any;
@@ -177,3 +178,35 @@ export const create = (vars: ThemeVar, rest?: Rest): Theme => ({
 
   ...(rest || {}),
 });
+
+export const ensure = (input: any): Theme => {
+  return {
+    base: input.base,
+    color: input.colors || light.colors,
+    background: input.background || light.background,
+    typography: input.typography || light.typography,
+    animation: input.animation || light.animation,
+    easing: input.easing || light.easing,
+
+    input: input.input || light.input,
+
+    // UI
+    layoutMargin: input.layoutMargin || light.layoutMargin,
+    appBorderColor: input.appBorderColor || light.appBorderColor,
+    appBorderRadius: input.appBorderRadius || light.appBorderRadius,
+
+    // Toolbar default/active colors
+    barTextColor: input.barTextColor || light.barTextColor,
+    barSelectedColor: input.barSelectedColor || light.barSelectedColor,
+    barBg: input.barBg || light.barBg,
+
+    // Brand logo/text
+    brand: input.brand || light.brand,
+
+    code: input.code || light.code,
+
+    // Addon actions theme
+    // API example https://github.com/xyc/react-inspector/blob/master/src/styles/themes/chromeLight.js
+    addonActionsTheme: input.addonActionsTheme || light.addonActionsTheme,
+  };
+};
