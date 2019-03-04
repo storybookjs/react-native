2aad74cca4a6fb408cf5577ddf006f425d4c1b4d
Norbert de Langen <ndelangen@me.com>
2019-03-04 15:18:02 +0100

    CLEANUP the creation of default themes

--- lib/theming/src/themes/dark.ts
+++ lib/theming/src/themes/dark.ts
@@ -1,9 +1,5 @@
 import { create } from '../create';
-import { themeVars } from './dark-vars';
 
 export default create({
-  // Is this a light theme or a dark theme?
   base: 'dark',
-
-  ...themeVars,
 });
--- lib/theming/src/themes/light.ts
+++ lib/theming/src/themes/light.ts
@@ -1,10 +1,5 @@
 import { create } from '../create';
 
-import { themeVars } from './dark-vars';
-
 export default create({
-  // Is this a light theme or a dark theme?
   base: 'light',
-
-  ...themeVars,
 });
