6b4f86117f84aaeb1c4f46643036a10c41aba512
Thanh Tran <thanh.trantrong@lazada.com>
2019-03-04 17:38:47 +0700

    feature (rax-kitchen-sink): Adapt Welcome screen with Rax components and styles, fix stories link, add storysource addon (not working)

--- examples/rax-kitchen-sink/src/Welcome.js
+++ examples/rax-kitchen-sink/src/Welcome.js
@@ -1,87 +1,65 @@
+/* eslint-disable react/prop-types, jsx-a11y/anchor-is-valid */
 import { createElement } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
-import Button from 'rax-button';
 import Image from 'rax-image';
 import Link from 'rax-link';
 
-const Main = props => (
+const Main = ({ children, ...props }) => (
   <View
     {...props}
     style={{
       margin: 15,
-      maxWidth: 600,
-      lineHeight: 1.4,
       fontFamily: '"Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif',
     }}
-  />
-);
-
-// eslint-disable-next-line react/prop-types
-const P = ({ children, ...props }) => (
-  <View {...props}>
-    <Text>{children}</Text>
+  >
+    {children}
   </View>
 );
 
-// eslint-disable-next-line react/prop-types
+const P = ({ children, ...props }) => (
+  <View {...props} style={{ marginBottom: 10 }}>
+    <Text style={{ fontSize: 12 }}>{children}</Text>
+  </View>
+);
+
 const Title = ({ children, ...props }) => (
   <Text {...props} style={{ fontSize: 40, fontWeight: 'bold' }}>
     {children}
   </Text>
 );
 
-const Note = props => (
-  <Text
+const Note = ({ children, ...props }) => (
+  <View
     {...props}
     style={{
       opacity: 0.5,
     }}
-  />
+  >
+    {children}
+  </View>
 );
 
-const InlineCode = props => (
+const InlineCode = ({ children, ...props }) => (
   <Text
     {...props}
     style={{
-      fontSize: 15,
-      fontWeight: 600,
+      fontFamily: 'monospace',
+      fontSize: 12,
       padding: '2px 5px',
       border: '1px solid #eae9e9',
       borderRadius: 4,
       backgroundColor: '#f3f2f2',
       color: '#3a3a3a',
     }}
-  />
-);
-
-// eslint-disable-next-line react/prop-types
-const NavButton = ({ children, ...props }) => (
-  <Button
-    {...props}
-    type="button"
-    style={{
-      color: '#1474f3',
-      textDecoration: 'none',
-      borderBottom: '1px solid #1474f3',
-      paddingBottom: 2,
-      borderTop: 'none',
-      borderRight: 'none',
-      borderLeft: 'none',
-      backgroundColor: 'transparent',
-      padding: 0,
-      cursor: 'pointer',
-      font: 'inherit',
-    }}
   >
     {children}
-  </Button>
+  </Text>
 );
 
-/* eslint-disable-next-line react/prop-types */
 const Welcome = ({ showApp }) => (
   <Main>
-    <Title>Welcome to storybook for Preact</Title>
+    <Title>Welcome to storybook for Rax</Title>
     <Image
       source={{
         uri: './rax-logo.png',
@@ -91,8 +69,8 @@ const Welcome = ({ showApp }) => (
           'https://user-images.githubusercontent.com/677114/49848760-999e7d00-fe11-11e8-978f-264ea31f6739.png',
       }}
       style={{
-        width: 150,
-        height: 150,
+        width: 100,
+        height: 100,
         margin: '0 auto',
       }}
       resizeMode="cover"
@@ -105,8 +83,11 @@ const Welcome = ({ showApp }) => (
       want. (Basically a story is like a visual test case.)
     </P>
     <P>
-      See these sample <NavButton onClick={showApp}>stories</NavButton> for a component called{' '}
-      <InlineCode>Button</InlineCode>.
+      See these sample{' '}
+      <Link href="#" onPress={showApp} rel="noopener noreferrer">
+        stories
+      </Link>{' '}
+      for a component called <InlineCode>Button</InlineCode>.
     </P>
     <P>
       Just like that, you can add your own components as stories. You can also edit those components
--- examples/rax-kitchen-sink/src/stories/index.stories.js
+++ examples/rax-kitchen-sink/src/stories/index.stories.js
@@ -6,14 +6,16 @@ import { linkTo } from '@storybook/addon-links';
 import Button from 'rax-button';
 import Text from 'rax-text';
 
-import Welcome from '../App';
+import Welcome from '../Welcome';
 
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button', 'with text')} />
+));
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onclick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => <Button onPress={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
-    <Button onclick={action('clicked')}>
+    <Button onPress={action('clicked')}>
       <Text role="img" aria-label="so cool">
         😀 😎 👍 💯
       </Text>
