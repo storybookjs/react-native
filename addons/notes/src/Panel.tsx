5655105435102cbf33b4281dc7abf5e9e8b8a80e
Vince Picone <vpicone@gmail.com>
2019-03-06 09:28:02 -0600

    Allow for theming of the notes addon panel

--- addons/notes/src/Panel.tsx
+++ addons/notes/src/Panel.tsx
@@ -10,12 +10,13 @@ import Markdown from 'markdown-to-jsx';
 
 import { PARAM_KEY, API, Parameters } from './shared';
 
-const Panel = styled.div({
+const Panel = styled.div(({theme}) => {
   padding: '3rem 40px',
   boxSizing: 'border-box',
   width: '100%',
   maxWidth: 980,
   margin: '0 auto',
+  ...theme.addonNotesTheme
 });
 
 interface Props {
