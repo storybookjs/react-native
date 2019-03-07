8aa71c5382d47dac06d101344efacc9d3234a8fa
codebyalex <wilsonc.alexander@gmail.com>
2019-03-07 00:37:57 -0500

    modified debounce logic to use lodash rather than custom timeout code

--- addons/knobs/src/registerKnobs.js
+++ addons/knobs/src/registerKnobs.js
@@ -1,13 +1,13 @@
 import addons from '@storybook/addons';
 import { STORY_CHANGED, FORCE_RE_RENDER, REGISTER_SUBSCRIPTION } from '@storybook/core-events';
 
+import debounce from 'lodash.debounce';
 import KnobManager from './KnobManager';
 import { CHANGE, CLICK, RESET, SET } from './shared';
 
 export const manager = new KnobManager();
 const { knobStore } = manager;
-const KNOB_CHANGED_DEBOUNCE_DELAY_MS = 155; // approximate average time between keypresses
-let timeoutId = null;
+const KNOB_CHANGED_DEBOUNCE_DELAY_MS = 150;
 
 function forceReRender() {
   addons.getChannel().emit(FORCE_RE_RENDER);
@@ -18,20 +18,21 @@ function setPaneKnobs(timestamp = +new Date()) {
   channel.emit(SET, { knobs: knobStore.getAll(), timestamp });
 }
 
+// Increased performance by reducing the number of times a component is rendered during knob changes
+const debouncedOnKnobChanged = debounce(change => {
+  const { name, value } = change;
+
+  // Update the related knob and it's value.
+  const knobOptions = knobStore.get(name);
+
+  knobOptions.value = value;
+  knobStore.markAllUnused();
+
+  forceReRender();
+}, KNOB_CHANGED_DEBOUNCE_DELAY_MS);
+
 function knobChanged(change) {
-  clearTimeout(timeoutId);
-  timeoutId = setTimeout(() => {
-    // debouncing occurs here to increase performance
-    const { name, value } = change;
-
-    // Update the related knob and it's value.
-    const knobOptions = knobStore.get(name);
-
-    knobOptions.value = value;
-    knobStore.markAllUnused();
-
-    forceReRender();
-  }, KNOB_CHANGED_DEBOUNCE_DELAY_MS); // amount of time used to ensure that the user has time to type before re-rendering
+  debouncedOnKnobChanged(change);
 }
 
 function knobClicked(clicked) {
