1a3607c4a6f58e7f6ecd66b812be7aa192eb7466
Norbert de Langen <ndelangen@me.com>
2019-03-14 13:18:29 +0100

    CHANGE addon-events to use API instead of channel

--- addons/events/src/manager.js
+++ addons/events/src/manager.js
@@ -5,12 +5,11 @@ import Panel from './components/Panel';
 import { ADDON_ID, PANEL_ID } from './constants';
 
 export function register() {
-  addons.register(ADDON_ID, () => {
-    const channel = addons.getChannel();
+  addons.register(ADDON_ID, api => {
     addons.addPanel(PANEL_ID, {
       title: 'Events',
       // eslint-disable-next-line react/prop-types
-      render: ({ active, key }) => <Panel key={key} channel={channel} active={active} />,
+      render: ({ active, key }) => <Panel key={key} api={api} active={active} />,
     });
   });
 }
