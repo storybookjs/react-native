af0ea7e8b420f93da25a6f776ce287bdbab3de71
Arman Sargsyan <mail@arman.io>
2019-03-16 00:47:43 +0300

    fix(a11y): a11y each report has 'name' param for generate unique key for item @see https://github.com/storybooks/storybook/issues/6107

--- addons/a11y/src/components/A11YPanel.tsx
+++ addons/a11y/src/components/A11YPanel.tsx
@@ -165,17 +165,29 @@ export class A11YPanel extends Component<A11YPanelProps, A11YPanelState> {
                 {
                   label: <Violations>{violations.length} Violations</Violations>,
                   panel: (
-                    <Report passes={false} items={violations} empty="No a11y violations found." />
+                    <Report
+                      name="violations"
+                      passes={false}
+                      items={violations}
+                      empty="No a11y violations found."
+                    />
                   ),
                 },
                 {
                   label: <Passes>{passes.length} Passes</Passes>,
-                  panel: <Report passes items={passes} empty="No a11y check passed." />,
+                  panel: (
+                    <Report name={'passes'} passes items={passes} empty="No a11y check passed." />
+                  ),
                 },
                 {
                   label: <Incomplete>{incomplete.length} Incomplete</Incomplete>,
                   panel: (
-                    <Report passes={false} items={incomplete} empty="No a11y incomplete found." />
+                    <Report
+                      name="incomplete"
+                      passes={false}
+                      items={incomplete}
+                      empty="No a11y incomplete found."
+                    />
                   ),
                 },
               ]}
