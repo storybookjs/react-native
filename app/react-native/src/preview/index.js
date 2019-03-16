da3dd88ee6ad925d8795c43d33c76499ba6c71ae
Benoit Dion <benoitdion@gmail.com>
2019-03-16 17:10:17 -0400

    Fix infinte SET_CURRENT_STORY/SELECT_STORY loop

--- app/react-native/src/preview/index.js
+++ app/react-native/src/preview/index.js
@@ -135,9 +135,12 @@ export default class Preview {
 
   _sendSetStories() {
     const channel = addons.getChannel();
-    const stories = this._stories.dumpStoryBook();
+    const stories = this._stories.extract();
     channel.emit(Events.SET_STORIES, { stories });
     channel.emit(Events.STORIES_CONFIGURED);
+    if (this.currentStory) {
+      channel.emit(Events.SET_CURRENT_STORY, this.currentStory);
+    }
   }
 
   _sendGetCurrentStory() {
@@ -196,6 +199,7 @@ export default class Preview {
   }
 
   _selectStory(story) {
+    this.currentStory = story;
     const channel = addons.getChannel();
     channel.emit(Events.SELECT_STORY, story);
   }
