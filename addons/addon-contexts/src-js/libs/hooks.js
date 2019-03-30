f2f8c1d0d632a43acaff9f84f6215caa9b2a4433
Leo Y. Li <leo.li@imagia.com>
2019-03-29 23:13:17 -0400

    #1: remove untyped src

--- addons/addon-contexts/src-js/libs/hooks.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import addons from '@storybook/addons';
-
-export const useChannel = (event, eventHandler, inputs = []) =>
-  useEffect(() => {
-    const channel = addons.getChannel();
-    channel.on(event, eventHandler);
-    return () => channel.removeListener(event, eventHandler);
-  }, inputs);
