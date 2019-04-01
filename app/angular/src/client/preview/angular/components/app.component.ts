9dbbdaa9f7fc9250304dd79580e9b5719a596544
Max Sagan <maxim.sagan@winning.com.au>
2019-04-01 15:16:02 +1100

    Fix issue reference

--- app/angular/src/client/preview/angular/components/app.component.ts
+++ app/angular/src/client/preview/angular/components/app.component.ts
@@ -50,7 +50,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.subscription = this.data.subscribe(newData => {
         this.setProps(instance, newData);
         childChangeDetectorRef.markForCheck();
-        // Must detect changes on the current component in order to update any changes in child component's @HostBinding properties (angular/angular##22560)
+        // Must detect changes on the current component in order to update any changes in child component's @HostBinding properties (angular/angular#22560)
         this.changeDetectorRef.detectChanges();
       });
     });
