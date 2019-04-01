7d1412bb815bd6b141e3e216b26aa08031ad5128
Gaëtan Maisse <gmaisse@leadformance.com>
2019-04-01 21:07:50 +0200

    refactor: rewrite some code to avoid anonymous fct in JS compiled code
    It looks like TS compiler does not generate the same as babel one and so
    some code must be rewrite in order to "force" TS compiler to generate JS
     code that is compatible with SB codebase (and that match tests
     snapshots).

--- lib/components/src/ScrollArea/ScrollArea.tsx
+++ lib/components/src/ScrollArea/ScrollArea.tsx
@@ -1,7 +1,7 @@
 // Storybook's implementation of SimpleBar https://github.com/Grsmto/simplebar
 // Note: "SimpleBar can't be used on the <body>, <textarea> or <iframe> elements."
 
-import React, { Fragment, FunctionComponent } from 'react';
+import React, { Fragment, FunctionComponent, ReactNode } from 'react';
 import { styled, Global } from '@storybook/theming';
 
 import SimpleBar from 'simplebar-react';
@@ -38,19 +38,25 @@ export interface ScrollAreaProps {
   className?: string;
 }
 
-export const ScrollArea: FunctionComponent<ScrollAreaProps> = ({
+// Need to define ScrollArea using old fashioned `function ScrollArea` to avoid TS compiler to
+// generate an anonymous function instead of naming it ScrollArea...
+// See https://github.com/storybooks/storybook/pull/6095#issuecomment-477480930
+// tslint:disable-next-line:no-shadowed-variable
+export const ScrollArea: FunctionComponent<ScrollAreaProps> = function ScrollArea({
   children,
   vertical,
   horizontal,
   ...props
-}) => (
-  <Fragment>
-    <Global styles={getScrollAreaStyles} />
-    <Scroll vertical={vertical} horizontal={horizontal} {...props}>
-      {children}
-    </Scroll>
-  </Fragment>
-);
+}) {
+  return (
+    <Fragment>
+      <Global styles={getScrollAreaStyles} />
+      <Scroll vertical={vertical} horizontal={horizontal} {...props}>
+        {children}
+      </Scroll>
+    </Fragment>
+  );
+};
 
 ScrollArea.defaultProps = {
   horizontal: false,
