e798429a0ad2efb2ddf925db192baf575f19e222
Leo Y. Li <leo.li@imagia.com>
2019-04-04 22:09:23 -0400

    #0: ensure a consistent naming

--- addons/addon-contexts/src/libs/ducks.ts
+++ addons/addon-contexts/src/libs/ducks.ts
@@ -3,10 +3,10 @@
  * @see https://github.com/erikras/ducks-modular-redux
  */
 import { OPT_OUT } from './constants';
-import { UPDATE_PROPS_MAP, PropsTreeUpdaterType } from '../@types';
+import { UPDATE_PROPS_MAP, PropsMapUpdaterType } from '../@types';
 
 // reducers
-export const propsTreeReducer = (state: any, { type, payload }: UPDATE_PROPS_MAP) => {
+export const propsMapsReducer = (state: any, { type, payload }: UPDATE_PROPS_MAP) => {
   switch (type) {
     case 'UPDATE_PROPS_MAP': {
       return {
@@ -21,7 +21,7 @@ export const propsTreeReducer = (state: any, { type, payload }: UPDATE_PROPS_MAP
 };
 
 // actions
-export const propsTreeUpdater: PropsTreeUpdaterType = (nodes) => ([nodeId, name]) => {
+export const PropsMapUpdater: PropsMapUpdaterType = (nodes) => ([nodeId, name]) => {
   const { params = [] } = nodes.find((node) => node.nodeId === nodeId) || {};
   const { props = null } =
     // when opt-out context
