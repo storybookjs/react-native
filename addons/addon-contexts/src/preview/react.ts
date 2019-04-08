2e170b752b15a2c65579c8394ed828cca4395352
Leo Y. Li <leo.li@imagia.com>
2019-04-08 00:14:39 -0400

    #0: enhance readability

--- addons/addon-contexts/src/preview/react.ts
+++ addons/addon-contexts/src/preview/react.ts
@@ -1,12 +1,12 @@
-import { createElement as h } from 'react';
+import { createElement } from 'react';
 import { addonContextsAPI } from './api';
 import { Wrapper } from '../@types';
 
 export const reactWrapper: Wrapper = (getStory, content, settings) => {
-  const { getContextNodes, getSelectionState, getPropsMap, renderInContexts } = addonContextsAPI();
+  const { getContextNodes, getSelectionState, getPropsMap, getRenderFrom } = addonContextsAPI();
   const nodes = getContextNodes(settings);
   const state = getSelectionState();
   const props = getPropsMap(nodes, state);
-  const render = renderInContexts(h);
+  const render = getRenderFrom(createElement);
   return render(nodes, props, () => getStory(content));
 };
