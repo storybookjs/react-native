ecdb0b2457fa998c4ac75767c50f5dc7be0da7b3
Leo Y Li <leo.li@imagia.com>
2019-04-17 15:20:40 -0400

    ADD addon-contexts example stories

--- examples/official-storybook/stories/addon-contexts.sotries.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withContexts } from '@storybook/addon-contexts/react';
-
-storiesOf('Addons|Contexts', module)
-  .addDecorator(withContexts)
-  .add('Contexts 1', () => <div>123</div>);
