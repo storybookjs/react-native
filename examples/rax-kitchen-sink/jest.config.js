154c9e7af42e5f1380bf18d958a949a7bf75fdae
Norbert de Langen <ndelangen@me.com>
2019-04-26 15:02:53 +0200

    FIX tests

--- examples/rax-kitchen-sink/jest.config.js
+++ examples/rax-kitchen-sink/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(css|scss|stylesheet)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(md)$': '<rootDir>/__mocks__/htmlMock.js',
   },
   moduleDirectories: ['<rootDir>/node_modules', 'src'],
