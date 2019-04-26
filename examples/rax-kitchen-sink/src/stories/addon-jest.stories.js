c75cba243071f14d6f9bcf42e6ff237cfd3749c4
Norbert de Langen <ndelangen@me.com>
2019-04-26 14:54:55 +0200

    FIX linting

--- examples/rax-kitchen-sink/src/stories/addon-jest.stories.js
+++ examples/rax-kitchen-sink/src/stories/addon-jest.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/rax';
 import { withTests } from '@storybook/addon-jest';
 import App from '../components/App';
 import Welcome from '../components/Welcome';
+// eslint-disable-next-line import/no-unresolved
 import results from '../../jest-test-results.json';
 
 storiesOf('Addon|addon-jest', module)
