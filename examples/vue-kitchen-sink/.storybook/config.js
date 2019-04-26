b1e23d6f057550cdfad500ed20827382a11ad199
codebyalex <wilsonc.alexander@gmail.com>
2019-04-26 18:36:24 -0400

    Added the a11y addon to the Vue example

--- examples/vue-kitchen-sink/.storybook/config.js
+++ examples/vue-kitchen-sink/.storybook/config.js
@@ -1,9 +1,10 @@
-import { configure, addParameters } from '@storybook/vue';
+import { configure, addParameters, addDecorator } from '@storybook/vue';
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import { withA11y } from '@storybook/addon-a11y';
 import MyButton from '../src/stories/Button.vue';
 
+addDecorator(withA11y);
 Vue.component('my-button', MyButton);
 Vue.use(Vuex);
 
