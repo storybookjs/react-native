1ae1e5eef475f37a4e315bfa345d141e377d23c1
Leo Y. Li <leo.li@imagia.com>
2019-04-26 22:41:11 -0400

    FIX addon-contexts: correct code comment for Preact integration

--- addons/contexts/src/preview/frameworks/preact.ts
+++ addons/contexts/src/preview/frameworks/preact.ts
@@ -4,7 +4,7 @@ import { ContextsPreviewAPI } from '../ContextsPreviewAPI';
 
 /**
  * This is the framework specific bindings for Preact.
- * '@storybook/preact' expects the returning object from a decorator to be a 'Preact vNode' (vNode).
+ * '@storybook/preact' expects the returning object from a decorator to be a 'Preact vNode'.
  */
 export const renderPreact: Render<Preact.VNode> = (contextNodes, propsMap, getStoryVNode) => {
   const { getRendererFrom } = ContextsPreviewAPI();
