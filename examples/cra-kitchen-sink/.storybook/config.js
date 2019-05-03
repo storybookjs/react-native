113588eb734a06d3072352063c360869400db651
Rob Snow <rsnow@adobe.com>
2019-05-03 14:30:19 -0700

    Sort in story_store so we aren't sorting on every render. Also, we can safely use the options object here because it hasn't been sent over the channel

--- examples/cra-kitchen-sink/.storybook/config.js
+++ examples/cra-kitchen-sink/.storybook/config.js
@@ -20,6 +20,7 @@ addParameters({
       brandUrl: 'https://github.com/storybooks/storybook/tree/master/examples/cra-kitchen-sink',
       gridCellSize: 12,
     }),
+    storySort: (a, b) => a[1].id.localeCompare(b[1].id),
   },
 });
 
