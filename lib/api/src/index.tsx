590f670e46fbdb18d940ee7cf54f97a84d021968
Norbert de Langen <ndelangen@me.com>
2019-05-07 13:59:43 +0200

    FIX linting

--- lib/api/src/index.tsx
+++ lib/api/src/index.tsx
@@ -94,6 +94,10 @@ export type Props = Children & RouterData & ProviderData;
 class ManagerProvider extends Component<Props, State> {
   static displayName = 'Manager';
 
+  api: API;
+
+  modules: any[];
+
   constructor(props: Props) {
     super(props);
     const { provider, location, path, viewMode, storyId, navigate } = props;
@@ -199,10 +203,6 @@ class ManagerProvider extends Component<Props, State> {
     return false;
   }
 
-  api: API;
-
-  modules: any[];
-
   render() {
     const { children } = this.props;
     const value = {
@@ -229,16 +229,16 @@ interface SubState {
 }
 
 class ManagerConsumer extends Component<ConsumerProps<SubState, Combo>> {
-  constructor(props: ConsumerProps<SubState, Combo>) {
-    super(props);
-    this.dataMemory = props.filter ? memoize(10)(props.filter) : null;
-  }
-
   dataMemory?: (combo: Combo) => SubState;
 
   prevChildren?: ReactElement<any> | null;
 
   prevData?: SubState;
+
+  constructor(props: ConsumerProps<SubState, Combo>) {
+    super(props);
+    this.dataMemory = props.filter ? memoize(10)(props.filter) : null;
+  }
 
   render() {
     const { children, pure } = this.props;
