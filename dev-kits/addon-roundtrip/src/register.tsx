276c2466b24b24a57433febee7edc7dc1ac07a10
Norbert de Langen <ndelangen@me.com>
2019-05-14 11:42:11 +0200

    DELETE the placeholder addon panel

--- dev-kits/addon-roundtrip/src/register.tsx
+++ dev-kits/addon-roundtrip/src/register.tsx
@@ -5,15 +5,6 @@ import { ADDON_ID, PANEL_ID } from './constants';
 import { Panel } from './panel';
 
 addons.register(ADDON_ID, () => {
-  addons.add('placeholder', {
-    title: 'empty placeholder',
-    type: types.PANEL,
-    render: ({ active, key }) => (
-      <div hidden={!active} key={key}>
-        {active}Empty indeed
-      </div>
-    ),
-  });
   addons.add(PANEL_ID, {
     title: 'roundtrip',
     type: types.PANEL,
