014655d1d4ecf0119600fe779d3f418b90dbcbde
Norbert de Langen <ndelangen@me.com>
2019-05-20 11:59:50 +0200

    Extract formatter into it's own file and write tests

--- lib/components/src/syntaxhighlighter/syntaxhighlighter.tsx
+++ lib/components/src/syntaxhighlighter/syntaxhighlighter.tsx
@@ -13,6 +13,8 @@ import ReactSyntaxHighlighter, { registerLanguage } from 'react-syntax-highlight
 import { ActionBar } from '../ActionBar/ActionBar';
 import { ScrollArea } from '../ScrollArea/ScrollArea';
 
+import { formatter } from './formatter';
+
 registerLanguage('jsx', jsx);
 registerLanguage('bash', bash);
 registerLanguage('css', css);
@@ -90,31 +92,6 @@ export interface SyntaxHighlighterState {
 }
 
 type ReactSyntaxHighlighterProps = React.ComponentProps<typeof ReactSyntaxHighlighter>;
-
-const formatter = memoize(2)((code: string) => {
-  // code provided to the component is often coming from template literals, which preserve whitespace.
-  // sometimes the first line doesn't have padding, but the second does.
-  // we split the code-string into lines, then if we find padding on line 0 or 1,
-  // we assume that padding is bad, and remove that much padding on all following lines
-  return code
-    .split(/\n/)
-    .reduce(
-      (acc, i, index) => {
-        const match = i.match(/^((:?\s|\t)+)/);
-        const padding = match ? match[1] : '';
-
-        if (acc.firstIndent === '' && padding && index < 3) {
-          return { result: `${acc.result}\n${i.replace(padding, '')}`, firstIndent: padding };
-        }
-        return {
-          result: `${acc.result}\n${i.replace(acc.firstIndent, '')}`,
-          firstIndent: acc.firstIndent,
-        };
-      },
-      { firstIndent: '', result: '' }
-    )
-    .result.trim();
-});
 
 export class SyntaxHighlighter extends Component<
   SyntaxHighlighterProps & ReactSyntaxHighlighterProps,
