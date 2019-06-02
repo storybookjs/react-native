703b5d545db8cd53f028da35f42450a13adabdee
lonyele <skyrabbits1@gmail.com>
2019-06-02 20:59:31 +0900

    Add comment on guarding linear-gradient

--- lib/theming/src/utils.ts
+++ lib/theming/src/utils.ts
@@ -2,6 +2,10 @@ import { rgba, lighten, darken } from 'polished';
 
 export const mkColor = (color: string) => ({ color });
 
+// Passing linear-gradient string to library polished's functions such as
+// lighten or darken throws the error that crashes the entire storybook.
+// It needs to be guarded when arguments of those functions are from
+// user input.
 const isLinearGradient = (color: string) => {
   return typeof color === 'string' && color.includes('linear-gradient');
 };
