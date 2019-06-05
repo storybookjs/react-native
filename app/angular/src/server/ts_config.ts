4fa23b9a801bed6ff1c1eea3539481f5720687ad
Kai Röder <roeder.kai@gmail.com>
2019-06-05 20:54:53 +0200

    WIP: Fix lint errors; add public_api for storybook/angular

--- app/angular/src/server/ts_config.ts
+++ app/angular/src/server/ts_config.ts
@@ -3,19 +3,17 @@ import path from 'path';
 import { logger } from '@storybook/node-logger';
 
 function resolveTsConfig(tsConfigPath: string): string | undefined {
-  if (!fs.existsSync(tsConfigPath)) {
-    return;
+  if (fs.existsSync(tsConfigPath)) {
+    logger.info('=> Found custom tsconfig.json');
+    return tsConfigPath;
   }
-
-  logger.info('=> Found custom tsconfig.json');
-
-  return tsConfigPath;
+  return undefined;
 }
 
 export default function(configDir: string) {
   const configFilePath = resolveTsConfig(path.resolve(configDir, 'tsconfig.json'));
   return {
     transpileOnly: true,
-    configFile: configFilePath ? configFilePath : undefined,
+    configFile: configFilePath || undefined,
   };
 }
