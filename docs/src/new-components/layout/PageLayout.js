e09011f9d1dc4b9956444808df1ff25287459120
Zoltan Olah <zol@hichroma.com>
2019-06-10 10:46:29 -0700

    Makes header consistent with sb.js.org

--- docs/src/new-components/layout/PageLayout.js
+++ docs/src/new-components/layout/PageLayout.js
@@ -4,6 +4,7 @@ const { url } = site;
 
 export const navLinks = [
   { title: 'Docs', href: url.docs.home, isGatsby: false },
+  { title: 'Tutorials', href: url.tutorials, isGatsby: false },
   { title: 'Addons', href: url.addons, isGatsby: false },
   { title: 'Community', href: url.community, isGatsby: false },
   { title: 'Use cases', href: url.useCases, isGatsby: false },
