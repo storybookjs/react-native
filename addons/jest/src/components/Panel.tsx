08fc66f68986a6ca6531012adcb7855d75ee569d
codebyalex <wilsonc.alexander@gmail.com>
2019-06-18 05:57:44 -0400

    modified panel and result of the jest testing addon to look and feel similar to the accessibility addon for a more cohesive experience

--- addons/jest/src/components/Panel.tsx
+++ addons/jest/src/components/Panel.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { styled } from '@storybook/theming';
-import { ScrollArea } from '@storybook/components';
-
-import Indicator from './Indicator';
-import Result, { FailedResult } from './Result';
+import { ScrollArea, TabsState } from '@storybook/components';
+import Result from './Result';
 import provideJestResult, { Test } from '../hoc/provideJestResult';
-import colors from '../colors';
 
 const List = styled.ul({
   listStyle: 'none',
   fontSize: 14,
   padding: 0,
-  margin: '10px 0',
+  margin: 0,
 });
 
 const Item = styled.li({
   display: 'block',
-  margin: '10px 0',
   padding: 0,
 });
 
 const NoTests = styled.div({
   padding: '10px 20px',
   flex: 1,
+});
+
+const ProgressWrapper = styled.div({
+  position: 'relative',
+  height: '10px',
 });
 
 const FileTitle = styled.h2({
@@ -35,15 +36,17 @@ const FileTitle = styled.h2({
 const SuiteHead = styled.div({
   display: 'flex',
   alignItems: 'baseline',
-  justifyContent: 'space-between',
-  position: 'relative',
-  paddingTop: 10,
+  position: 'absolute',
+  zIndex: 2,
+  right: '60px',
+  marginTop: '14px',
+  marginBottom: '14px',
 });
 
 const SuiteTotals = styled(({ successNumber, failedNumber, result, className }) => (
   <div className={className}>
-    {successNumber > 0 && <div style={{ color: colors.success }}>{successNumber} passed</div>}
-    {failedNumber > 0 && <div style={{ color: colors.error }}>{failedNumber} failed</div>}
+    {successNumber > 0 && <div style={{ color: 'green' }}>{successNumber} passed</div>}
+    {failedNumber > 0 && <div style={{ color: 'red' }}>{failedNumber} failed</div>}
     <div>{result.assertionResults.length} total</div>
     <div>
       <strong>
@@ -55,11 +58,11 @@ const SuiteTotals = styled(({ successNumber, failedNumber, result, className }) 
 ))({
   display: 'flex',
   alignItems: 'center',
-  color: colors.grey,
+  color: 'grey',
   fontSize: '10px',
 
   '& > *': {
-    marginLeft: 10,
+    marginRight: 10,
   },
 });
 
@@ -68,18 +71,18 @@ const SuiteProgress = styled(({ successNumber, result, className }) => (
     <span style={{ width: `${(successNumber / result.assertionResults.length) * 100}%` }} />
   </div>
 ))(() => ({
-  width: '100%',
-  backgroundColor: colors.error,
-  height: 4,
+  width: '50px',
+  backgroundColor: 'red',
+  height: '10px',
   top: 0,
   position: 'absolute',
   left: 0,
-  borderRadius: 3,
   overflow: 'hidden',
   appearance: 'none',
+  marginTop: '1px',
 
   '& > span': {
-    backgroundColor: colors.success,
+    backgroundColor: 'green',
     bottom: 0,
     position: 'absolute',
     left: 0,
@@ -87,12 +90,6 @@ const SuiteProgress = styled(({ successNumber, result, className }) => (
     boxShadow: '4px 0 0 white',
   },
 }));
-
-const SuiteTitle = styled.div({
-  display: 'flex',
-  alignItems: 'center',
-  marginBottom: '3px',
-});
 
 const PassingRate = styled.div({
   fontWeight: 500,
@@ -107,54 +104,58 @@ interface ContentProps {
 const Content = styled(({ tests, className }: ContentProps) => (
   <div className={className}>
     {tests.map(({ name, result }) => {
-      const title = name || 'Result status';
-
       if (!result) {
-        return (
-          <NoTests key={title}>This story has tests configured, but no file was found</NoTests>
-        );
+        return <NoTests key={name}>This story has tests configured, but no file was found</NoTests>;
       }
 
       const successNumber = result.assertionResults.filter(({ status }) => status === 'passed')
         .length;
       const failedNumber = result.assertionResults.length - successNumber;
       return (
-        <section key={title}>
-          <SuiteTitle>
-            <FileTitle>{`${title}:`}</FileTitle>
-            <Indicator
-              color={result.status === 'passed' ? colors.success : colors.error}
-              size={16}
-              styles={{ marginRight: 5 }}
-            >
-              {result.status}
-            </Indicator>
-          </SuiteTitle>
+        <section key={name}>
           <SuiteHead>
-            <PassingRate>{`Passing rate: ${(
+            {/* <PassingRate>{`Passing rate: ${(
               (successNumber / result.assertionResults.length) *
               100
-            ).toFixed(2)}%`}</PassingRate>
-            <SuiteProgress {...{ successNumber, failedNumber, result }} />
+            ).toFixed(2)}%`}</PassingRate> */}
             <SuiteTotals {...{ successNumber, failedNumber, result }} />
+            <ProgressWrapper>
+              <SuiteProgress {...{ successNumber, failedNumber, result }} />
+            </ProgressWrapper>
           </SuiteHead>
-          <List>
-            {result.assertionResults.map(res => (
-              <Item key={res.fullName || res.title}>
-                {res.failureMessages && res.failureMessages.length ? (
-                  <FailedResult {...res} />
-                ) : (
-                  <Result {...res} />
-                )}
-              </Item>
-            ))}
-          </List>
+          <TabsState initial="failing-tests">
+            <div id="failing-tests" title={`${failedNumber} Failing`}>
+              <List>
+                {result.assertionResults.map(res => {
+                  if (res.status === 'failed') {
+                    return (
+                      <Item key={res.fullName || res.title}>
+                        <Result {...res} />
+                      </Item>
+                    );
+                  }
+                })}
+              </List>
+            </div>
+            <div id="passing-tests" title={`${successNumber} Passing`}>
+              <List>
+                {result.assertionResults.map(res => {
+                  if (res.status === 'passed') {
+                    return (
+                      <Item key={res.fullName || res.title}>
+                        <Result {...res} />
+                      </Item>
+                    );
+                  }
+                })}
+              </List>
+            </div>
+          </TabsState>
         </section>
       );
     })}
   </div>
 ))({
-  padding: '10px 20px',
   flex: '1 1 0%',
 });
 
--- addons/jest/src/components/Result.tsx
+++ addons/jest/src/components/Result.tsx
@@ -1,89 +1,108 @@
-import React from 'react';
-
+import React, { Component, Fragment } from 'react';
 import { styled } from '@storybook/theming';
-
+import { Icons } from '@storybook/components';
 import Message from './Message';
-import Indicator from './Indicator';
-import colors from '../colors';
 
-const FlexContainer = styled.div({
+const Wrapper = styled.div(({ theme }) => ({
   display: 'flex',
-  alignItems: 'center',
-});
+  width: '100%',
+  borderBottom: `1px solid ${theme.appBorderColor}`,
+  '&:hover': {
+    background: theme.background.hoverable,
+  },
+}));
 
-const Head = styled.header({
+const HeaderBar = styled.div(({ theme }) => ({
+  padding: theme.layoutMargin,
+  paddingLeft: theme.layoutMargin - 3,
+  background: 'none',
+  color: 'inherit',
+  textAlign: 'left',
+  cursor: 'pointer',
+  borderLeft: '3px solid transparent',
+  width: '100%',
   display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'flex-start',
-});
 
-const Title = styled.h3({
-  padding: '10px 10px 0 10px',
-  margin: 0,
-});
+  '&:focus': {
+    outline: '0 none',
+    borderLeft: `3px solid ${theme.color.secondary}`,
+  },
+}));
 
-export const FailedResult = styled(({ fullName, title, status, failureMessages, className }) => (
-  <div className={className}>
-    <Head>
-      <FlexContainer>
-        <Indicator
-          color={colors.error}
-          size={10}
-          overrides={{ borderRadius: '5px 0', position: 'absolute', top: -1, left: -1 }}
-        />
-        <Title>{fullName || title}</Title>
-      </FlexContainer>
-      <Indicator
-        color={colors.error}
-        size={16}
-        overrides={{ borderRadius: '0 5px', position: 'absolute', top: -1, right: -1 }}
-      >
-        {status}
-      </Indicator>
-    </Head>
-    {failureMessages.map((msg: string, i: number) => (
-      // eslint-disable-next-line react/no-array-index-key
-      <Message msg={msg} key={i} />
-    ))}
-  </div>
-))({
-  display: 'block',
-  borderRadius: 5,
-  margin: 0,
-  padding: 0,
-  position: 'relative',
-  border: '1px solid silver',
-  boxSizing: 'border-box',
-});
+const Icon = styled<any, any>(Icons)(({ theme }) => ({
+  height: 10,
+  width: 10,
+  minWidth: 10,
+  color: theme.color.mediumdark,
+  marginRight: '10px',
+  transition: 'transform 0.1s ease-in-out',
+  alignSelf: 'center',
+  display: 'inline-flex',
+}));
+
+const formatSeverityText = (severity: string) => {
+  return severity
+    .charAt(0)
+    .toUpperCase()
+    .concat(severity.slice(1));
+};
 
 interface ResultProps {
   fullName?: string;
   title?: string;
-  status: string;
+  failureMessages: any;
 }
-const Result = ({ fullName, title, status }: ResultProps) => (
-  <div
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    }}
-  >
-    <FlexContainer>
-      <Indicator color={colors.success} size={10} overrides={{ marginRight: 10 }} />
-      <div>{fullName || title}</div>
-    </FlexContainer>
-    <FlexContainer>
-      <Indicator color={colors.success} size={14} right>
-        {status}
-      </Indicator>
-    </FlexContainer>
-  </div>
-);
 
-Result.defaultProps = {
-  fullName: '',
-  title: '',
-};
+interface ResultState {
+  open: boolean;
+}
+
+export class Result extends Component<ResultProps, ResultState> {
+  state = {
+    open: false,
+  };
+
+  onToggle = () =>
+    this.setState(prevState => ({
+      open: !prevState.open,
+    }));
+
+  render() {
+    const { fullName, title, failureMessages } = this.props;
+    const { open } = this.state;
+
+    return (
+      <Fragment>
+        <Wrapper>
+          {failureMessages && failureMessages.length ? (
+            <HeaderBar onClick={this.onToggle} role="button">
+              <Icon
+                icon="chevrondown"
+                size={10}
+                color="#9DA5AB"
+                style={{
+                  transform: `rotate(${open ? 0 : -90}deg)`,
+                }}
+              />
+              <div>{fullName || title}</div>
+            </HeaderBar>
+          ) : (
+            <HeaderBar>
+              <div>{fullName || title}</div>
+            </HeaderBar>
+          )}
+        </Wrapper>
+        {open ? (
+          <Fragment>
+            {failureMessages.map((msg: string, i: number) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <Message msg={msg} key={i} />
+            ))}
+          </Fragment>
+        ) : null}
+      </Fragment>
+    );
+  }
+}
 
 export default Result;
