b4d2fb6ac42cc57078799d932c51a6e1e77f93bf
Norbert de Langen <ndelangen@me.com>
2019-06-19 14:28:09 +0200

    WIP

--- dev-kits/addon-roundtrip/src/index.ts
+++ dev-kits/addon-roundtrip/src/index.ts
@@ -7,8 +7,19 @@ const run = async () => {
   channel.on(EVENTS.REQUEST, () => {
     // do something in the preview context
     // then report back to the manager
+
     channel.emit(EVENTS.RESULT, ['from the preview']);
   });
 };
 
 run();
+
+// import { useChannel } from '@storybook/client-api';
+
+// export const myDecorator = () => {
+
+//   const emit = useChannel({
+//     [EVENTS.REQUEST]: () => {
+//       emit(EVENTS.RESULT, ['from the preview'])}
+//   });
+// };
