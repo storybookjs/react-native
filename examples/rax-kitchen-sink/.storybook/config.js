ebc37f48bc6b34558e7f68a965f7ded826c8a671
Michael Shilman <michael@lab80.co>
2019-06-22 10:44:12 +0800

    Github org rename

--- examples/rax-kitchen-sink/.storybook/config.js
+++ examples/rax-kitchen-sink/.storybook/config.js
@@ -3,7 +3,7 @@ import { configure, addParameters } from '@storybook/rax';
 addParameters({
   options: {
     name: 'Rax Kitchen Sink',
-    url: 'https://github.com/storybooks/storybook/tree/master/examples/rax-kitchen-sink',
+    url: 'https://github.com/storybookjs/storybook/tree/master/examples/rax-kitchen-sink',
     goFullScreen: false,
     showAddonsPanel: true,
     showSearchBox: false,
