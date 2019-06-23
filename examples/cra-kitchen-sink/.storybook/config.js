710b88f3b3a14fa2f571650f6681f50077d10987
Michael Shilman <michael@lab80.co>
2019-06-24 00:23:57 +0800

    Convert cra-kitchen-sink to module format

--- examples/cra-kitchen-sink/.storybook/config.js
+++ examples/cra-kitchen-sink/.storybook/config.js
@@ -1,4 +1,4 @@
-import { configure, addParameters, addDecorator } from '@storybook/react';
+import { load, addParameters, addDecorator } from '@storybook/react';
 import { create } from '@storybook/theming';
 import { withA11y } from '@storybook/addon-a11y';
 
@@ -21,13 +21,4 @@ addParameters({
   },
 });
 
-function loadStories() {
-  // put welcome screen at the top of the list so it's the first one displayed
-  require('../src/stories/welcome');
-
-  // automatically import all story js files that end with *.stories.js
-  const req = require.context('../src/stories', true, /\.stories\.js$/);
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+load(require.context('../src/stories', true, /\.stories\.js$/), module);
--- examples/cra-kitchen-sink/src/stories/index.stories.js
+++ examples/cra-kitchen-sink/src/stories/index.stories.js
@@ -1,5 +1,5 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
 import { Button } from '@storybook/react/demo';
@@ -23,46 +23,53 @@ const InfoButton = () => (
   </span>
 );
 
-storiesOf('Button', module)
-  .addParameters({
+export default {
+  title: 'Button',
+
+  parameters: {
     component: Button,
-  })
-  .add('with text', () => <Button onClick={action('clicked', { depth: 1 })}>Hello Button</Button>, {
-    options: { selectedPanel: 'storybook/actions/panel' },
-  })
-  .add(
-    'with some emoji',
-    () => (
-      <Button onClick={action('clicked')}>
-        <span role="img" aria-label="yolo">
-          😀 😎 👍 💯
-        </span>
-      </Button>
+  },
+};
+
+export const story1 = () => <Button onClick={action('clicked', { depth: 1 })}>Hello Button</Button>;
+story1.title = 'with text';
+story1.parameters = {
+  options: { selectedPanel: 'storybook/actions/panel' },
+};
+
+export const story2 = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="yolo">
+      😀 😎 👍 💯
+    </span>
+  </Button>
+);
+story2.title = 'with some emoji';
+story2.parameters = {
+  options: { selectedPanel: 'storybook/actions/panel' },
+};
+
+export const story3 = () => <Button>Check my notes in the notes panel</Button>;
+story3.title = 'with notes';
+story3.parameters = {
+  notes: 'A very simple button',
+  options: { selectedPanel: 'storybook/notes/panel' },
+};
+
+export const story4 = context => (
+  <Container>
+    <span>
+      click the <InfoButton /> label in top right for info about "{context.name}"
+    </span>
+  </Container>
+);
+story4.title = 'with new info';
+story4.parameters = {
+  notes: 'Composition: Info(Notes())',
+  options: { selectedPanel: 'storybook/info/panel' },
+  decorators: [
+    withInfo(
+      'Use the [info addon](https://github.com/storybookjs/storybook/tree/master/addons/info) with its new painless API.'
     ),
-    {
-      options: { selectedPanel: 'storybook/actions/panel' },
-    }
-  )
-  .add('with notes', () => <Button>Check my notes in the notes panel</Button>, {
-    notes: 'A very simple button',
-    options: { selectedPanel: 'storybook/notes/panel' },
-  })
-  .add(
-    'with new info',
-    context => (
-      <Container>
-        <span>
-          click the <InfoButton /> label in top right for info about "{context.name}"
-        </span>
-      </Container>
-    ),
-    {
-      notes: 'Composition: Info(Notes())',
-      options: { selectedPanel: 'storybook/info/panel' },
-      decorators: [
-        withInfo(
-          'Use the [info addon](https://github.com/storybookjs/storybook/tree/master/addons/info) with its new painless API.'
-        ),
-      ],
-    }
-  );
+  ],
+};
