9677427fee688519c8cf1ea4fd12e229df117cd0
Michael Shilman <michael@lab80.co>
2019-06-28 00:21:36 +0800

    Riot-kitchen-sink: update stories to module format

--- examples/riot-kitchen-sink/src/stories/addon-backgrounds.stories.js
+++ examples/riot-kitchen-sink/src/stories/addon-backgrounds.stories.js
@@ -1,26 +1,37 @@
-import { storiesOf } from '@storybook/riot';
 import ButtonRaw from './Button.txt';
 
-storiesOf('Addon|Backgrounds', module)
-  .addParameters({
+export default {
+  title: 'Addon|Backgrounds',
+  parameters: {
     backgrounds: [
       { name: 'light', value: '#eeeeee' },
       { name: 'dark', value: '#222222', default: true },
     ],
-  })
-  .add('story 1', () => {
-    const content = 'You should be able to switch backgrounds for this story';
+  },
+};
 
-    return {
-      tags: [{ boundAs: 'my-button', content: ButtonRaw }],
-      template: `<my-button>${content}</my-button>`,
-    };
-  })
-  .add('story 2', () => {
-    const content = 'This one too!';
+export const story1 = () => {
+  const content = 'You should be able to switch backgrounds for this story';
 
-    return {
-      tags: [{ boundAs: 'my-button', content: ButtonRaw }],
-      template: `<my-button>${content}</my-button>`,
-    };
-  });
+  return {
+    tags: [{ boundAs: 'my-button', content: ButtonRaw }],
+    template: `<my-button>${content}</my-button>`,
+  };
+};
+
+story1.story = {
+  name: 'story 1',
+};
+
+export const story2 = () => {
+  const content = 'This one too!';
+
+  return {
+    tags: [{ boundAs: 'my-button', content: ButtonRaw }],
+    template: `<my-button>${content}</my-button>`,
+  };
+};
+
+story2.story = {
+  name: 'story 2',
+};
