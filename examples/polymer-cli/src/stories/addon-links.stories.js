07c2df0506c5ce330a9a6c6ae07937c752237eb0
Michael Shilman <michael@lab80.co>
2019-06-27 23:37:03 +0800

    Polymer-cli: update stories to module format

--- examples/polymer-cli/src/stories/addon-links.stories.js
+++ examples/polymer-cli/src/stories/addon-links.stories.js
@@ -1,12 +1,19 @@
-import { storiesOf } from '@storybook/polymer';
 import { linkTo } from '@storybook/addon-links';
 import { document } from 'global';
 
 import '../simple-button.html';
 
-storiesOf('Addon|Links', module).add('With Create Element', () => {
+export default {
+  title: 'Addon|Links',
+};
+
+export const withCreateElement = () => {
   const el = document.createElement('simple-button');
   el.title = 'Go to welcome';
   el.handleClick = linkTo('Welcome');
   return el;
-});
+};
+
+withCreateElement.story = {
+  name: 'With Create Element',
+};
