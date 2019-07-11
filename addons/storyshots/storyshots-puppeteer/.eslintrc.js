0218cb5e1a2a7bbe094b859f9748c96851af6754
Norbert de Langen <ndelangen@me.com>
2019-07-11 10:55:46 +0200

    IMPROVE linting config

--- addons/storyshots/storyshots-puppeteer/.eslintrc.js
+++ addons/storyshots/storyshots-puppeteer/.eslintrc.js
@@ -1,10 +1,5 @@
-const error = 2;
-const warn = 1;
-const ignore = 0;
-
 module.exports = {
-  rules: {
-    'import/no-unresolved': ignore,
-    'import/extensions': ignore,
+  settings: {
+    'import/core-modules': ['puppeteer'],
   },
 };
