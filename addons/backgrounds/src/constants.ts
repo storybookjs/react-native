b5b4deb7b17b77bc2c28d6fd10886ba531eb6ebe
Norbert de Langen <ndelangen@me.com>
2019-07-13 01:12:09 +0200

    MOVE grid functionality to background addon

--- addons/backgrounds/src/constants.ts
+++ addons/backgrounds/src/constants.ts
@@ -1,4 +1,6 @@
 export const ADDON_ID = 'storybook/background';
+export const BACKGROUND = `${ADDON_ID}/background`;
+export const GRID = `${ADDON_ID}/grid`;
 export const PARAM_KEY = 'backgrounds';
 
 export const EVENTS = {
