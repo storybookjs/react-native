4df0199382535dbba93bd14b842f01af17de3b87
codebyalex <wilsonc.alexander@gmail.com>
2019-07-15 21:04:25 -0400

    made the progress information mobile friendly

--- addons/jest/src/components/Panel.tsx
+++ addons/jest/src/components/Panel.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { styled } from '@storybook/theming';
 import { ScrollArea, TabsState } from '@storybook/components';
+import { SizeMe } from 'react-sizeme';
 import Result from './Result';
 import provideJestResult, { Test } from '../hoc/provideJestResult';
 
@@ -32,26 +33,34 @@ const SuiteHead = styled.div({
   position: 'absolute',
   zIndex: 2,
   right: '60px',
-  marginTop: '13px',
+  marginTop: '15px',
 });
 
-const SuiteTotals = styled(({ successNumber, failedNumber, result, className }) => (
+const SuiteTotals = styled(({ successNumber, failedNumber, result, className, width }) => (
   <div className={className}>
-    <div>
-      {result.assertionResults.length} {result.assertionResults.length > 1 ? `tests` : `test`}
-    </div>
-    <div>
-      {result.endTime - result.startTime}
-      ms
-    </div>
-    <div>{((successNumber / (failedNumber + successNumber)) * 100).toFixed(2)}%</div>
+    {width > 380 ? (
+      <Fragment>
+        <div>
+          {result.assertionResults.length} {result.assertionResults.length > 1 ? `tests` : `test`}
+        </div>
+        <div>
+          {result.endTime - result.startTime}
+          ms
+        </div>
+      </Fragment>
+    ) : null}
+    {width > 280 ? (
+      <Fragment>
+        <div>{((successNumber / (failedNumber + successNumber)) * 100).toFixed(2)}%</div>
+      </Fragment>
+    ) : null}
   </div>
 ))({
   display: 'flex',
   alignItems: 'center',
   color: '#666666',
   fontSize: '14px',
-  marginTop: '-2px',
+  marginTop: '-5px',
   '& > *': {
     marginRight: 10,
   },
@@ -98,39 +107,49 @@ const Content = styled(({ tests, className }: ContentProps) => (
         .length;
       const failedNumber = result.assertionResults.length - successNumber;
       const passingRate = ((successNumber / result.assertionResults.length) * 100).toFixed(2);
+
       return (
-        <section key={name}>
-          <SuiteHead>
-            <SuiteTotals {...{ successNumber, failedNumber, result, passingRate }} />
-            <ProgressWrapper>
-              <SuiteProgress {...{ successNumber, failedNumber, result }} />
-            </ProgressWrapper>
-          </SuiteHead>
-          <TabsState initial="failing-tests" backgroundColor="rgba(0,0,0,.05)">
-            <div id="failing-tests" title={`${failedNumber} Failed`} color="#FF4400">
-              <List>
-                {result.assertionResults.map(res => {
-                  return res.status === 'failed' ? (
-                    <Item key={res.fullName || res.title}>
-                      <Result {...res} />
-                    </Item>
-                  ) : null;
-                })}
-              </List>
-            </div>
-            <div id="passing-tests" title={`${successNumber} Passed`} color="#66BF3C">
-              <List>
-                {result.assertionResults.map(res => {
-                  return res.status === 'passed' ? (
-                    <Item key={res.fullName || res.title}>
-                      <Result {...res} />
-                    </Item>
-                  ) : null;
-                })}
-              </List>
-            </div>
-          </TabsState>
-        </section>
+        <SizeMe refreshMode="debounce">
+          {({ size }: { size: any }) => {
+            const { width } = size;
+            return (
+              <section key={name}>
+                <SuiteHead>
+                  <SuiteTotals {...{ successNumber, failedNumber, result, passingRate, width }} />
+                  {width > 240 ? (
+                    <ProgressWrapper>
+                      <SuiteProgress {...{ successNumber, failedNumber, result }} />
+                    </ProgressWrapper>
+                  ) : null}
+                </SuiteHead>
+                <TabsState initial="failing-tests" backgroundColor="rgba(0,0,0,.05)">
+                  <div id="failing-tests" title={`${failedNumber} Failed`} color="#FF4400">
+                    <List>
+                      {result.assertionResults.map(res => {
+                        return res.status === 'failed' ? (
+                          <Item key={res.fullName || res.title}>
+                            <Result {...res} />
+                          </Item>
+                        ) : null;
+                      })}
+                    </List>
+                  </div>
+                  <div id="passing-tests" title={`${successNumber} Passed`} color="#66BF3C">
+                    <List>
+                      {result.assertionResults.map(res => {
+                        return res.status === 'passed' ? (
+                          <Item key={res.fullName || res.title}>
+                            <Result {...res} />
+                          </Item>
+                        ) : null;
+                      })}
+                    </List>
+                  </div>
+                </TabsState>
+              </section>
+            );
+          }}
+        </SizeMe>
       );
     })}
   </div>
