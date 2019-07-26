f083e737cfe85c5191977db5627df402b8fe0ab1
Michael Shilman <michael@lab80.co>
2019-07-27 03:50:36 +0800

    Addon-docs: Configure JSX automagically

--- addons/docs/common/makePreset.js
+++ addons/docs/common/makePreset.js
@@ -4,13 +4,17 @@ const common = require('./preset');
 module.exports = framework => {
   const frameworkConfig = `${__dirname}/../${framework}/config.js`;
   const preconfig = fs.existsSync(frameworkConfig) ? [frameworkConfig] : [];
-
   function config(entry = []) {
     return [...preconfig, ...entry];
   }
 
+  const configureJSX = framework !== 'react';
+  const webpack = (webpackConfig, options) =>
+    common.webpack(webpackConfig, { configureJSX, ...options });
+
   return {
     ...common,
+    webpack,
     config,
   };
 };
