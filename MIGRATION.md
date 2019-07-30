28fdb3d3acd8bbecb8662d7f0f8278bedfb34337
Gert Hengeveld <info@ghengeveld.nl>
2019-07-30 16:39:03 +0200

    Use a sensible sort order for stories and suggest it in the docs.

--- MIGRATION.md
+++ MIGRATION.md
@@ -87,7 +87,8 @@ For example, here's how to sort by story ID using `storySort`:
 ```js
 addParameters({
   options: {
-    storySort: (a, b) => a[1].id.localeCompare(b[1].id),
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, { numeric: true }),
   },
 });
 ```
--- examples/official-storybook/config.js
+++ examples/official-storybook/config.js
@@ -49,6 +49,8 @@ addParameters({
     hierarchySeparator: /\/|\./,
     hierarchyRootSeparator: '|',
     theme: themes.light, // { base: 'dark', brandTitle: 'Storybook!' },
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, { numeric: true }),
   },
   backgrounds: [
     { name: 'storybook app', value: themes.light.appBg, default: true },
