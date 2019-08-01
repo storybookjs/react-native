18abecafd2c9269e500d64f9862cbc402ee6febc
Michael Shilman <michael@lab80.co>
2019-08-01 16:32:21 +0800

    Addon-docs: Empty string if we can't infer description

--- addons/docs/src/blocks/Description.tsx
+++ addons/docs/src/blocks/Description.tsx
@@ -20,13 +20,14 @@ interface DescriptionProps {
   markdown?: string;
 }
 
-const getNotes = (notes?: Notes) =>
-  (notes && (typeof notes === 'string' ? notes : notes.markdown || notes.text)) || '';
+const str = (o: any) => (typeof o === 'string' ? o : '');
 
-const getInfo = (info?: Info) => (info && (typeof info === 'string' ? info : info.text)) || '';
+const getNotes = (notes?: Notes) => notes && (str(notes) || str(notes.markdown) || str(notes.text));
+
+const getInfo = (info?: Info) => info && (str(info) || str(info.text));
 
 const getDocgen = (component?: Component) =>
-  (component && component.__docgenInfo && component.__docgenInfo.description) || '';
+  component && component.__docgenInfo && str(component.__docgenInfo.description);
 
 export const getDescriptionProps = (
   { of, type, markdown }: DescriptionProps,
@@ -50,7 +51,7 @@ export const getDescriptionProps = (
         markdown: `
 ${getNotes(notes) || getInfo(info) || ''}
 
-${getDocgen(target)}
+${getDocgen(target) || ''}
 `.trim(),
       };
   }
