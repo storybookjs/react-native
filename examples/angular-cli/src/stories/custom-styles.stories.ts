c560ee2d8a5392b0928188fb175aded690a3e9ac
Michael Shilman <michael@lab80.co>
2019-08-03 10:00:56 +0800

    Angular: Convert angular-cli stories to CSF

--- examples/angular-cli/src/stories/custom-styles.stories.ts
+++ examples/angular-cli/src/stories/custom-styles.stories.ts
@@ -1,47 +1,54 @@
-import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { Button } from '@storybook/angular/demo';
 
-storiesOf('Custom|Style', module)
-  .addDecorator(
+export default {
+  title: 'Custom|Style',
+  decorators: [
     moduleMetadata({
       declarations: [Button],
-    })
-  )
-  .add('Default', () => ({
-    template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
-    props: {
-      text: 'Button with custom styles',
-      onClick: action('log'),
-    },
-    styles: [
-      `
+    }),
+  ],
+};
+
+export const defaultStory = () => ({
+  template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+  props: {
+    text: 'Button with custom styles',
+    onClick: action('log'),
+  },
+  styles: [
+    `
       storybook-button-component {
         background-color: yellow;
         padding: 25px;
       }
     `,
-    ],
-  }))
-  .add(
-    'With Knobs',
-    () => ({
-      template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
-      props: {
-        text: text('text', 'Button with custom styles'),
-        onClick: action('log'),
-      },
-      styles: [
-        `
-      storybook-button-component {
-        background-color: red;
-        padding: 25px;
-      }
-    `,
-      ],
-    }),
-    {
-      decorators: [withKnobs],
-    }
-  );
+  ],
+});
+
+defaultStory.story = {
+  name: 'Default',
+};
+
+export const withKnobsStory = () => ({
+  template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+  props: {
+    text: text('text', 'Button with custom styles'),
+    onClick: action('log'),
+  },
+  styles: [
+    `
+  storybook-button-component {
+    background-color: red;
+    padding: 25px;
+  }
+`,
+  ],
+});
+
+withKnobsStory.story = {
+  name: 'With Knobs',
+  decorators: [withKnobs],
+};
