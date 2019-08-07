10777e7fb3e33835ae21fda77593044b473f7675
Mario <mariocads@gmail.com>
2019-08-07 08:55:36 +0200

    using makedecorator on export function to be able to disable center addon

--- addons/centered/src/react.tsx
+++ addons/centered/src/react.tsx
@@ -1,13 +1,20 @@
 import React, { ReactNode } from 'react';
+import { makeDecorator, StoryFn } from '@storybook/addons';
 import styles from './styles';
 
-export default function(storyFn: () => ReactNode) {
+function centered(storyFn: () => ReactNode) {
   return (
     <div style={styles.style}>
       <div style={styles.innerStyle}>{storyFn()}</div>
     </div>
   );
 }
+
+export default makeDecorator({
+  name: 'centered',
+  parameterName: 'centered',
+  wrapper: getStory => centered(getStory as StoryFn),
+});
 
 if (module && module.hot && module.hot.decline) {
   module.hot.decline();
