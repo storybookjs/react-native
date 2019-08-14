0a92f4734efba96e9ec588d11a9f338a64170d87
Michael Shilman <michael@lab80.co>
2019-08-15 01:31:13 +0800

    Addon-docs: Refactor HTML component overrides

--- addons/docs/src/blocks/DocsContainer.tsx
+++ addons/docs/src/blocks/DocsContainer.tsx
@@ -4,21 +4,13 @@ import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { ThemeProvider, ensure as ensureTheme } from '@storybook/theming';
 import { DocsWrapper, DocsContent } from '@storybook/components';
-import * as html from '@storybook/components/html';
+import { components as defaultComponents } from '@storybook/components/html';
 import { DocsContextProps, DocsContext } from './DocsContext';
 
 interface DocsContainerProps {
   context: DocsContextProps;
   content: React.ElementType<any>;
 }
-
-const defaultComponents = Object.entries(html).reduce(
-  (acc, [k, v]) => ({
-    ...acc,
-    [k.toLowerCase()]: v,
-  }),
-  {}
-);
 
 export const DocsContainer: React.FunctionComponent<DocsContainerProps> = ({
   context,
--- lib/components/src/html.ts
+++ lib/components/src/html.ts
@@ -1,1 +1,11 @@
+import * as rawComponents from './typography/DocumentFormatting';
+
 export * from './typography/DocumentFormatting';
+
+export const components = Object.entries(rawComponents).reduce(
+  (acc, [k, v]) => ({
+    ...acc,
+    [k.toLowerCase()]: v,
+  }),
+  {}
+);
