0bb8c5980a4c4fe308a610c256eb6bce77e8498d
Michael Shilman <michael@lab80.co>
2019-08-16 14:24:44 +0800

    Addon-docs: Preview with toolbar stories

--- lib/components/src/blocks/Preview.stories.tsx
+++ lib/components/src/blocks/Preview.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
 
 import { Preview } from './Preview';
+import { Story } from './Story';
 import { DocsPageWrapper } from './DocsPage';
 import { Button } from '../Button/Button';
 import * as sourceStories from './Source.stories';
@@ -78,5 +78,20 @@ export const gridWith3Columns = () => (
     <Button secondary>Button 18</Button>
     <Button secondary>Button 19</Button>
     <Button secondary>Button 20</Button>
+  </Preview>
+);
+
+const buttonFn = () => <Button secondary>Hello Button</Button>;
+
+export const withToolbar = () => (
+  <Preview withToolbar>
+    <Story inline storyFn={buttonFn} title="with toolbar" />
+  </Preview>
+);
+
+export const withToolbarMulti = () => (
+  <Preview withToolbar>
+    <Story inline storyFn={buttonFn} title="story1" />
+    <Story inline storyFn={buttonFn} title="story2" />
   </Preview>
 );
