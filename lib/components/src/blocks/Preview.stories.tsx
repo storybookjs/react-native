93011e5f28f12ec60c602f5260aa0a23069966ba
Michael Shilman <michael@lab80.co>
2019-08-17 20:24:41 +0800

    Revert "Addon-docs: Fix snapshots & chromatic"
    This reverts commit 8e853799ea6394a444d4e3fbbd94b34714f00f17.

--- lib/components/src/blocks/Preview.stories.tsx
+++ lib/components/src/blocks/Preview.stories.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
+
 import { Preview } from './Preview';
 
 import { Story } from './Story';
 import { Button } from '../Button/Button';
-
-import { DocsPageWrapper } from './DocsPage';
 import * as sourceStories from './Source.stories';
 
 export default {
   title: 'Docs|Preview',
   component: Preview,
-  decorators: [getStory => <DocsPageWrapper>{getStory()}</DocsPageWrapper>],
 };
 
 export const codeCollapsed = () => (
