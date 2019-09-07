85dc7173784f4a73e6621d2131f44ad5d8eb4eeb
atanasster <atanasster@gmail.com>
2019-09-08 02:46:40 +0300

    mdx-js parsing issue with empty lines

--- addons/docs/src/mdx/mdx-compiler-plugin.js
+++ addons/docs/src/mdx/mdx-compiler-plugin.js
@@ -202,38 +202,53 @@ const hasStoryChild = node => {
 function extractExports(node, options) {
   node.children.forEach(child => {
     if (child.type === 'jsx') {
-      const ast = parser.parseExpression(child.value, { plugins: ['jsx'] });
-      if (
-        ast.openingElement &&
-        ast.openingElement.type === 'JSXOpeningElement' &&
-        ast.openingElement.name.name === 'Preview' &&
-        !hasStoryChild(ast)
-      ) {
-        const previewAst = ast.openingElement;
-        previewAst.attributes.push({
-          type: 'JSXAttribute',
-          name: {
-            type: 'JSXIdentifier',
-            name: 'mdxSource',
-          },
-          value: {
-            type: 'StringLiteral',
-            value: encodeURI(
-              ast.children
-                .map(
-                  el =>
-                    generate(el, {
-                      quotes: 'double',
-                    }).code
-                )
-                .join('\n')
-            ),
-          },
-        });
+      try {
+        const ast = parser.parseExpression(child.value, { plugins: ['jsx'] });
+        if (
+          ast.openingElement &&
+          ast.openingElement.type === 'JSXOpeningElement' &&
+          ast.openingElement.name.name === 'Preview' &&
+          !hasStoryChild(ast)
+        ) {
+          const previewAst = ast.openingElement;
+          previewAst.attributes.push({
+            type: 'JSXAttribute',
+            name: {
+              type: 'JSXIdentifier',
+              name: 'mdxSource',
+            },
+            value: {
+              type: 'StringLiteral',
+              value: encodeURI(
+                ast.children
+                  .map(
+                    el =>
+                      generate(el, {
+                        quotes: 'double',
+                      }).code
+                  )
+                  .join('\n')
+              ),
+            },
+          });
+        }
+        const { code } = generate(ast, {});
+        // eslint-disable-next-line no-param-reassign
+        child.value = code;
+      } catch {
+        /** catch erroneous child.value string where the babel parseExpression makes exception
+         * https://github.com/mdx-js/mdx/issues/767
+         * eg <button>
+         *      <div>hello world</div>
+         *
+         *    </button>
+         * generates error
+         * 1. child.value =`<button>\n  <div>hello world</div`
+         * 2. child.value =`\n`
+         * 3. child.value =`</button>`
+         *
+         */
       }
-      const { code } = generate(ast, {});
-      // eslint-disable-next-line no-param-reassign
-      child.value = code;
     }
   });
   // we're overriding default export
