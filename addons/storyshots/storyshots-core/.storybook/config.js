41039d0ea593ccdc08e474c704352cfd9b484b3b
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2019-10-08 01:18:26 +0200

    Try adding an mdx story

--- addons/storyshots/storyshots-core/.storybook/config.js
+++ addons/storyshots/storyshots-core/.storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/react';
 
 configure(
   [
-    require.context('../stories/required_with_context', false, /\.stories\.js$/),
+    require.context('../stories/required_with_context', false, /\.stories\.(js|mdx)$/),
     require.context('../stories/directly_required', false, /index\.js$/),
   ],
   module
