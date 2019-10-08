robaxelsen/storybook

refactor: move addon-storyshots framework base file to TS and types them

addons/storyshots/storyshots-core/src/frameworks/hasDependency.ts
@@ -1,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import readPkgUp from 'read-pkg-up';
+
+const {
+  package: { dependencies, devDependencies } = {
+    dependencies: undefined,
+    devDependencies: undefined,
+  },
+} = readPkgUp.sync() || {};
+
+export default function hasDependency(name: string): boolean {
+  return Boolean(
+    (devDependencies && devDependencies[name]) ||
+      (dependencies && dependencies[name]) ||
+      fs.existsSync(path.join('node_modules', name, 'package.json'))
+  );
+}

storybookjs/react-native

addons/storyshots/storyshots-core/src/frameworks/hasDependency.ts
@@ -1,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import readPkgUp from 'read-pkg-up';
+
+const {
+  packageJson: { dependencies, devDependencies } = {
+    dependencies: undefined,
+    devDependencies: undefined,
+  },
+} = readPkgUp.sync() || {};
+
+export default function hasDependency(name: string): boolean {
+  return Boolean(
+    (devDependencies && devDependencies[name]) ||
+      (dependencies && dependencies[name]) ||
+      fs.existsSync(path.join('node_modules', name, 'package.json'))
+  );
+}