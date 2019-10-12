4fb8e874ce58366970b575a5564f1f2f9e5661f4
Norbert de Langen <ndelangen@me.com>
2019-10-12 12:29:03 +0200

    ADD first cypress test

---
+++ cypress/integration/mine.spec.js
@@ -0,0 +1,11 @@
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8001/official-storybook/');
+  });
+
+  it('do something', () => {
+    cy.contains('Invalid contrast').click();
+
+    cy.url().should('include', 'path=/story/addons-a11y-basebutton--invalid-contrast');
+  });
+});
