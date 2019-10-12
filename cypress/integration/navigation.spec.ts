561c9e93dec02e6c2fe9797c8eddb32f770426a8
Kai Röder <roeder.kai@gmail.com>
2019-10-12 18:55:04 +0200

    add navigation click, routing and searching navigation items

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -1,13 +1,31 @@
 import { visitApp } from '../helper';
 
-describe('Actions', () => {
+describe('Navigation and routing', () => {
   beforeEach(() => {
     visitApp('official-storybook');
   });
 
-  it('do something', () => {
-    cy.contains('Invalid contrast').click();
+  it('should navigate to story addons-a11y-basebutton--default', () => {
+    cy.get('#exploreraddons-a11y-basebutton--label').click();
 
-    cy.url().should('include', 'path=/story/addons-a11y-basebutton--invalid-contrast');
+    cy.url().should('include', 'path=/story/addons-a11y-basebutton--label');
+  });
+
+  it('should search navigation item', () => {
+    cy.get('#storybook-explorer-searchfield')
+      .click()
+      .type('persisting the action logger');
+
+    cy.get('.sidebar-container a')
+      .should('contain', 'Persisting the action logger')
+      .and('not.contain', 'a11y');
+  });
+
+  it('should display no results after searching a non-existing navigation item', () => {
+    cy.get('#storybook-explorer-searchfield')
+      .click()
+      .type('zzzzzzzzzz');
+
+    cy.get('.sidebar-container').should('contain', 'This filter resulted in 0 results');
   });
 });
