0e0a66370ba6ca4f071e0d7b5877816a509b89c9
atanasster <atanasster@gmail.com>
2019-10-29 16:02:38 +0200

    using types.PREVIEW wrapper to enclose the iframe

---
+++ dev-kits/addon-preview-wrapper/src/constants.ts
@@ -0,0 +1,1 @@
+export const ADDON_ID = 'storybook/preview-wrapper';
--- examples/dev-kits/addons.js
+++ examples/dev-kits/addons.js
@@ -1,2 +1,3 @@
 import '@storybook/addon-roundtrip/register';
 import '@storybook/addon-parameter/register';
+import '@storybook/addon-preview-wrapper/register';
