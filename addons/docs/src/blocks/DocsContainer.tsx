629119c1ab6f50cb9b8370e6a46ef14c66625b9b
Michael Shilman <shilman@users.noreply.github.com>
2019-10-30 16:21:24 +0800

    Addon-docs: Fix story scroll-to heuristics (#8629)
    Addon-docs: Fix story scroll-to heuristics

--- addons/docs/src/blocks/DocsContainer.tsx
+++ addons/docs/src/blocks/DocsContainer.tsx
@@ -57,9 +57,14 @@ export const DocsContainer: React.FunctionComponent<DocsContainerProps> = ({
       element = document.getElementById(storyBlockIdFromId(storyId));
     }
     if (element) {
+      const allStories = element.parentElement.querySelectorAll('[id|="anchor-"]');
+      let block = 'start';
+      if (allStories && allStories[0] === element) {
+        block = 'end'; // first story should be shown with the intro content above
+      }
       element.scrollIntoView({
         behavior: 'smooth',
-        block: 'end',
+        block,
         inline: 'nearest',
       });
     }
