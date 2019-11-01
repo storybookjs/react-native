ae8bea7e87248b5c80474c755bb342467f251edf
Michael Shilman <michael@lab80.co>
2019-11-01 21:32:54 +0800

    Update config.ts

--- examples/angular-cli/.storybook/config.ts
+++ examples/angular-cli/.storybook/config.ts
@@ -4,6 +4,7 @@ import { setCompodocJson } from '@storybook/addon-docs/angular';
 import addCssWarning from '../src/cssWarning';
 
 // @ts-ignore
+// eslint-disable-next-line import/extensions, import/no-unresolved
 import docJson from '../documentation.json';
 
 setCompodocJson(docJson);
