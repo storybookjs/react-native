e9e4ac195e735febee0cb0fce6242b9429ac5656
Tom Coleman <tom@thesnail.org>
2019-11-12 13:02:34 +1100

    Added migration notes and updated docs

--- addons/docs/src/blocks/DocsPage.tsx
+++ addons/docs/src/blocks/DocsPage.tsx
@@ -49,13 +49,26 @@ interface StoryData {
 
 const defaultTitleSlot: StringSlot = ({ selectedKind, parameters }) => {
   const {
+    showRoots,
     hierarchyRootSeparator: rootSeparator,
     hierarchySeparator: groupSeparator,
   } = (parameters && parameters.options) || {
+    showRoots: undefined,
     hierarchyRootSeparator: '|',
     hierarchySeparator: /\/|\./,
   };
-  const { groups } = parseKind(selectedKind, { rootSeparator, groupSeparator });
+
+  let groups;
+  if (typeof showRoots !== 'undefined') {
+    groups = selectedKind.split('/');
+  } else {
+    // This covers off all the remaining cases:
+    //   - If the separators were set above, we should use them
+    //   - If they weren't set, we should only should use the old defaults if the kind contains '.' or '|',
+    //     which for this particular splitting is the only case in which it actually matters.
+    ({ groups } = parseKind(selectedKind, { rootSeparator, groupSeparator }));
+  }
+
   return (groups && groups[groups.length - 1]) || selectedKind;
 };
 
