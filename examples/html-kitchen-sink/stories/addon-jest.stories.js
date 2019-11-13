7e549c8b75c4577148b29c9491c3c8bd09f637d0
Michael Shilman <michael@lab80.co>
2019-11-13 18:17:20 +0800

    Examples: Upgrade html-kitchen-sink to simplified hierarchy separator

--- examples/html-kitchen-sink/stories/addon-jest.stories.js
+++ examples/html-kitchen-sink/stories/addon-jest.stories.js
@@ -2,7 +2,7 @@ import { withTests as wt } from '@storybook/addon-jest';
 import results from './addon-jest.testresults.json';
 
 export default {
-  title: 'Addons|Jest',
+  title: 'Addons/Jest',
   decorators: [wt({ results })],
 };
 
