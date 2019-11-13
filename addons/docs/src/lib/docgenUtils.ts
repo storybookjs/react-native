1db7e4321d1352cc1ce4239ee87792e9e8935965
patrick.lafrance <patrick.lafrance@outlook.com>
2019-11-12 20:45:33 -0500

    Merge thing to the new utils

--- addons/docs/src/lib/docgenUtils.ts
+++ addons/docs/src/lib/docgenUtils.ts
@@ -6,21 +6,8 @@ export type PropsExtractor = (component: Component) => PropsTableProps | null;
 
 export type PropDefGetter = (component: Component, section: string) => PropDef[];
 
-export const str = (o: any) => {
-  if (!o) {
-    return '';
-  }
-  if (typeof o === 'string') {
-    return o as string;
-  }
-  throw new Error(`Description: expected string, got: ${JSON.stringify(o)}`);
-};
-
 export const hasDocgen = (component: Component) => !!component.__docgenInfo;
 
 export const extractPropsFromDocgen: PropDefGetter = (component, section) => {
   return [];
 };
-
-export const extractComponentDescription = (component?: Component) =>
-  component && component.__docgenInfo && str(component.__docgenInfo.description);
--- addons/docs/src/lib2/docgen/utils.ts
+++ addons/docs/src/lib2/docgen/utils.ts
@@ -3,6 +3,16 @@
 import { isNil } from 'lodash';
 import { Component } from '../../blocks/shared';
 
+export const str = (obj: any) => {
+  if (!obj) {
+    return '';
+  }
+  if (typeof obj === 'string') {
+    return obj as string;
+  }
+  throw new Error(`Description: expected string, got: ${JSON.stringify(obj)}`);
+};
+
 export function hasDocgen(component: Component): boolean {
   return !!component.__docgenInfo;
 }
@@ -14,3 +24,6 @@ export function isValidDocgenSection(docgenSection: any) {
 export function getDocgenSection(component: Component, section: string): any {
   return hasDocgen(component) ? component.__docgenInfo[section] : null;
 }
+
+export const extractComponentDescription = (component?: Component) =>
+  component && hasDocgen(component) && str(component.__docgenInfo.description);
