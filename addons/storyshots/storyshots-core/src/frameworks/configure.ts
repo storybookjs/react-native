79405c4ef5ce1b47fa86696c039aab16ccd188dd
Norbert de Langen <ndelangen@me.com>
2019-11-14 13:37:56 +0100

    CHANGE to use configure over copied loadStories fn

--- addons/storyshots/storyshots-core/src/frameworks/configure.ts
+++ addons/storyshots/storyshots-core/src/frameworks/configure.ts
@@ -2,10 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import glob from 'glob';
 import { ClientApi } from '@storybook/client-api';
-// @ts-ignore I don't know why this isn't in the interface, it should be
-import { storyNameFromExport } from '@storybook/router/utils';
-import { DecoratorFunction } from '@storybook/addons';
 import { StoryshotsOptions } from '../api/StoryshotsOptions';
+
+// @ts-ignore I don't know why this isn't in the interface, it should be
+// import { storyNameFromExport } from '@storybook/router/utils';
+// import { DecoratorFunction } from '@storybook/addons';
 
 const isFile = (file: string): boolean => {
   try {
@@ -20,69 +21,69 @@ interface Output {
   files: string[];
 }
 
-function matches(storyKey: string, arrayOrRegex: string[] | RegExp) {
-  if (Array.isArray(arrayOrRegex)) {
-    return arrayOrRegex.includes(storyKey);
-  }
-  return storyKey.match(arrayOrRegex);
-}
+// function matches(storyKey: string, arrayOrRegex: string[] | RegExp) {
+//   if (Array.isArray(arrayOrRegex)) {
+//     return arrayOrRegex.includes(storyKey);
+//   }
+//   return storyKey.match(arrayOrRegex);
+// }
 
-function isExportStory(key: string, { includeStories, excludeStories }: any) {
-  return (
-    // https://babeljs.io/docs/en/babel-plugin-transform-modules-commonjs
-    key !== '__esModule' &&
-    (!includeStories || matches(key, includeStories)) &&
-    (!excludeStories || !matches(key, excludeStories))
-  );
-}
+// function isExportStory(key: string, { includeStories, excludeStories }: any) {
+//   return (
+//     // https://babeljs.io/docs/en/babel-plugin-transform-modules-commonjs
+//     key !== '__esModule' &&
+//     (!includeStories || matches(key, includeStories)) &&
+//     (!excludeStories || !matches(key, excludeStories))
+//   );
+// }
 
-const loadStories = (loadable: string, framework: string, clientApi: ClientApi) => {
-  const currentExports = new Map();
+// const loadStories = (loadable: string, framework: string, clientApi: ClientApi) => {
+//   const currentExports = new Map();
 
-  // eslint-disable-next-line global-require, import/no-dynamic-require
-  const exported = require(loadable);
+//   // eslint-disable-next-line global-require, import/no-dynamic-require
+//   const exported = require(loadable);
 
-  // An old-style story file
-  if (!exported.default) {
-    return;
-  }
+//   // An old-style story file
+//   if (!exported.default) {
+//     return;
+//   }
 
-  if (!exported.default.title) {
-    throw new Error(`Unexpected default export without title: ${JSON.stringify(exported.default)}`);
-  }
+//   if (!exported.default.title) {
+//     throw new Error(`Unexpected default export without title: ${JSON.stringify(exported.default)}`);
+//   }
 
-  const { default: meta, ...exports } = exported;
-  const { title: kindName, parameters: params, decorators: decos, component } = meta;
+//   const { default: meta, ...exports } = exported;
+//   const { title: kindName, parameters: params, decorators: decos, component } = meta;
 
-  // @ts-ignore We pass true here to avoid the warning about HMR. It's cool clientApi, we got this
-  const kind = clientApi.storiesOf(kindName, true);
+//   // @ts-ignore We pass true here to avoid the warning about HMR. It's cool clientApi, we got this
+//   const kind = clientApi.storiesOf(kindName, true);
 
-  // we should always have a framework, rest optional
-  kind.addParameters({
-    framework,
-    component,
-    fileName: currentExports.get(exported),
-    ...params,
-  });
+//   // we should always have a framework, rest optional
+//   kind.addParameters({
+//     framework,
+//     component,
+//     fileName: currentExports.get(exported),
+//     ...params,
+//   });
 
-  (decos || []).forEach((decorator: DecoratorFunction) => {
-    kind.addDecorator(decorator);
-  });
+//   (decos || []).forEach((decorator: DecoratorFunction) => {
+//     kind.addDecorator(decorator);
+//   });
 
-  Object.keys(exports).forEach(key => {
-    if (isExportStory(key, meta)) {
-      const storyFn = exports[key];
-      const { name, parameters, decorators } = storyFn.story || {};
-      const decoratorParams = decorators ? { decorators } : null;
-      const displayNameParams = name ? { displayName: name } : {};
-      kind.add(storyNameFromExport(key), storyFn, {
-        ...parameters,
-        ...decoratorParams,
-        ...displayNameParams,
-      });
-    }
-  });
-};
+//   Object.keys(exports).forEach(key => {
+//     if (isExportStory(key, meta)) {
+//       const storyFn = exports[key];
+//       const { name, parameters, decorators } = storyFn.story || {};
+//       const decoratorParams = decorators ? { decorators } : null;
+//       const displayNameParams = name ? { displayName: name } : {};
+//       kind.add(storyNameFromExport(key), storyFn, {
+//         ...parameters,
+//         ...decoratorParams,
+//         ...displayNameParams,
+//       });
+//     }
+//   });
+// };
 
 const getPreviewFile = (configDir: string): string | false => {
   const preview = path.join(configDir, 'preview.js');
@@ -160,9 +161,22 @@ function configure(
     require.requireActual(f);
   });
 
-  stories.forEach(file => {
-    loadStories(file, framework, storybook);
-  });
+  if (stories && stories.length) {
+    // loadStories(file, framework, storybook);
+    const configure = require(`@storybook/${framework}`);
+
+    configure(
+      stories.reduce((acc, f) => {
+        const { default: a, ...b } = require(f) || {};
+
+        if (a && b.length) {
+          return acc.concat({ default: a, ...b });
+        }
+        return acc;
+      }),
+      []
+    );
+  }
 }
 
 export default configure;
