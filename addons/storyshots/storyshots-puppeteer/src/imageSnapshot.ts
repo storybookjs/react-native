25c55667e29ce8190d7bfb550e4ffc31dac70156
Gaëtan Maisse <gmaisse@solocal.com>
2019-11-22 09:20:00 +0100

    Revert "feat: use `puppeteer-core` instead of `puppeteer`"
    This reverts commit aad29ed5

--- addons/storyshots/storyshots-puppeteer/src/imageSnapshot.ts
+++ addons/storyshots/storyshots-puppeteer/src/imageSnapshot.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Browser, Page } from 'puppeteer-core';
+import puppeteer, { Browser, Page } from 'puppeteer';
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import { logger } from '@storybook/node-logger';
 import { constructUrl } from './url';
