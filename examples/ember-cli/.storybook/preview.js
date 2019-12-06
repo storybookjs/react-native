4e2f349f485460af099abf6fa323f9c7ff49686a
Matthew Irish <matthew@hashicorp.com>
2019-12-06 06:30:14 -0600

    correct import of json file

--- examples/ember-cli/.storybook/preview.js
+++ examples/ember-cli/.storybook/preview.js
@@ -1,9 +1,7 @@
 import { addParameters, addDecorator } from '@storybook/ember';
 import { setJSONDoc } from '@storybook/addon-docs/ember';
 import { withA11y } from '@storybook/addon-a11y';
-
-/* eslint-disable import/no-unresolved */
-import docJson from '../storybook-docgen/index.json';
+import docJson from '../dist/storybook-docgen/index.json';
 
 setJSONDoc(docJson);
 addDecorator(withA11y);
