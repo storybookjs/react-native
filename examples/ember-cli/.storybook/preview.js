4d65199e6588314f9bcf5e09f58de86328f3cc63
Matthew Irish <matthew@hashicorp.com>
2019-12-05 21:26:29 -0600

    add addon-docs for the ember-cli example

--- examples/ember-cli/.storybook/preview.js
+++ examples/ember-cli/.storybook/preview.js
@@ -1,6 +1,11 @@
 import { addParameters, addDecorator } from '@storybook/ember';
+import { setJSONDoc } from '@storybook/addon-docs/ember';
 import { withA11y } from '@storybook/addon-a11y';
 
+/* eslint-disable import/no-unresolved */
+import docJson from '../storybook-docgen/index.json';
+
+setJSONDoc(docJson);
 addDecorator(withA11y);
 addParameters({
   options: {
