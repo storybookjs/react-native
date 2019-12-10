9aebb639126d55f55c098fb934970c346b363206
Norbert de Langen <ndelangen@me.com>
2019-12-10 13:47:48 +0100

    FIX rax axample

--- examples/rax-kitchen-sink/src/stories/index.stories.js
+++ examples/rax-kitchen-sink/src/stories/index.stories.js
@@ -1,18 +1,11 @@
 import { createElement } from 'rax';
 import { linkTo } from '@storybook/addon-links';
-import App from '../components/App';
-import Welcome from '../components/Welcome';
+import { App as AppComponent } from '../components/App/index';
+import { Welcome } from '../components/Welcome/index';
 
 export default {
   title: 'Basic',
 };
 
 export const WelcomeScreen = () => <Welcome showApp={linkTo('Addon|addon-actions', 'with text')} />;
-WelcomeScreen.story = {
-  name: 'Welcome screen',
-};
-
-export const app = () => <App />;
-app.story = {
-  name: 'App',
-};
+export const App = () => <AppComponent />;
