d768b53770a9eaf329f8e4db94dc76ccf9280efb
Michael Shilman <michael@lab80.co>
2019-12-31 14:38:00 +0800

    Main.js: Combine addons/presets in examples

--- examples/cra-ts-kitchen-sink/.storybook/main.js
+++ examples/cra-ts-kitchen-sink/.storybook/main.js
@@ -20,9 +20,9 @@ module.exports = {
         },
       },
     },
-    '@storybook/addon-docs/preset',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-links/register',
-    '@storybook/addon-a11y/register',
+    '@storybook/addon-docs',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
   ],
 };
