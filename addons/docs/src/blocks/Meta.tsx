eaa7d84b93295e3d3d8b17cef77850d8f5003059
patrick.lafrance <patrick.lafrance@outlook.com>
2020-01-05 18:12:29 -0500

    Only render anchor when in docs viewmode

--- addons/docs/src/blocks/Meta.tsx
+++ addons/docs/src/blocks/Meta.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useContext } from 'react';
+import { document } from 'global';
 import { Anchor } from './Anchor';
 import { DocsContext, DocsContextProps } from './DocsContext';
 import { getDocsStories } from './utils';
@@ -18,14 +19,21 @@ function getFirstStoryId(docsContext: DocsContextProps): string {
   return stories.length > 0 ? stories[0].id : null;
 }
 
-/**
- * This component is used to declare component metadata in docs
- * and gets transformed into a default export underneath the hood.
- */
-export const Meta: FC<MetaProps> = () => {
+function renderAnchor() {
   const context = useContext(DocsContext);
   // eslint-disable-next-line react/destructuring-assignment
   const anchorId = getFirstStoryId(context) || context.id;
 
   return <Anchor storyId={anchorId} />;
+}
+
+/**
+ * This component is used to declare component metadata in docs
+ * and gets transformed into a default export underneath the hood.
+ */
+export const Meta: FC<MetaProps> = () => {
+  const params = new URL(document.location).searchParams;
+  const isDocs = params.get('viewMode') === 'docs';
+
+  return isDocs ? renderAnchor() : null;
 };
