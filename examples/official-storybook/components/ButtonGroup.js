2d22ca81f5f385df7bf8a9baf42c17feec479761
Michael Shilman <michael@lab80.co>
2020-01-09 12:40:11 +0800

    Official-storybook: Add prop table example for multiple named exports

--- examples/official-storybook/components/ButtonGroup.js
+++ examples/official-storybook/components/ButtonGroup.js
@@ -12,6 +12,25 @@ ButtonGroup.defaultProps = {
 };
 
 ButtonGroup.propTypes = {
+  /**
+   * Background color for the group
+   */
+  background: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.element),
+};
+
+/** SubGroup component description from docgen */
+export const SubGroup = ({ background, children }) => <div style={{ background }}>{children}</div>;
+
+SubGroup.defaultProps = {
+  background: '#0f0',
+  children: null,
+};
+
+SubGroup.propTypes = {
+  /**
+   * Background color for the sub-group
+   */
   background: PropTypes.string,
   children: PropTypes.arrayOf(PropTypes.element),
 };
