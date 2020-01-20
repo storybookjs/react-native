3429aee6bfedfdce9498f73aab55f1039c59190b
Norbert de Langen <ndelangen@me.com>
2020-01-21 00:00:06 +0100

    FIX manager using ESM in dev-kits example

--- examples/dev-kits/main.js
+++ examples/dev-kits/main.js
@@ -20,4 +20,20 @@ module.exports = {
       extensions: [...(config.resolve.extensions || []), '.ts', '.tsx'],
     },
   }),
+  managerWebpack: async config => ({
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /manager\.js$/,
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [['react-app', { flow: false, typescript: true }]],
+          },
+        },
+      ],
+    },
+  }),
 };
