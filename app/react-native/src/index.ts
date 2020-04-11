019135e2c1ef9be97f7eb41601ac6b9b090672f1
Henrik Raitasola <henrik.raitasola@gmail.com>
2020-04-11 11:34:11 +0300

    Define variable types because `bind` returns `any`

--- app/react-native/src/index.ts
+++ app/react-native/src/index.ts
@@ -1,17 +1,20 @@
 /* eslint-disable prefer-destructuring */
+import { ClientApi } from '@storybook/client-api';
 import Preview from './preview';
 
 const preview = new Preview();
 
-const rawStoriesOf = preview.api().storiesOf.bind(preview);
-export const setAddon = preview.api().setAddon.bind(preview);
-export const addDecorator = preview.api().addDecorator.bind(preview);
-export const addParameters = preview.api().addParameters.bind(preview);
-export const clearDecorators = preview.api().clearDecorators.bind(preview);
+const rawStoriesOf: ClientApi['storiesOf'] = preview.api().storiesOf.bind(preview);
+export const setAddon: ClientApi['setAddon'] = preview.api().setAddon.bind(preview);
+export const addDecorator: ClientApi['addDecorator'] = preview.api().addDecorator.bind(preview);
+export const addParameters: ClientApi['addParameters'] = preview.api().addParameters.bind(preview);
+export const clearDecorators: ClientApi['clearDecorators'] = preview
+  .api()
+  .clearDecorators.bind(preview);
 export const configure = preview.configure;
-export const getStorybook = preview.api().getStorybook.bind(preview);
+export const getStorybook: ClientApi['getStorybook'] = preview.api().getStorybook.bind(preview);
 export const getStorybookUI = preview.getStorybookUI;
-export const raw = preview.api().raw.bind(preview);
+export const raw: ClientApi['raw'] = preview.api().raw.bind(preview);
 
-export const storiesOf = (...args: any[]) =>
-  rawStoriesOf(...args).addParameters({ framework: 'react-native' });
+export const storiesOf = (kind: string, module: NodeModule) =>
+  rawStoriesOf(kind, module).addParameters({ framework: 'react-native' });
