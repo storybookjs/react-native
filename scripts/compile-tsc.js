bfac824cdd9bb64677e383c4b5a313c8737b380c
Danny <danny.h.williams@hotmail.com>
2020-08-15 23:33:15 +0100

    fix: emit type declarations when building so type info is not lost (#94)

--- scripts/compile-tsc.js
+++ scripts/compile-tsc.js
@@ -6,7 +6,7 @@ const shell = require('shelljs');
 function getCommand(watch) {
   const tsc = path.join(__dirname, '..', 'node_modules', '.bin', 'tsc');
 
-  const args = ['--outDir ./dist', '--listEmittedFiles true'];
+  const args = ['--outDir ./dist', '--listEmittedFiles true', '--declaration true'];
 
   if (watch) {
     args.push('-w');
