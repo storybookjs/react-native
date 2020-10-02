8f54701a8eeab517b8550e33f2de8ae267093866
Danny Williams <danny.h.williams@hotmail.com>
2020-10-02 18:02:03 +0100

    fix: remove left over file from rebase

--- examples/native/storybook/stories/Knobs/Knobs.stories.js
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react-native';
-import { withKnobs } from '@storybook/addon-ondevice-knobs';
-import knobsWrapper from '.';
-
-storiesOf('Knobs', module).addDecorator(withKnobs).add('with knobs', knobsWrapper);
