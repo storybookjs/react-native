b833544356b5f5f8e44b04e8c0ca291684a44ee8
Danny Williams <danny.h.williams@hotmail.com>
2020-12-01 19:28:50 +0000

    remove commented stuff

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -1,10 +1,1 @@
-// @ts-ignore
-// import prevalscript from '@storybook/react-native/babel';
-
-// const { x } = require('@storybook/react-native/babel');
-
-// console.log({ x });
-// eslint-disable-next-line import/no-unresolved
-require('@storybook/react-native/babel');
-
 export { default } from './Storybook';
