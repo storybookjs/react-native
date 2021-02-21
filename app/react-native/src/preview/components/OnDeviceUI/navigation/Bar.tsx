baaa12af5ad403c62d93d6046d9038c8b79ad90a
Danny Williams <danny.h.williams@hotmail.com>
2021-02-21 23:03:32 +0000

    fix: commit file name case change

---
+++ app/react-native/src/preview/components/OnDeviceUI/navigation/Bar.tsx
@@ -0,0 +1,47 @@
+import React, { PureComponent } from 'react';
+import styled from '@emotion/native';
+import Button from './Button';
+import { NAVIGATOR, PREVIEW, ADDONS } from './constants';
+
+const Container = styled.View(({ theme }) => ({
+  flexDirection: 'row',
+  paddingHorizontal: 8,
+  backgroundColor: theme.backgroundColor || 'white',
+  borderTopWidth: 1,
+  borderBottomWidth: 1,
+  borderColor: theme.borderColor || '#e6e6e6',
+}));
+
+export interface Props {
+  index: number;
+  onPress: (id: number) => void;
+}
+
+export default class Bar extends PureComponent<Props> {
+  render() {
+    const { index, onPress } = this.props;
+    return (
+      <Container>
+        <Button
+          onPress={onPress}
+          testID="BottomMenu.Navigator"
+          id={NAVIGATOR}
+          active={index === NAVIGATOR}
+        >
+          NAVIGATOR
+        </Button>
+        <Button
+          onPress={onPress}
+          testID="BottomMenu.Preview"
+          id={PREVIEW}
+          active={index === PREVIEW}
+        >
+          PREVIEW
+        </Button>
+        <Button onPress={onPress} testID="BottomMenu.Addons" id={ADDONS} active={index === ADDONS}>
+          ADDONS
+        </Button>
+      </Container>
+    );
+  }
+}
