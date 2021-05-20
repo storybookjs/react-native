8d66219017afbdcae4c5c00887aaa7120602e310
Daniel Williams <danny.h.williams@hotmail.com>
2021-05-20 23:19:52 +0100

    feat: rename CSF example to be more clear

---
+++ examples/native/components/CSFExample/CSFExampleButton.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from './CSFExampleButton';
+
+export default {
+  title: 'CSF Example',
+};
+
+export const CSFExample1 = () => <Button text="test1" onPress={() => null} />;
+export const CSFExample2 = () => <Button text="test2" onPress={() => null} />;
+export const CSFExample3 = () => <Button text="test3" onPress={() => null} />;
