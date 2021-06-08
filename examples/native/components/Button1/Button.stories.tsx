a6828d8e06b2ba2f3a4bbcbf65b0c0d938068132
Daniel Williams <danny.h.williams@hotmail.com>
2021-06-08 13:19:04 +0100

    refactor: promise example does something visually

--- examples/native/components/Button1/Button.stories.tsx
+++ examples/native/components/Button1/Button.stories.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { Button } from './Button';
 
-const testing = () => {
-  const timeout = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('hi');
-    }, 1000);
-  });
-  timeout
-    .then((res) => {
-      console.log(res);
-    })
-    .finally(() => {
-      console.log('resolved ');
-    });
-};
-
 storiesOf('button promise', module)
-  .add('finally', () => <Button text="Do a promise" onPress={testing} />)
+  .add('finally', () => {
+    const [text, setText] = useState('Do a promise');
+    const testing = () => {
+      const timeout = new Promise<string>((resolve) => {
+        setTimeout(() => {
+          resolve('Promise resolved');
+        }, 1000);
+      });
+      timeout
+        .then((res: string) => {
+          setText(res);
+        })
+        .finally(() => {
+          setText('Done!');
+        });
+    };
+    return <Button text={text} onPress={testing} />;
+  })
   .add('nothing', () => <Button text="test4" onPress={() => null} />);
