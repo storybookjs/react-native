2ec4cd6ebeffc44babac07879b2522f5a0172d24
Daniel Williams <danny.h.williams@hotmail.com>
2021-06-08 13:27:22 +0100

    feat: re-add notes example

--- examples/native/components/Button1/Button.stories.tsx
+++ examples/native/components/Button1/Button.stories.tsx
@@ -3,22 +3,32 @@ import { storiesOf } from '@storybook/react-native';
 import { Button } from './Button';
 
 storiesOf('button promise', module)
-  .add('finally', () => {
-    const [text, setText] = useState('Do a promise');
-    const testing = () => {
-      const timeout = new Promise<string>((resolve) => {
-        setTimeout(() => {
-          resolve('Promise resolved');
-        }, 1000);
-      });
-      timeout
-        .then((res: string) => {
-          setText(res);
-        })
-        .finally(() => {
-          setText('Done!');
+  .add(
+    'finally',
+    () => {
+      const [text, setText] = useState('Do a promise');
+      const testing = () => {
+        const timeout = new Promise<string>((resolve) => {
+          setTimeout(() => {
+            resolve('Promise resolved');
+          }, 1000);
         });
-    };
-    return <Button text={text} onPress={testing} />;
-  })
+        timeout
+          .then((res: string) => {
+            setText(res);
+          })
+          .finally(() => {
+            setText('Done!');
+          });
+      };
+      return <Button text={text} onPress={testing} />;
+    },
+    {
+      notes: `
+# Markdown!\n
+* Promises are working finally!
+* [storybook!](https://storybook.js.org)
+`,
+    }
+  )
   .add('nothing', () => <Button text="test4" onPress={() => null} />);
