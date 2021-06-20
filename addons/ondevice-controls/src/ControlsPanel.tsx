5d5d6177013ede7be15e2632e74020d5e37d046a
Daniel Williams <danny.h.williams@hotmail.com>
2021-06-20 13:54:23 +0100

    feat(controls): adds a use args hook that works

--- addons/ondevice-controls/src/ControlsPanel.tsx
+++ addons/ondevice-controls/src/ControlsPanel.tsx
@@ -1,16 +1,9 @@
 import styled from '@emotion/native';
-import React, { useState, useEffect, useCallback } from 'react';
-import { Linking, Text } from 'react-native';
-// import { PARAM_KEY } from './index';
-import PropForm from './PropForm';
-// import { useArgs, useParameter } from './hooks';
-import { Channel } from '@storybook/addons';
 import { API } from '@storybook/api';
-import { SET_CURRENT_STORY } from '@storybook/core-events';
-
-// const getTimestamp = () => +new Date();
-
-// const DEFAULT_GROUP_ID = 'Other';
+import React from 'react';
+import { Linking, Text } from 'react-native';
+import { useArgs } from './hooks';
+import PropForm from './PropForm';
 
 const Touchable = styled.TouchableOpacity(({ theme }) => ({
   borderRadius: 2,
@@ -50,50 +43,19 @@ export interface ArgTypes {
   [key: string]: ArgType;
 }
 
-const ControlsPanel = ({
-  api,
-  active,
-  channel,
-}: {
-  channel: Channel;
-  api: API;
-  active: boolean;
-}) => {
-  const [story, setStory] = useState<any>();
+const ControlsPanel = ({ api }: { api: API }) => {
+  const store = api.store();
+  const storyId = store.getSelection().storyId;
+  const [argsfromHook, updateArgs, resetArgs] = useArgs(storyId, store);
+  const { argTypes, parameters } = store.fromId(storyId);
 
-  const getStory = useCallback(() => {
-    const sel = api.store().getSelection();
-    setStory(api.store().fromId(sel.storyId));
-  }, [api]);
+  console.log({ argTypes, parameters });
 
-  const reset = () => {
-    api.store().resetStoryArgs(story.id);
-  };
-
-  useEffect(() => {
-    if (active) {
-      getStory();
-    }
-    channel.on(SET_CURRENT_STORY, getStory);
-    return channel.off(SET_CURRENT_STORY, getStory);
-  }, [api, active, channel, getStory]);
-  if (!active || !story) {
-    return null;
-  }
-
-  const params = story.parameters;
-
-  const argTypes = params.argTypes;
-  const isArgsStory = params.__isArgsStory;
-
-  const updateArg = ({ name, value }) => {
-    story.parameters.args[name] = value;
-    api.store().updateStoryArgs(story.id, story.parameters.args);
-  };
+  const isArgsStory = parameters.__isArgsStory;
 
   const hasControls = Object.values(argTypes).some((arg: ArgType) => arg?.control);
   const showWarning = !(hasControls && isArgsStory);
-  const args = Object.entries(story.parameters.args).reduce((prev, [name, value]) => {
+  const argsObject = Object.entries(argsfromHook).reduce((prev, [name, value]) => {
     return {
       ...prev,
       [name]: { name, type: argTypes[name].control.type, value },
@@ -114,199 +76,12 @@ const ControlsPanel = ({
           </Text>
         </Text>
       )}
-      <PropForm args={args} onFieldChange={updateArg} />
-      <Touchable onPress={reset}>
+      <PropForm args={argsObject} onFieldChange={updateArgs} />
+      <Touchable onPress={() => resetArgs()}>
         <ResetButton>RESET</ResetButton>
       </Touchable>
-      {/* <ArgsTable
-        {...{
-          key: path, // resets state when switching stories
-          compact: !expanded && hasControls,
-          rows: withPresetColors,
-          args,
-          updateArgs,
-          resetArgs,
-          inAddonPanel: true,
-          sort,
-        }}
-      /> */}
     </>
   );
 };
 
 export default ControlsPanel;
-
-// export default class Panel extends React.Component<
-//   PanelProps,
-//   { knobs: Record<string, Knob>; groupId: string }
-// > {
-//   constructor(props) {
-//     super(props);
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handlePress = this.handlePress.bind(this);
-//     this.setKnobs = this.setKnobs.bind(this);
-//     this.reset = this.reset.bind(this);
-//     this.setOptions = this.setOptions.bind(this);
-//     this.onGroupSelect = this.onGroupSelect.bind(this);
-
-//     this.state = { knobs: {}, groupId: DEFAULT_GROUP_ID };
-//     this.options = {};
-
-//     this.lastEdit = getTimestamp();
-//     this.loadedFromUrl = false;
-//   }
-
-//   options: any;
-
-//   lastEdit: number;
-
-//   loadedFromUrl: boolean;
-
-//   componentDidMount() {
-//     const { channel } = this.props;
-
-//     channel.on(SET, this.setKnobs);
-//     channel.on(SET_OPTIONS, this.setOptions);
-//     channel.on(SELECT_STORY, this.selectStory);
-//     channel.emit(FORCE_RE_RENDER);
-//   }
-
-//   componentWillUnmount() {
-//     const { channel } = this.props;
-//     channel.removeListener(SET, this.setKnobs);
-//     channel.removeListener(SELECT_STORY, this.selectStory);
-//   }
-
-//   onGroupSelect(name) {
-//     this.setState({ groupId: name });
-//   }
-
-//   setOptions(options = { timestamps: false }) {
-//     this.options = options;
-//   }
-
-//   setKnobs({ knobs, timestamp }) {
-//     if (!this.options.timestamps || !timestamp || this.lastEdit <= timestamp) {
-//       this.setState({ knobs });
-//     }
-//   }
-
-//   reset = () => {
-//     const { channel } = this.props;
-//     this.setState({ knobs: {} });
-//     channel.emit(RESET);
-//   };
-
-//   selectStory = () => {
-//     const { channel } = this.props;
-//     this.setState({ knobs: {}, groupId: DEFAULT_GROUP_ID });
-//     channel.emit(RESET);
-//   };
-
-//   emitChange(changedKnob) {
-//     const { channel } = this.props;
-//     channel.emit(CHANGE, changedKnob);
-//   }
-
-//   handleChange(changedKnob) {
-//     this.lastEdit = getTimestamp();
-//     const { knobs } = this.state;
-//     const { name } = changedKnob;
-//     const newKnobs = { ...knobs };
-//     newKnobs[name] = {
-//       ...newKnobs[name],
-//       ...changedKnob,
-//     };
-
-//     this.setState({ knobs: newKnobs });
-
-//     this.setState({ knobs: newKnobs }, () =>
-//       this.emitChange(
-//         changedKnob.type === 'number'
-//           ? { ...changedKnob, value: parseFloat(changedKnob.value) }
-//           : changedKnob
-//       )
-//     );
-//   }
-
-//   handlePress(knob) {
-//     const { channel } = this.props;
-
-//     channel.emit(CLICK, knob);
-//   }
-
-//   render() {
-//     const { active } = this.props;
-
-//     if (!active) {
-//       return null;
-//     }
-
-//     const { knobs, groupId: stateGroupId } = this.state;
-
-//     const groups = {};
-//     const groupIds = [];
-
-//     let knobsKeys = Object.keys(knobs);
-
-//     const knobsWithGroups = knobsKeys.filter((key) => knobs[key].groupId);
-
-//     knobsWithGroups.forEach((key) => {
-//       const knobKeyGroupId = knobs[key].groupId;
-//       groupIds.push(knobKeyGroupId);
-//       groups[knobKeyGroupId] = {
-//         render: () => <Text testID={knobKeyGroupId}>{knobKeyGroupId}</Text>,
-//         title: knobKeyGroupId,
-//       };
-//     });
-
-//     const allHaveGroups = groupIds.length > 0 && knobsKeys.length === knobsWithGroups.length;
-
-//     // If all of the knobs are assigned to a group, we don't need the default group.
-//     const groupId =
-//       stateGroupId === DEFAULT_GROUP_ID && allHaveGroups
-//         ? knobs[knobsWithGroups[0]].groupId
-//         : stateGroupId;
-
-//     if (groupIds.length > 0) {
-//       if (!allHaveGroups) {
-//         groups[DEFAULT_GROUP_ID] = {
-//           render: () => <Text testID={DEFAULT_GROUP_ID}>{DEFAULT_GROUP_ID}</Text>,
-//           title: DEFAULT_GROUP_ID,
-//         };
-//       }
-
-//       if (groupId === DEFAULT_GROUP_ID) {
-//         knobsKeys = knobsKeys.filter((key) => !knobs[key].groupId);
-//       }
-
-//       if (groupId !== DEFAULT_GROUP_ID) {
-//         knobsKeys = knobsKeys.filter((key) => knobs[key].groupId === groupId);
-//       }
-//     }
-
-//     const knobsArray = knobsKeys.map((key) => knobs[key]);
-
-//     if (knobsArray.length === 0) {
-//       return <Text>NO KNOBS</Text>;
-//     }
-
-//     return (
-//       <View style={{ flex: 1, paddingTop: 10 }}>
-//         {groupIds.length > 0 && (
-//           <GroupTabs groups={groups} onGroupSelect={this.onGroupSelect} selectedGroup={groupId} />
-//         )}
-//         <View>
-//           <PropForm
-//             knobs={knobsArray}
-//             onFieldChange={this.handleChange}
-//             onFieldPress={this.handlePress}
-//           />
-//         </View>
-//         <Touchable onPress={this.reset}>
-//           <ResetButton>RESET</ResetButton>
-//         </Touchable>
-//       </View>
-//     );
-//   }
-// }
