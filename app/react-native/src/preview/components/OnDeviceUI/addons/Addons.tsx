f2b9f0b3e7dfa2e0f7a65b3eefc17b4b20a2a6c5
Lauri Harpf <lauri.harpf@acelvia.com>
2021-06-25 12:36:32 +0300

    refactor: Refactors Addons.tsx to a function component

--- app/react-native/src/preview/components/OnDeviceUI/addons/Addons.tsx
+++ app/react-native/src/preview/components/OnDeviceUI/addons/Addons.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import styled from '@emotion/native';
 import { addons } from '@storybook/addons';
@@ -17,45 +17,26 @@ const Container = styled.View(({ theme }) => ({
   backgroundColor: theme.backgroundColor,
 }));
 
-export default class Addons extends PureComponent<{}, { addonSelected: string }> {
-  panels = addons.getElements('panel');
+export default React.memo(() => {
+  const panels = addons.getElements('panel');
+  const [addonSelected, setAddonSelected] = useState<string | null>(Object.keys(panels)[0] || null);
 
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      addonSelected: Object.keys(this.panels)[0] || null,
-    };
-  }
-
-  onPressAddon = (addonSelected: string) => {
-    this.setState({ addonSelected });
-  };
-
-  render() {
-    const { addonSelected } = this.state;
-
-    if (Object.keys(this.panels).length === 0) {
-      return (
-        <SafeAreaView style={{ flex: 1 }}>
-          <NoAddonContainer>
-            <Label>No addons loaded.</Label>
-          </NoAddonContainer>
-        </SafeAreaView>
-      );
-    }
-
+  if (Object.keys(panels).length === 0) {
     return (
-      <Container>
-        <SafeAreaView style={{ flex: 1 }}>
-          <AddonsList
-            onPressAddon={this.onPressAddon}
-            panels={this.panels}
-            addonSelected={addonSelected}
-          />
-          <AddonWrapper addonSelected={addonSelected} panels={this.panels} />
-        </SafeAreaView>
-      </Container>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NoAddonContainer>
+          <Label>No addons loaded.</Label>
+        </NoAddonContainer>
+      </SafeAreaView>
     );
   }
-}
+
+  return (
+    <Container>
+      <SafeAreaView style={{ flex: 1 }}>
+        <AddonsList onPressAddon={setAddonSelected} panels={panels} addonSelected={addonSelected} />
+        <AddonWrapper addonSelected={addonSelected} panels={panels} />
+      </SafeAreaView>
+    </Container>
+  );
+});
