abba92ff8c79fa22208c8842aeeeaa3d23ead173
Daniel Williams <danny.h.williams@hotmail.com>
2021-06-29 22:50:05 +0100

    feat(controls): re-enable number control

--- examples/native/components/CSFExample/CSFExampleButton.tsx
+++ examples/native/components/CSFExample/CSFExampleButton.tsx
@@ -4,14 +4,17 @@ import { TouchableOpacity, StyleSheet, Text } from 'react-native';
 interface ButtonProps {
   onPress: () => void;
   buttonText: string;
+  num: number;
 }
 
 const styles = StyleSheet.create({
   button: { paddingHorizontal: 16, paddingVertical: 8 },
 });
 
-export const Button: FunctionComponent<ButtonProps> = ({ onPress, buttonText }) => (
+export const Button: FunctionComponent<ButtonProps> = ({ onPress, buttonText, num }) => (
   <TouchableOpacity style={styles.button} onPress={onPress}>
-    <Text>{buttonText}</Text>
+    <Text>
+      {buttonText}:{num}
+    </Text>
   </TouchableOpacity>
 );
