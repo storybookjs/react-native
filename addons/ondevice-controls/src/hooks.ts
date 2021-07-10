e304762ecfdf4e18fd5b4ad1b72aaad29ae94472
Daniel Williams <danny.h.williams@hotmail.com>
2021-07-10 16:59:39 +0100

    fix(controls): don't spam re-render

--- addons/ondevice-controls/src/hooks.ts
+++ addons/ondevice-controls/src/hooks.ts
@@ -1,11 +1,24 @@
 import { Args } from '@storybook/addons';
 import { useState, useEffect, useCallback } from 'react';
 import Events, { FORCE_RE_RENDER } from '@storybook/core-events';
+import useDebouncedCallback from './useDebounceCallback';
 
 export const useArgs = (
   storyId: string,
   storyStore: any
 ): [Args, (args: Args) => void, (argNames?: string[]) => void] => {
+  // TODO: remove need to force re-render
+  const debouncedReRender = useDebouncedCallback(
+    () => storyStore._channel.emit(FORCE_RE_RENDER),
+    100
+  );
+
+  useEffect(() => {
+    return () => {
+      debouncedReRender.cancel();
+    };
+  }, [debouncedReRender]);
+
   const story = storyStore.fromId(storyId);
   if (!story) {
     throw new Error(`Unknown story: ${storyId}`);
@@ -22,14 +35,15 @@ export const useArgs = (
     storyStore._channel.on(Events.STORY_ARGS_UPDATED, cb);
     return () => storyStore._channel.off(Events.STORY_ARGS_UPDATED, cb);
   }, [storyId, storyStore._channel]);
+
   const updateArgs = useCallback(
     (newArgs) => {
       storyStore.updateStoryArgs(storyId, newArgs);
 
       //TODO: remove this if possible
-      storyStore._channel.emit(FORCE_RE_RENDER);
+      debouncedReRender();
     },
-    [storyId, storyStore]
+    [debouncedReRender, storyId, storyStore]
   );
   const resetArgs = useCallback(
     (argNames?: string[]) => {
