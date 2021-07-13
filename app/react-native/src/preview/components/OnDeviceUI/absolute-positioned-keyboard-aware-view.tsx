7ad6009951c9927fbfe052c6c4f34450ddbb5868
Lauri Harpf <lauri.harpf@acelvia.com>
2021-07-13 17:43:29 +0300

    fix: Fixes #211 (After opening the keyboard the android UI shrinks). The steps to reproduce this issue on the Android emulator were:
    0. Run the React Native examples (under examples/native) on Android
    1. Open the story Navigator.
    2. Select the "Basic" story.
    3. Open "Addons -> Controls"
    4. Click on the "first" field so that the keyboard comes up.
    5. Click back to "Navigator"
    
    ## What happened
    The story list in the navigator gets truncated a lot (seems to only occupy ~50% or so of the screen) after Step 5.
    
    ## What happens now after the fix
    The story list doesn't get truncated; it looks the same it did after Step 1.

--- app/react-native/src/preview/components/OnDeviceUI/absolute-positioned-keyboard-aware-view.tsx
+++ app/react-native/src/preview/components/OnDeviceUI/absolute-positioned-keyboard-aware-view.tsx
@@ -1,13 +1,5 @@
-import React, { ReactNode, useRef, useEffect } from 'react';
-import {
-  Platform,
-  Keyboard,
-  Dimensions,
-  View,
-  LayoutChangeEvent,
-  KeyboardEvent,
-  StyleSheet,
-} from 'react-native';
+import React, { ReactNode } from 'react';
+import { View, LayoutChangeEvent, StyleSheet } from 'react-native';
 
 export interface PreviewDimens {
   width: number;
@@ -20,46 +12,6 @@ type Props = {
   children: ReactNode;
 };
 
-const useIsKeyboardOpen = (previewWidth: number) => {
-  const keyboardOpen = useRef(false);
-  useEffect(() => {
-    const keyboardDidShowHandler = (e: KeyboardEvent) => {
-      if (Platform.OS === 'android') {
-        // There is bug in RN android that keyboardDidShow event is called when you go from portrait to landscape.
-        // To make sure that this is keyboard event we check screen width
-        if (previewWidth === e.endCoordinates.width) {
-          keyboardOpen.current = true;
-        }
-      }
-    };
-
-    const keyboardDidHideHandler = () => {
-      if (keyboardOpen.current) {
-        keyboardOpen.current = false;
-      }
-    };
-
-    // When rotating screen from portrait to landscape with keyboard open on android it calls keyboardDidShow, but doesn't call
-    // keyboardDidHide. To avoid issues we set keyboardOpen to false immediately on keyboardChange.
-    const removeKeyboardOnOrientationChange = () => {
-      if (Platform.OS === 'android') {
-        keyboardOpen.current = false;
-      }
-    };
-
-    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShowHandler);
-    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHideHandler);
-    Dimensions.addEventListener('change', removeKeyboardOnOrientationChange);
-    return () => {
-      keyboardDidShowListener.remove();
-      keyboardDidHideListener.remove();
-      Dimensions.removeEventListener('change', removeKeyboardOnOrientationChange);
-    };
-  }, [previewWidth]);
-
-  return keyboardOpen.current;
-};
-
 // Android changes screen size when keyboard opens.
 // To avoid issues we use absolute positioned element with predefined screen size
 const AbsolutePositionedKeyboardAwareView = ({
@@ -67,15 +19,11 @@ const AbsolutePositionedKeyboardAwareView = ({
   previewDimensions: { width, height },
   children,
 }: Props) => {
-  const keyboardOpen = useIsKeyboardOpen(width);
-
   const onLayoutHandler = ({ nativeEvent }: LayoutChangeEvent) => {
-    if (!keyboardOpen) {
-      onLayout({
-        height: nativeEvent.layout.height,
-        width: nativeEvent.layout.width,
-      });
-    }
+    onLayout({
+      height: nativeEvent.layout.height,
+      width: nativeEvent.layout.width,
+    });
   };
 
   return (
