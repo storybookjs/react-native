be2036a3be5a51561b198ccfd4a675f4856b042c
Lauri Harpf <lauri.harpf@acelvia.com>
2021-07-16 21:07:04 +0300

    Code review comments:
    - Add dependency to @storybook/addon-ondevice-backgrounds in the examples/native package.json
    - Add the withBackgrounds decorator to all stories in examples/native via preview.js

--- examples/native/.storybook/preview.js
+++ examples/native/.storybook/preview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-// import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
+import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
 
 export const decorators = [
   (StoryFn) => (
@@ -8,8 +8,16 @@ export const decorators = [
       <StoryFn />
     </View>
   ),
+  withBackgrounds,
 ];
-export const parameters = { my_param: 'anything' };
+export const parameters = {
+  my_param: 'anything',
+  backgrounds: [
+    { name: 'plain', value: 'white', default: true },
+    { name: 'warm', value: 'hotpink' },
+    { name: 'cool', value: 'deepskyblue' },
+  ],
+};
 
 const styles = StyleSheet.create({
   container: { padding: 8 },
