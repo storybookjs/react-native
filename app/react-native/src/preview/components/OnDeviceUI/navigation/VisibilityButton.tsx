4f23118f7a80ceb23bad007c0165f51d100fd5d7
Pavlos Vinieratos <pvinis@gmail.com>
2022-07-15 13:11:16 +0300

    better

--- app/react-native/src/preview/components/OnDeviceUI/navigation/VisibilityButton.tsx
+++ app/react-native/src/preview/components/OnDeviceUI/navigation/VisibilityButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/native';
+import { View } from 'react-native';
 
 interface Props {
   onPress: () => void;
@@ -13,10 +14,36 @@ const Touchable = styled.TouchableOpacity({
   zIndex: 100,
 });
 
-const HideIcon = styled.Text(({ theme }) => ({
-  fontSize: 14,
-  color: theme.buttonTextColor || '#999999',
+const HIDE_ICON_SIZE = 14;
+const HIDE_ICON_BORDER_WIDTH = 1;
+const Inner = styled.View(({ theme }) => ({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: HIDE_ICON_SIZE,
+  height: HIDE_ICON_SIZE,
+  borderRadius: HIDE_ICON_BORDER_WIDTH,
+  overflow: 'hidden',
+  borderColor: theme.buttonTextColor || '#999999',
+  borderWidth: HIDE_ICON_BORDER_WIDTH * 2,
 }));
+const Outer = styled.View({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: HIDE_ICON_SIZE,
+  height: HIDE_ICON_SIZE,
+  borderRadius: HIDE_ICON_BORDER_WIDTH,
+  overflow: 'hidden',
+  borderColor: 'white',
+  borderWidth: HIDE_ICON_BORDER_WIDTH,
+});
+const HideIcon = () => (
+  <View style={{ width: HIDE_ICON_SIZE, height: HIDE_ICON_SIZE, marginRight: 4 }}>
+    <Inner />
+    <Outer />
+  </View>
+);
 
 const VisibilityButton = ({ onPress }: Props) => (
   <Touchable
@@ -25,7 +52,7 @@ const VisibilityButton = ({ onPress }: Props) => (
     accessibilityLabel="Storybook.OnDeviceUI.toggleUI"
     hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}
   >
-    <HideIcon>□</HideIcon>
+    <HideIcon />
   </Touchable>
 );
 export default React.memo(VisibilityButton);
