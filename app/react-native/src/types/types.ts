bb03c4cafc5ce03e29836dab4e61bd159a3a122b
Danny <dannyhyunsoowilliams@gmail.com>
2022-10-27 12:54:25 +0100

    refactor: remove more comments

--- app/react-native/src/types/types.ts
+++ app/react-native/src/types/types.ts
@@ -5,16 +5,6 @@ export interface PreviewError {
   stack?: string;
 }
 
-// Copy of require.context
-// interface RequireContext {
-//   keys(): string[];
-//   (id: string): any;
-//   <T>(id: string): T;
-//   resolve(id: string): string;
-//   /** The module id of the context module. This may be useful for module.hot.accept. */
-//   id: string;
-// }
-
 export interface RequireContext {
   keys: () => string[];
   (id: string): any;
