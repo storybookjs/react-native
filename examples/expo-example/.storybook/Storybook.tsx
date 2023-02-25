4181baa8e60fcc6c53e509078d9fd740288f1adb
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2023-02-25 18:37:07 +0000

    feat: replace example app with an expo one

---
+++ examples/expo-example/.storybook/Storybook.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { getStorybookUI } from '@storybook/react-native';
+// import { SafeAreaView } from 'react-native';
+
+import './doctools';
+import './storybook.requires';
+
+const StorybookUIRoot = getStorybookUI({
+  initialSelection: { kind: 'TextInput', name: 'Basic' },
+  shouldPersistSelection: true,
+  // onDeviceUI: false,
+  // enableWebsockets: true,
+});
+
+export default () => (
+  // <SafeAreaView style={{ flex: 1 }}>
+  <StorybookUIRoot />
+  // </SafeAreaView>
+);
--- examples/native/App.web.tsx
+++
@@ -1,3 +0,0 @@
-import StorybookUIRoot from './.storybook/Storybook';
-
-export { StorybookUIRoot as default };
