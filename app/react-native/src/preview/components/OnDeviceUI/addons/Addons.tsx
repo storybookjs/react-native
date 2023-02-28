742b3acd11b10a6672613d347b80bfa56cd27700
Jonathan Jacobs <jonathan@yoco.co.za>
2023-02-28 19:52:24 +0200

    chore: Apply theme values to the addons panel

--- app/react-native/src/preview/components/OnDeviceUI/addons/Addons.tsx
+++ app/react-native/src/preview/components/OnDeviceUI/addons/Addons.tsx
@@ -1,52 +1,36 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import styled from '@emotion/native';
+import { Text } from 'react-native';
 import { addons } from '@storybook/addons';
 import AddonsList from './List';
 import AddonWrapper from './Wrapper';
-import { Label } from '../../Shared/text';
+import { Box } from '../../Shared/layout';
 
-const NoAddonContainer = styled.View({
-  flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
-});
+interface AddonsProps {
+  active: boolean;
+}
 
-const Container = styled.View(({ theme }) => ({
-  flex: 1,
-  backgroundColor: theme.backgroundColor,
-}));
-
-const Addons = ({ active }: { active: boolean }) => {
+const Addons = ({ active }: AddonsProps) => {
   const panels = addons.getElements('panel');
   const [addonSelected, setAddonSelected] = useState<string | null>(Object.keys(panels)[0] || null);
 
   if (Object.keys(panels).length === 0) {
     return (
-      <SafeAreaView style={styles.container}>
-        <NoAddonContainer>
-          <Label>No addons loaded.</Label>
-        </NoAddonContainer>
-      </SafeAreaView>
+      <Box alignItems="center" justifyContent="center">
+        <Text>No addons loaded.</Text>
+      </Box>
     );
   }
 
   return (
-    <Container>
-      <SafeAreaView style={styles.container}>
-        <AddonsList
-          onPressAddon={setAddonSelected}
-          panels={panels}
-          addonSelected={active ? addonSelected : null}
-        />
-        <AddonWrapper addonSelected={active ? addonSelected : null} panels={panels} />
-      </SafeAreaView>
-    </Container>
+    <Box flex>
+      <AddonsList
+        onPressAddon={setAddonSelected}
+        panels={panels}
+        addonSelected={active ? addonSelected : null}
+      />
+      <AddonWrapper addonSelected={active ? addonSelected : null} panels={panels} />
+    </Box>
   );
 };
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-});
 
 export default React.memo(Addons);
