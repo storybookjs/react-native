c00811e8562f948da41cf7b19be60d240bbb8bf7
Leonardo F.C <leonardofc2002@gmail.com>
2023-03-14 04:47:39 -0300

    fix: compatibility with webpack5

--- app/react-native-server/src/server/options.js
+++ app/react-native-server/src/server/options.js
@@ -1,8 +1,12 @@
+import path from 'path';
 import packageJson from '../../package.json';
 
 function extendOptions(options, extendServer) {
   const { manualId, https: secured, host, port } = options;
   const storybookOptions = { manualId, secured, host, port };
+
+  const config = require(path.join(options.configDir, 'main'));
+  const useWebpack5 = config.core?.builder === 'webpack5';
 
   return {
     ...options,
@@ -13,7 +17,7 @@ function extendOptions(options, extendServer) {
     ignorePreview: true,
     corePresets: [
       {
-        name: '@storybook/manager-webpack4/manager-preset',
+        name: `@storybook/manager-webpack${useWebpack5 ? 5 : 4}/manager-preset`,
         options: { managerEntry: require.resolve('../client/manager') },
       },
       {
