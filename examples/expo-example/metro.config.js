e12b5b2e90c6cb2d6a210d9fa4fbb07353ac7022
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2023-07-24 12:11:20 +0100

    run from metro config and refactors

--- examples/expo-example/metro.config.js
+++ examples/expo-example/metro.config.js
@@ -1,2 +1,29 @@
-require('ts-node/register');
-module.exports = require('./metro.config.ts');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('metro-config');
+const path = require('path');
+const defaultConfig = getDefaultConfig(__dirname);
+
+const { writeRequires } = require('@storybook/react-native/scripts/loader');
+
+module.exports = (async () => {
+  writeRequires({
+    configPath: path.resolve(__dirname, './.storybook'),
+    useRequireContext: true,
+  });
+
+  return mergeConfig(defaultConfig, {
+    watchFolders: ['../../', '.storybook', __dirname],
+    resolver: {
+      resolverMainFields: ['sbmodern', ...defaultConfig.resolver.resolverMainFields],
+      // unstable_enablePackageExports: true,
+      nodeModulesPaths: ['../../node_modules', './node_modules'],
+      disableHierarchicalLookup: true,
+      unstable_enableSymlinks: true,
+      // sourceExts: ['mjs', ...defaultConfig.resolver.sourceExts],
+    },
+    transformer: {
+      unstable_allowRequireContext: true,
+    },
+  });
+})();
--- packages/react-native/scripts/loader.js
+++ packages/react-native/scripts/loader.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
 const prettier = require('prettier');
-const { normalizeStories, globToRegexp, toRequireContext } = require('@storybook/core-common');
+const { normalizeStories, toRequireContext } = require('@storybook/core-common');
 
 const cwd = process.cwd();
 const supportedExtensions = ['js', 'jsx', 'ts', 'tsx', 'cjs', 'mjs'];
@@ -45,14 +45,17 @@ function normalizeExcludePaths(paths) {
 
 function requireUncached(module) {
   delete require.cache[require.resolve(module)];
+
   return require(module);
 }
 
 function getMain({ configPath }) {
   const fileExtension = getFilePathExtension({ configPath }, 'main');
+
   if (fileExtension === null) {
     throw new Error('main config file not found');
   }
+
   const mainPath = path.resolve(cwd, configPath, `main.${fileExtension}`);
 
   return requireUncached(mainPath);
@@ -61,10 +64,12 @@ function getMain({ configPath }) {
 function getFilePathExtension({ configPath }, fileName) {
   for (const ext of supportedExtensions) {
     const filePath = path.resolve(cwd, configPath, `${fileName}.${ext}`);
+
     if (fs.existsSync(filePath)) {
       return ext;
     }
   }
+
   return null;
 }
 
@@ -80,9 +85,13 @@ function writeRequires({ configPath, absolute = false, useRequireContext = false
   const storybookRequiresLocation = path.resolve(cwd, configPath, 'storybook.requires.js');
 
   const mainImport = getMain({ configPath });
+
   const main = mainImport.default ?? mainImport;
+
   const reactNativeOptions = main.reactNativeOptions;
+
   const excludePaths = reactNativeOptions && reactNativeOptions.excludePaths;
+
   const normalizedExcludePaths = normalizeExcludePaths(excludePaths);
 
   const storiesSpecifiers = normalizeStories(main.stories, {
@@ -95,9 +104,10 @@ function writeRequires({ configPath, absolute = false, useRequireContext = false
   if (useRequireContext) {
     const contexts = storiesSpecifiers.map((specifier) => {
       const { path: p, recursive: r, match: m } = toRequireContext(specifier);
-      // TODO: remove this dot 👇 and find actual solution
+      // TODO remove this dot 👇 and find actual solution
       return `require.context('.${p}', ${r}, ${m})`;
     });
+
     configure = `
       const stories = [${contexts.join(',')}];
       
@@ -115,11 +125,13 @@ function writeRequires({ configPath, absolute = false, useRequireContext = false
         })
         .map((storyPath) => {
           const pathWithDirectory = path.join(specifier.directory, storyPath);
+
           const requirePath = absolute
             ? storyPath
             : ensureRelativePathHasDot(path.relative(configPath, pathWithDirectory));
 
           const absolutePath = absolute ? requirePath : path.resolve(configPath, requirePath);
+
           const pathRelativeToCwd = path.relative(cwd, absolutePath);
 
           const normalizePathForWindows = (str) =>
@@ -131,7 +143,9 @@ function writeRequires({ configPath, absolute = false, useRequireContext = false
         });
       return [...acc, ...paths];
     }, []);
+
     const path_obj_str = `{${storyRequires.join(',')}}`;
+
     configure = `   
       const getStories=() => {
           return ${path_obj_str};
@@ -148,7 +162,9 @@ function writeRequires({ configPath, absolute = false, useRequireContext = false
   let previewJs = previewExists ? previewImports : '';
 
   const registerAddons = main.addons?.map((addon) => `import "${addon}/register";`).join('\n');
+
   let enhancersImport = '';
+
   let enhancers = '';
 
   // TODO: implement presets or something similar
