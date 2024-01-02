77ceb0d879a48ce9806f1b0d5076d0d4886fe766
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-01-02 13:53:17 +0000

    chore: update to 7.6.7

--- examples/expo-example/metro.config.js
+++ examples/expo-example/metro.config.js
@@ -21,29 +21,62 @@ module.exports = (async () => {
       // unstable_enablePackageExports: true,
       disableHierarchicalLookup: true,
       unstable_enableSymlinks: true,
-      resolverMainFields: ['react-native', 'module', 'browser', 'main'],
-
-      // NOTE from: https://github.com/aws/aws-sdk-js-v3/issues/4877#issuecomment-1803353706
+      // resolverMainFields: ['react-native', 'module', 'browser', 'main'],
       resolveRequest: (context, moduleName, platform) => {
-        // Additional logic here
-        const defaultResolveResult = context.resolveRequest(context, moduleName, platform);
-
-        if (
-          moduleName === 'punycode' &&
-          defaultResolveResult.type === 'sourceFile' &&
-          defaultResolveResult.filePath?.endsWith('punycode.es6.js')
-        ) {
+        if (moduleName === '@storybook/addon-actions') {
+          const defaultResolveResult = context.resolveRequest(context, moduleName, platform);
           defaultResolveResult.filePath = defaultResolveResult.filePath.replace(
-            'punycode.es6.js',
-            'punycode.js'
+            'index.js',
+            'index.mjs'
           );
+          return defaultResolveResult;
         }
-
-        if (moduleName === 'lru-cache' && defaultResolveResult.filePath?.endsWith('mjs')) {
-          defaultResolveResult.filePath = defaultResolveResult.filePath.replace('mjs', 'js');
-        }
-        return defaultResolveResult;
+        return context.resolveRequest(context, moduleName, platform);
       },
+      // NOTE from: https://github.com/aws/aws-sdk-js-v3/issues/4877#issuecomment-1803353706
+      // if (
+      //   moduleName === 'punycode' &&
+      //   defaultResolveResult.type === 'sourceFile' &&
+      //   defaultResolveResult.filePath?.endsWith('punycode.es6.js')
+      // ) {
+      //   defaultResolveResult.filePath = defaultResolveResult.filePath.replace(
+      //     'punycode.es6.js',
+      //     'punycode.js'
+      //   );
+      // }
+
+      // if (moduleName === 'lru-cache' && defaultResolveResult.filePath?.endsWith('mjs')) {
+      //   defaultResolveResult.filePath = defaultResolveResult.filePath.replace('mjs', 'js');
+      // }
+      // if (moduleName === '@storybook/global') {
+      //   console.log('res', defaultResolveResult);
+      //   // console.log(defaultResolveResult.filePath);
+      //   defaultResolveResult.filePath =
+      //     '/Users/danielwilliams/Workspace/storybook/react-native-storybook/node_modules/@storybook/global/dist/index.mjs';
+      // }
+      // const virtualPath = path.resolve(
+      //   __dirname,
+      //   'node_modules/.cache/virtual/virtual-module.js'
+      // );
+      // const fs = require('fs');
+      // // Create the virtual module in a generated directory...
+      // fs.mkdirSync(path.dirname(virtualPath), { recursive: true });
+      // fs.writeFileSync(virtualPath, 'module.exports =  {global: {}};');
+      // if (moduleName.startsWith('@storybook/global')) {
+      //   // Logic to resolve the module name to a file path...
+      //   // NOTE: Throw an error if there is no resolution.
+
+      //   return {
+      //     // filePath:
+      //     //   '/Users/danielwilliams/Workspace/storybook/react-native-storybook/node_modules/@storybook/global/dist/index.mjs',
+      //     // type: 'sourceFile',
+      //     filePath: virtualPath,
+      //     type: 'sourceFile',
+      //   };
+      // }
+
+      //   return context.resolveRequest(context, moduleName, platform);
+      // },
     },
     transformer: {
       unstable_allowRequireContext: true,
