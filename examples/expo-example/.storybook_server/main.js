9d4a0f45ed1fe7d08f4d48edc2b941aa6b8b05a9
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-01-15 18:22:44 +0000

    remove server in favour of addon server

--- examples/expo-example/.storybook_server/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: [
-    '../components/**/*.stories.?(ts|tsx|js|jsx)',
-    '../other_components/AnotherButton/AnotherButton.stories.tsx',
-  ],
-  logLevel: 'debug',
-  env: () => ({}),
-  addons: ['@storybook/addon-essentials'],
-};
