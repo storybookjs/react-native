4752f6a080c37429811d9f5e370bc2d0bb2f0d3d
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-02-06 00:01:43 +0000

    feat: add story unit test

---
+++ examples/expo-example/components/ControlExamples/Array/Array.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/react-native/extend-expect';
+import { render, screen } from '@testing-library/react-native';
+import { composeStory } from '@storybook/react';
+import Meta, { Basic } from './Array.stories';
+
+const ArrayStory = composeStory(Basic, Meta);
+
+test('form submits two answers', () => {
+  render(<ArrayStory />);
+  expect(screen.getByTestId('array-story-container')).toHaveTextContent(/abc/);
+});
