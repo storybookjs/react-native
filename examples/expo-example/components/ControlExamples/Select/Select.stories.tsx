da891b2f2eaee4fc6bd087589e0fe5772d890db8
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-07-12 22:03:27 +0100

    fix: ignore test for now

--- examples/expo-example/components/ControlExamples/Select/Select.stories.tsx
+++ examples/expo-example/components/ControlExamples/Select/Select.stories.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import type { StoryObj, Meta } from '@storybook/react';
 import { SelectExample } from './Select';
 
 const arrows = { ArrowUp: '⬆', ArrowDown: '⬇', ArrowLeft: '⬅️', ArrowRight: '➡️' };
 
-const SelectExampleMeta: Meta<typeof SelectExample> = {
+const meta = {
   title: 'ControlExamples/Select control',
   component: SelectExample,
   argTypes: {
@@ -18,56 +17,56 @@ const SelectExampleMeta: Meta<typeof SelectExample> = {
   parameters: {
     notes: 'Select from mulitple options!',
   },
+} satisfies Meta<typeof SelectExample>;
+
+export default meta;
+
+type SelectExampleStory = StoryObj<typeof meta>;
+
+export const Basic: SelectExampleStory = {
+  args: {
+    arrow: arrows.ArrowLeft,
+  },
 };
 
-export default SelectExampleMeta;
+export const WithLabels: SelectExampleStory = {
+  args: {
+    arrow: arrows.ArrowUp,
+  },
 
-type SelectExampleStory = StoryObj<typeof SelectExample>;
-
-export const Basic: SelectExampleStory = (args) => <SelectExample {...args} />;
-
-Basic.args = {
-  arrow: arrows.ArrowLeft,
-};
-
-export const WithLabels: SelectExampleStory = (args) => <SelectExample {...args} />;
-
-WithLabels.args = {
-  arrow: arrows.ArrowUp,
-};
-
-WithLabels.argTypes = {
-  arrow: {
-    options: Object.values(arrows),
-    control: {
-      type: 'select',
-      labels: {
-        [arrows.ArrowUp]: 'Up',
-        [arrows.ArrowDown]: 'Down',
-        [arrows.ArrowLeft]: 'Left',
-        [arrows.ArrowRight]: 'Right',
+  argTypes: {
+    arrow: {
+      options: Object.values(arrows),
+      control: {
+        type: 'select',
+        labels: {
+          [arrows.ArrowUp]: 'Up',
+          [arrows.ArrowDown]: 'Down',
+          [arrows.ArrowLeft]: 'Left',
+          [arrows.ArrowRight]: 'Right',
+        },
       },
     },
   },
 };
 
-export const WithMapping: SelectExampleStory = (args) => <SelectExample {...args} />;
+export const WithMapping: SelectExampleStory = {
+  args: {
+    arrow: 'ArrowRight',
+  },
 
-WithMapping.args = {
-  arrow: 'ArrowUp',
-};
-
-WithMapping.argTypes = {
-  arrow: {
-    options: Object.keys(arrows),
-    mapping: arrows,
-    control: {
-      type: 'select',
-      labels: {
-        ArrowUp: 'Up',
-        ArrowDown: 'Down',
-        ArrowLeft: 'Left',
-        ArrowRight: 'Right',
+  argTypes: {
+    arrow: {
+      options: Object.keys(arrows),
+      mapping: arrows,
+      control: {
+        type: 'select',
+        labels: {
+          ArrowUp: 'Up',
+          ArrowDown: 'Down',
+          ArrowLeft: 'Left',
+          ArrowRight: 'Right',
+        },
       },
     },
   },
