a349ec9145d31222b7a412745d237b7bef3169e2
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-08-18 13:50:37 +0100

    built in websocket server for withStorybook

--- examples/expo-example/babel.config.js
+++ examples/expo-example/babel.config.js
@@ -1,7 +1,22 @@
+// const path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]],
+    // plugins: [
+    //   [
+    //     'babel-plugin-react-docgen',
+    //     {
+    //       ignore: [
+    //         '**/node_modules/**',
+    //         '**/__tests__/**',
+    //         '**/__mocks__/**',
+    //         path.resolve(__dirname, '../../node_modules/*'),
+    //       ],
+    //     },
+    //   ],
+    // ],
   };
 };
