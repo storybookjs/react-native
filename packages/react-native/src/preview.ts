802137392af15603a9544c7cb6027f0af0bcd4e7
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-09-15 16:03:45 +0100

    fix: reuse more stuff

--- packages/react-native/src/preview.ts
+++ packages/react-native/src/preview.ts
@@ -1,30 +1,13 @@
-// @ts-ignore this exists but for some reason typescript doesn't want to believe it
-import { parameters as reactParameters } from '@storybook/react/dist/entry-preview-docs.mjs';
+import {
+  // @ts-ignore this exists but for some reason typescript doesn't want to believe it
+  parameters as reactParameters,
+  // @ts-ignore this exists but for some reason typescript doesn't want to believe it
+  argTypesEnhancers,
+} from '@storybook/react/dist/entry-preview-docs.mjs';
 import { type Preview } from '@storybook/react';
 
-import { combineParameters } from '@storybook/core/preview-api';
-import type { Renderer, StoryContextForEnhancers } from '@storybook/core/types';
-
-export const enhanceArgTypes = <TRenderer extends Renderer>(
-  context: StoryContextForEnhancers<TRenderer>
-) => {
-  const {
-    component,
-    argTypes: userArgTypes,
-    parameters: { docs = {} },
-  } = context;
-  const { extractArgTypes } = docs;
-
-  const extractedArgTypes = extractArgTypes && component ? extractArgTypes(component) : {};
-  const withExtractedTypes = extractedArgTypes
-    ? (combineParameters(extractedArgTypes, userArgTypes) as typeof userArgTypes)
-    : userArgTypes;
-
-  return withExtractedTypes;
-};
-
 const preview: Preview = {
-  argTypesEnhancers: [enhanceArgTypes],
+  argTypesEnhancers,
   parameters: {
     docs: {
       extractArgTypes: reactParameters.docs.extractArgTypes,
