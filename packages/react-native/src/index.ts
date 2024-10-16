a8737067e7bd1161ae41a2d7a52246f043e14c69
Archimedes Trajano <developer@trajano.net>
2024-10-15 23:33:36 -0400

    Add core config
    This is needed to disable telemetry

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig as StorybookConfigBase } from '@storybook/core/types';
+import type { CoreConfig, StorybookConfig as StorybookConfigBase } from '@storybook/core/types';
 import type { ReactNativeOptions } from './Start';
 export { darkTheme, theme, type Theme } from '@storybook/react-native-theming';
 
@@ -8,4 +8,5 @@ export interface StorybookConfig {
   stories: StorybookConfigBase['stories'];
   addons: string[];
   reactNative?: ReactNativeOptions;
+  core?: CoreConfig;
 }
