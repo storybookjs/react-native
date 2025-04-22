cfbd25dc4ec286cc4e123f02925ca245d8c7fdb3
Norbert de Langen <ndelangen@me.com>
2025-04-22 15:58:56 +0200

    chore: add @babel/plugin-transform-class-static-block to dependencies and update babel.config.js

--- examples/expo-example/babel.config.js
+++ examples/expo-example/babel.config.js
@@ -4,7 +4,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]],
+    plugins: [
+      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
+      ['@babel/plugin-transform-class-static-block', { loose: true }],
+    ],
     // plugins: [
     //   [
     //     'babel-plugin-react-docgen',
