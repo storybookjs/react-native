d3d4c8505aaeba00eaaf67b2458b65447498eedf
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-05-02 17:49:26 +0100

    fix: update storybook version

--- examples/expo-example/babel.config.js
+++ examples/expo-example/babel.config.js
@@ -4,11 +4,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // this started being required in v9 beta :(
-      '@babel/plugin-transform-class-static-block',
-      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
-    ],
+    plugins: [['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]],
     // plugins: [
     //   [
     //     'babel-plugin-react-docgen',
