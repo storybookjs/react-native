d5a3bff2a294e2a94aad8bf0df8080f05df51be6
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-06-19 20:31:33 +0100

    re-enable websockets

--- examples/expo-example/.rnstorybook/index.tsx
+++ examples/expo-example/.rnstorybook/index.tsx
@@ -12,7 +12,7 @@ const StorybookUIRoot = view.getStorybookUI({
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
   },
-  enableWebsockets: false,
+  enableWebsockets: true,
   // onDeviceUI: !isScreenshotTesting,
   host: 'localhost',
   port: 7007,
