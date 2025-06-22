67c9788ece6298b734534257d687fd5bc06eab9a
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-06-22 14:17:41 +0100

    fix: handle missing theme interface

--- packages/react-native-theming/scripts/patchdts.ts
+++ packages/react-native-theming/scripts/patchdts.ts
@@ -3,10 +3,17 @@ async function main() {
   // add a string of text at the end of the dist/index.d.ts file
   console.log('writing to dist/index.d.ts');
   const contents = await readFile('dist/index.d.ts', 'utf-8');
-  await writeFile(
-    'dist/index.d.ts',
-    contents.replace('interface Theme {}', 'export interface Theme extends StorybookTheme {}')
-  );
+  if (contents.includes('interface Theme {}')) {
+    await writeFile(
+      'dist/index.d.ts',
+      contents.replace('interface Theme {}', 'export interface Theme extends StorybookTheme {}')
+    );
+  } else {
+    await writeFile(
+      'dist/index.d.ts',
+      `${contents}\nexport interface Theme extends StorybookTheme {}`
+    );
+  }
 }
 
 main().catch((e) => {
