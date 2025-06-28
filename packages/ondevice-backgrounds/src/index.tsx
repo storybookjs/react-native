3a870907781a27b9ecb4dc44b610e683bdb6f4bf
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-06-28 18:06:29 +0100

    refactor: update withBackgrounds prop type for improved type safety

--- packages/ondevice-backgrounds/src/index.tsx
+++ packages/ondevice-backgrounds/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Container from './container';
 
-export const withBackgrounds = ({ Story }: { Story: React.ComponentType }) => (
+export const withBackgrounds = (Story: React.ComponentType<any>) => (
   <Container>
     <Story />
   </Container>
