c45a2b28e69a620b30e010613a3d4b3620ebf6a5
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-07-09 15:00:19 +0100

    fix: some versions

--- packages/react-native/jest.config.js
+++ packages/react-native/jest.config.js
@@ -2,7 +2,7 @@
 const config = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|storybook/.*|@storybook/.*|uuid)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|storybook/.*|@storybook/.*|uuid|storybook)',
   ],
 };
 module.exports = config;
