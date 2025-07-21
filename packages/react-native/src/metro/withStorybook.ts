7f417351c9606b5fa8ab14ca1c56f9e3616d6b64
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-07-21 13:28:28 +0100

    feat: add option to mock out ui dependencies (#771)
    * feat: add option to mock out ui dependencies
    
    * v9.0.18-alpha.0
    
    * fix: simplify if statement

--- packages/react-native/src/metro/withStorybook.ts
+++ packages/react-native/src/metro/withStorybook.ts
@@ -51,6 +51,12 @@ interface WithStorybookOptions {
    * Whether to include doc tools in the storybook.requires file. Defaults to true.
    */
   docTools?: boolean;
+
+  /**
+   * Whether to use lite mode for the storybook. Defaults to false.
+   * This will mock out the default storybook ui so you don't need to install all its dependencies like reanimated etc.
+   */
+  liteMode?: boolean;
 }
 
 type ResolveRequestFunction = (context: any, moduleName: string, platform: string | null) => any;
@@ -87,6 +93,7 @@ function withStorybook(
     useJs: false,
     onDisabledRemoveStorybook: false,
     docTools: true,
+    liteMode: false,
   }
 ): MetroConfig {
   const {
@@ -96,6 +103,7 @@ function withStorybook(
     useJs = false,
     onDisabledRemoveStorybook = false,
     docTools = true,
+    liteMode = false,
   } = options;
 
   if (!enabled) {
@@ -183,6 +191,17 @@ function withStorybook(
 
         // Workaround for template files with invalid imports
         if (resolveResult?.filePath?.includes?.('@storybook/react/template/cli')) {
+          return {
+            type: 'empty',
+          };
+        }
+
+        if (
+          liteMode &&
+          resolveResult?.filePath?.includes?.('@storybook/react-native-ui') &&
+          !resolveResult?.filePath?.includes?.('@storybook/react-native-ui-lite') &&
+          !resolveResult?.filePath?.includes?.('@storybook/react-native-ui-common')
+        ) {
           return {
             type: 'empty',
           };
