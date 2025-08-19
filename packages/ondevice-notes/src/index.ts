645ae0506ca2eccf83479eb176da1c117503e3ea
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-08-19 13:57:35 +0100

    fix: markdown-it vuln (#783)

--- packages/ondevice-notes/src/index.ts
+++ packages/ondevice-notes/src/index.ts
@@ -1,5 +1,6 @@
 import { logger } from 'storybook/internal/client-logger';
 
+// @ts-ignore
 if (__DEV__) {
   logger.log("import '@storybook/addon-ondevice-notes/register' to register the notes addon");
 }
