d8830ba717423815f39695d9e4f7f03dda85a95b
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-08-20 10:58:18 +0100

    update expo to 54 and enable react compiler

--- examples/expo-example/babel.config.js
+++ examples/expo-example/babel.config.js
@@ -4,6 +4,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo']],
-    plugins: [['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]],
+    plugins: [
+      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
+      'react-native-worklets/plugin',
+    ],
   };
 };
