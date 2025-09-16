be0b0d7aa6d75d1097d20a876cafcca5e5ab7859
Michael Shilman <shilman@users.noreply.github.com>
2025-09-16 08:47:29 -0700

    Prototype dev/build telemetry (untested)

--- packages/react-native/src/metro/withStorybook.ts
+++ packages/react-native/src/metro/withStorybook.ts
@@ -2,6 +2,8 @@ import * as path from 'path';
 import { generate } from '../../scripts/generate';
 import { WebSocketServer, WebSocket, Data } from 'ws';
 import type { MetroConfig } from 'metro-config';
+import { optionalEnvToBoolean } from 'storybook/internal/common';
+import { telemetry } from 'storybook/internal/telemetry';
 
 /**
  * Options for configuring WebSockets used for syncing storybook instances or sending events to storybook.
@@ -105,6 +107,12 @@ function withStorybook(
     docTools = true,
     liteMode = false,
   } = options;
+
+  const disableTelemetry = optionalEnvToBoolean(process.env.STORYBOOK_DISABLE_TELEMETRY);
+  if (!disableTelemetry && enabled) {
+    const event = process.env.NODE_ENV === 'production' ? 'build' : 'dev';
+    telemetry(event, {}).catch((e) => {});
+  }
 
   if (!enabled) {
     if (onDisabledRemoveStorybook) {
