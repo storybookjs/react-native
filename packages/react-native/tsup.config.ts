e577a490fe4b1a462b9b5494dd861252c8463b87
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2026-01-02 01:46:00 +0000

    format

--- packages/react-native/tsup.config.ts
+++ packages/react-native/tsup.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => {
   return {
-    entry: ['src/index.ts', 'src/preview.ts', 'src/metro/withStorybook.ts', 'src/stub.tsx', 'src/node.ts'],
+    entry: [
+      'src/index.ts',
+      'src/preview.ts',
+      'src/metro/withStorybook.ts',
+      'src/stub.tsx',
+      'src/node.ts',
+    ],
     // minify: !options.watch,
     clean: !options.watch,
     dts: !options.watch
