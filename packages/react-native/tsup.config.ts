887531fe0e10d8f8eaa6d11c6720474af40679fb
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2026-02-08 17:58:11 +0000

    style: format tsup.config.ts dts entry array

--- packages/react-native/tsup.config.ts
+++ packages/react-native/tsup.config.ts
@@ -14,7 +14,13 @@ export default defineConfig((options) => {
     clean: !options.watch,
     dts: !options.watch
       ? {
-          entry: ['src/index.ts', 'src/preview.ts', 'src/metro/withStorybook.ts', 'src/repack/withStorybook.ts', 'src/node.ts'],
+          entry: [
+            'src/index.ts',
+            'src/preview.ts',
+            'src/metro/withStorybook.ts',
+            'src/repack/withStorybook.ts',
+            'src/node.ts',
+          ],
           resolve: true,
         }
       : false,
