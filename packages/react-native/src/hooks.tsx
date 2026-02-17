cbedbc011ad67d91205835a49024356041f38848
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2026-02-17 22:19:55 +0000

    fix: jotai warning

--- packages/react-native/src/hooks.tsx
+++ packages/react-native/src/hooks.tsx
@@ -1,6 +1,8 @@
 import type { Args, StoryContext } from 'storybook/internal/csf';
-import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { atom, createStore, useAtomValue, useSetAtom } from 'jotai';
 import type { ReactRenderer } from '@storybook/react';
+
+const store = createStore();
 
 const storyContextAtom = atom(null as StoryContext<ReactRenderer, Args> | null);
 
@@ -8,12 +10,12 @@ const storyContextAtom = atom(null as StoryContext<ReactRenderer, Args> | null);
  * Hook that returns a function to set the current story context.
  */
 export function useSetStoryContext() {
-  return useSetAtom(storyContextAtom);
+  return useSetAtom(storyContextAtom, { store });
 }
 
 /**
  * Hook to read the current story context.
  */
 export function useStoryContext() {
-  return useAtomValue(storyContextAtom);
+  return useAtomValue(storyContextAtom, { store });
 }
