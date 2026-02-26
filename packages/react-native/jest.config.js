b830ed58ecd5e0235beeb2d7d3323788e1f7211a
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2026-02-26 19:45:39 +0000

    fix: tests

--- packages/react-native/jest.config.js
+++ packages/react-native/jest.config.js
@@ -4,7 +4,7 @@ const config = {
   setupFiles: ['<rootDir>/setup.js'],
 
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|storybook/.*|@storybook/.*|uuid)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|jest-expo|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|storybook/.*|@storybook/.*|uuid|@react-native/.*)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/scripts/generate\\.test\\.js$'],
 };
