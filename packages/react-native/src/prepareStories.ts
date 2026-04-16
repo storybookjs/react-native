11fba4e4e8b351df2858c2363bff5d922c42dff0
Norbert de Langen <ndelangen@me.com>
2026-04-16 15:20:42 +0200

    delete

--- packages/react-native/src/prepareStories.ts
+++ packages/react-native/src/prepareStories.ts
@@ -12,8 +12,6 @@ export interface ReactNativeOptions {
    * Note that this is for future and play functions are not yet fully supported on native.
    */
   playFn?: boolean;
-  liteMode?: boolean;
-  disableUI?: boolean;
 }
 
 export function prepareStories({
