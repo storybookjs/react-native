2b36ffdb31e26831e2c42213c60b82878e102825
Daniel Williams <me@dannyhw.com>
2026-05-02 13:17:48 +0100

    fix: use withstorybook wrapper for repack too

--- examples/repack-example/rspack.config.ts
+++ examples/repack-example/rspack.config.ts
@@ -1,7 +1,8 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import * as Repack from '@callstack/repack';
-import { StorybookPlugin } from '@storybook/react-native/repack/withStorybook';
+// import { StorybookPlugin } from '@storybook/react-native/repack/withStorybook';
+import { withStorybook } from '@storybook/react-native/withStorybook';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,35 +14,42 @@ const __dirname = path.dirname(__filename);
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-export default Repack.defineRspackConfig({
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions({
-      enablePackageExports: true,
-    }),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[cm]?[jt]sx?$/,
-        type: 'javascript/auto',
-        use: {
-          loader: '@callstack/repack/babel-swc-loader',
-          parallel: true,
-          options: {},
+export default withStorybook(
+  Repack.defineRspackConfig({
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions({
+        enablePackageExports: true,
+      }),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[cm]?[jt]sx?$/,
+          type: 'javascript/auto',
+          use: {
+            loader: '@callstack/repack/babel-swc-loader',
+            parallel: true,
+            options: {},
+          },
         },
-      },
-      ...Repack.getAssetTransformRules(),
+        ...Repack.getAssetTransformRules(),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin(),
+      // new StorybookPlugin({
+      //   enabled: true,
+      //   websockets: 'auto',
+      //   liteMode: true,
+      //   experimental_mcp: true,
+      // }),
     ],
-  },
-  plugins: [
-    new Repack.RepackPlugin(),
-    new StorybookPlugin({
-      enabled: true,
-      websockets: 'auto',
-      liteMode: true,
-      experimental_mcp: true,
-    }),
-  ],
-});
+  }),
+  {
+    experimental_mcp: true,
+    websockets: 'auto',
+    liteMode: true,
+  }
+);
