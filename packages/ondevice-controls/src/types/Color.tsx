2aa6378c923f6e767042bc8612fdd1a47df3618a
Daniel Williams <me@dannyhw.com>
2026-05-14 01:12:57 +0100

    fix update

--- packages/ondevice-controls/src/types/Color.tsx
+++ packages/ondevice-controls/src/types/Color.tsx
@@ -171,7 +171,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   modalOverlay: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     backgroundColor: 'rgba(0,0,0,0.3)',
   },
 });
