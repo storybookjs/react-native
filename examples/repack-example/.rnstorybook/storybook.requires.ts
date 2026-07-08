0b6fafd69555f2ee2e9881e993d13f66094080b6
Daniel Williams <me@dannyhw.com>
2026-07-08 20:00:27 +0100

    fix

--- examples/repack-example/.rnstorybook/storybook.requires.ts
+++ examples/repack-example/.rnstorybook/storybook.requires.ts
@@ -41,7 +41,7 @@ const annotations = [
 
 globalThis.STORIES = normalizedStories;
 globalThis.STORYBOOK_WEBSOCKET = {
-  host: '192.168.1.210',
+  host: '192.168.1.225',
   port: 7007,
   secured: false,
 };
